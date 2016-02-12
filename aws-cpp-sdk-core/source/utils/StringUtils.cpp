49775e53875046d079c3c4c1e4ee1793bd351d88
Henson <henso@amazon.com>
2016-02-12 15:48:03 -0800

    Forgot about windows not having a fully functioning c99 implementation.

--- aws-cpp-sdk-core/source/utils/StringUtils.cpp
+++ aws-cpp-sdk-core/source/utils/StringUtils.cpp
@@ -140,8 +140,12 @@ Aws::String StringUtils::URLEncode(const char* unsafe)
 Aws::String StringUtils::URLEncode(double unsafe)
 {
     char buffer[32];
+#if defined(_MSC_VER) && _MSC_VER < 1900
+    _vsnprintf_s(buffer, sizeof(buffer), _TRUNCATE, "%g", unsafe);
+#else
     snprintf(buffer, sizeof(buffer), "%g", unsafe);
     return URLEncode(buffer);
+#endif
 
 }
 
