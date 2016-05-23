cbf9feb3211ae22a40e090b50bc212c9fb11993b
Jonathan M. Henson <henso@amazon.com>
2016-05-23 15:57:39 -0700

    Doh! Missed an include guard for the first time in 5+ years.

--- aws-cpp-sdk-core/include/aws/core/utils/crypto/SecureRandom.h
+++ aws-cpp-sdk-core/include/aws/core/utils/crypto/SecureRandom.h
@@ -12,7 +12,7 @@
 * express or implied. See the License for the specific language governing
 * permissions and limitations under the License.
 */
-
+#pragma once
 #include <type_traits>
 
 namespace Aws
