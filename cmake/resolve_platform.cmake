7ea103a24335f5cebdf0e5cca3c681fae6923949
Bret Ambrose <bambrose@amazon.com>
2016-09-02 10:41:30 -0700

    Android build fixups; hand merge of ndk versioning logic

---
+++ cmake/resolve_platform.cmake
@@ -0,0 +1,37 @@
+
+# Platform recognition
+if(TARGET_ARCH)
+    string(TOUPPER ${TARGET_ARCH} __UPPER_TARGET_ARCH)
+endif()
+
+if("${__UPPER_TARGET_ARCH}" STREQUAL "WINDOWS" OR 
+   "${__UPPER_TARGET_ARCH}" STREQUAL "LINUX" OR 
+   "${__UPPER_TARGET_ARCH}" STREQUAL "APPLE" OR 
+   "${__UPPER_TARGET_ARCH}" STREQUAL "ANDROID")
+    SET(PLATFORM_${__UPPER_TARGET_ARCH} 1)
+elseif(TARGET_ARCH)
+    SET(PLATFORM_CUSTOM 1)
+else()
+    message(STATUS "TARGET_ARCH not specified; inferring host OS to be platform compilation target")
+    if(CMAKE_HOST_WIN32)
+        SET(PLATFORM_WINDOWS 1)
+        SET(TARGET_ARCH "WINDOWS")
+    elseif(CMAKE_HOST_APPLE)
+        SET(PLATFORM_APPLE 1)
+        SET(TARGET_ARCH "APPLE")
+    elseif(CMAKE_HOST_UNIX)
+        SET(PLATFORM_LINUX 1)
+        SET(TARGET_ARCH "LINUX")
+    else()
+        message(FATAL_ERROR "Unknown host OS; unable to determine platform compilation target")
+    endif()
+endif()
+
+string(TOLOWER ${TARGET_ARCH} __LOWER_ARCH)
+
+if(PLATFORM_LINUX OR PLATFORM_APPLE OR PLATFORM_ANDROID)
+    include(platform/unix)
+endif()
+
+include(platform/${__LOWER_ARCH})
+
