2302c405eb4b51fed43120f96e8f6955ec811b0c
Jonathan M. Henson <henso@amazon.com>
2016-11-18 22:08:08 -0800

    Added documentation to STS credentials provider.

--- aws-cpp-sdk-identity-management/include/aws/identity-management/auth/STSAssumeRoleCredentialsProvider.h
+++ aws-cpp-sdk-identity-management/include/aws/identity-management/auth/STSAssumeRoleCredentialsProvider.h
@@ -26,11 +26,29 @@ namespace Aws
 {
     namespace Auth
     {
+        /**
+         * The default credential lifetime is 15 minutes
+         */
         static const int DEFAULT_CREDS_LOAD_FREQ_SECONDS = 900;
 
+        /**
+         * Credentials provider for STS Assume Role
+         */
         class AWS_IDENTITY_MANAGEMENT_API STSAssumeRoleCredentialsProvider : public AWSCredentialsProvider
         {
         public:
+            /**
+             *
+             * Initializes credentials provider with
+             * roleArn - required, this is the arn for the role you want to assume.
+             * sessionName - if not specified, a unique session name will be generated for you.
+             * externalId - if not specified, it will not be sent to STS.
+             * loadFrequency, defaults to 15 minutes.
+             * stsClient, sts client implementation to use.
+             *
+             * For more information, see:
+             *    http://docs.aws.amazon.com/STS/latest/APIReference/API_AssumeRole.html
+             */
             STSAssumeRoleCredentialsProvider(const Aws::String& roleArn, const Aws::String& sessionName = Aws::String(), 
                 const Aws::String& externalId = Aws::String(), int loadFrequency = DEFAULT_CREDS_LOAD_FREQ_SECONDS, 
                 const std::shared_ptr<Aws::STS::STSClient>& stsClient = nullptr);
