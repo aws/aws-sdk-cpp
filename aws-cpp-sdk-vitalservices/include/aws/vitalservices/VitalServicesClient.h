5a921e41a837cf0c27c5c7beaedc510e7c9284c2
Christopher Simmons <christophersimmons@ChristohersMini.home>
2016-11-28 20:01:57 -0500

    Add vitalservices client.

---
+++ aws-cpp-sdk-vitalservices/include/aws/vitalservices/VitalServicesClient.h
@@ -0,0 +1,137 @@
+﻿/*
+* Copyright 2010-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License").
+* You may not use this file except in compliance with the License.
+* A copy of the License is located at
+*
+*  http://aws.amazon.com/apache2.0
+*
+* or in the "license" file accompanying this file. This file is distributed
+* on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+* express or implied. See the License for the specific language governing
+* permissions and limitations under the License.
+*/
+#pragma once
+#include <aws/vitalservices/VitalServices_EXPORTS.h>
+#include <aws/vitalservices/VitalServicesErrors.h>
+#include <aws/core/client/AWSError.h>
+#include <aws/core/client/ClientConfiguration.h>
+#include <aws/core/client/AWSClient.h>
+#include <aws/core/utils/memory/stl/AWSString.h>
+#include <aws/core/utils/json/JsonSerializer.h>
+#include <aws/vitalservices/model/getLoggedInUserResult.h>
+#include <aws/core/client/AsyncCallerContext.h>
+#include <aws/core/http/HttpTypes.h>
+#include <future>
+#include <functional>
+
+namespace Aws
+{
+
+namespace Http
+{
+  class HttpClient;
+  class HttpClientFactory;
+} // namespace Http
+
+namespace Utils
+{
+  template< typename R, typename E> class Outcome;
+
+namespace Threading
+{
+  class Executor;
+} // namespace Threading
+
+namespace Json
+{
+  class JsonValue;
+} // namespace Json
+} // namespace Utils
+
+namespace Auth
+{
+  class AWSCredentials;
+  class AWSCredentialsProvider;
+} // namespace Auth
+
+namespace Client
+{
+  class RetryStrategy;
+} // namespace Client
+
+namespace VitalServices
+{
+
+namespace Model
+{
+        class getLoggedInUserRequest;
+
+        typedef Aws::Utils::Outcome<getLoggedInUserResult, Aws::Client::AWSError<VitalServicesErrors>> getLoggedInUserOutcome;
+
+        typedef std::future<getLoggedInUserOutcome> getLoggedInUserOutcomeCallable;
+} // namespace Model
+
+  class VitalServicesClient;
+
+    typedef std::function<void(const VitalServicesClient*, const Model::getLoggedInUserRequest&, const Model::getLoggedInUserOutcome&, const std::shared_ptr<const Aws::Client::AsyncCallerContext>&) > getLoggedInUserResponseReceivedHandler;
+
+  class AWS_VITALSERVICES_API VitalServicesClient : public Aws::Client::AWSJsonClient
+  {
+    public:
+      typedef Aws::Client::AWSJsonClient BASECLASS;
+
+       /**
+        * Initializes client to use DefaultCredentialProviderChain, with default http client factory, and optional client config. If client config
+        * is not specified, it will be initialized to default values.
+        */
+        VitalServicesClient(const Client::ClientConfiguration& clientConfiguration = Client::ClientConfiguration());
+
+       /**
+        * Initializes client to use SimpleAWSCredentialsProvider, with default http client factory, and optional client config. If client config
+        * is not specified, it will be initialized to default values.
+        */
+        VitalServicesClient(const Auth::AWSCredentials& credentials, const Client::ClientConfiguration& clientConfiguration = Client::ClientConfiguration());
+
+       /**
+        * Initializes client to use specified credentials provider with specified client config. If http client factory is not supplied,
+        * the default http client factory will be used
+        */
+        VitalServicesClient(const std::shared_ptr<Auth::AWSCredentialsProvider>& credentialsProvider,
+            const Client::ClientConfiguration& clientConfiguration = Client::ClientConfiguration());
+
+        virtual ~VitalServicesClient();
+
+        /**
+         * 
+         */
+        virtual Model::getLoggedInUserOutcome getLoggedInUser(const Model::getLoggedInUserRequest& request) const;
+
+        /**
+         * 
+         *
+         * returns a future to the operation so that it can be executed in parallel to other requests.
+         */
+        virtual Model::getLoggedInUserOutcomeCallable getLoggedInUserCallable(const Model::getLoggedInUserRequest& request) const;
+
+        /**
+         * 
+         *
+         * Queues the request into a thread executor and triggers associated callback when operation has finished.
+         */
+        virtual void getLoggedInUserAsync(const Model::getLoggedInUserRequest& request, const getLoggedInUserResponseReceivedHandler& handler, const std::shared_ptr<const Aws::Client::AsyncCallerContext>& context = nullptr) const;
+
+
+    private:
+      void init(const Client::ClientConfiguration& clientConfiguration);
+
+        /**Async helpers**/
+        void getLoggedInUserAsyncHelper(const Model::getLoggedInUserRequest& request, const getLoggedInUserResponseReceivedHandler& handler, const std::shared_ptr<const Aws::Client::AsyncCallerContext>& context) const;
+
+      Aws::String m_uri;
+      std::shared_ptr<Utils::Threading::Executor> m_executor;
+  };
+
+} // namespace VitalServices
+} // namespace Aws
---
+++ aws-cpp-sdk-vitalservices/source/VitalServicesClient.cpp
@@ -0,0 +1,128 @@
+﻿/*
+* Copyright 2010-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License").
+* You may not use this file except in compliance with the License.
+* A copy of the License is located at
+*
+*  http://aws.amazon.com/apache2.0
+*
+* or in the "license" file accompanying this file. This file is distributed
+* on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+* express or implied. See the License for the specific language governing
+* permissions and limitations under the License.
+*/
+#include <aws/core/utils/Outcome.h>
+#include <aws/core/auth/AWSAuthSigner.h>
+#include <aws/core/client/CoreErrors.h>
+#include <aws/core/client/RetryStrategy.h>
+#include <aws/core/http/HttpClient.h>
+#include <aws/core/http/HttpResponse.h>
+#include <aws/core/http/HttpClientFactory.h>
+#include <aws/core/auth/AWSCredentialsProviderChain.h>
+#include <aws/core/utils/json/JsonSerializer.h>
+#include <aws/core/utils/memory/stl/AWSStringStream.h>
+#include <aws/core/utils/threading/Executor.h>
+#include <aws/vitalservices/VitalServicesClient.h>
+#include <aws/vitalservices/VitalServicesEndpoint.h>
+#include <aws/vitalservices/VitalServicesErrorMarshaller.h>
+#include <aws/vitalservices/model/getLoggedInUserRequest.h>
+
+using namespace Aws;
+using namespace Aws::Auth;
+using namespace Aws::Client;
+using namespace Aws::VitalServices;
+using namespace Aws::VitalServices::Model;
+using namespace Aws::Http;
+using namespace Aws::Utils::Json;
+
+static const char* SERVICE_NAME = "h6wguq9ghl.execute-api.us-east-1.amazonaws.com/prod";
+static const char* ALLOCATION_TAG = "VitalServicesClient";
+
+
+VitalServicesClient::VitalServicesClient(const Client::ClientConfiguration& clientConfiguration) :
+  BASECLASS(clientConfiguration,
+    Aws::MakeShared<AWSAuthV4Signer>(ALLOCATION_TAG, Aws::MakeShared<DefaultAWSCredentialsProviderChain>(ALLOCATION_TAG),
+        SERVICE_NAME, clientConfiguration.region),
+    Aws::MakeShared<VitalServicesErrorMarshaller>(ALLOCATION_TAG)),
+    m_executor(clientConfiguration.executor)
+{
+  init(clientConfiguration);
+}
+
+VitalServicesClient::VitalServicesClient(const AWSCredentials& credentials, const Client::ClientConfiguration& clientConfiguration) :
+  BASECLASS(clientConfiguration,
+    Aws::MakeShared<AWSAuthV4Signer>(ALLOCATION_TAG, Aws::MakeShared<SimpleAWSCredentialsProvider>(ALLOCATION_TAG, credentials),
+         SERVICE_NAME, clientConfiguration.region),
+    Aws::MakeShared<VitalServicesErrorMarshaller>(ALLOCATION_TAG)),
+    m_executor(clientConfiguration.executor)
+{
+  init(clientConfiguration);
+}
+
+VitalServicesClient::VitalServicesClient(const std::shared_ptr<AWSCredentialsProvider>& credentialsProvider,
+  const Client::ClientConfiguration& clientConfiguration) :
+  BASECLASS(clientConfiguration,
+    Aws::MakeShared<AWSAuthV4Signer>(ALLOCATION_TAG, credentialsProvider,
+         SERVICE_NAME, clientConfiguration.region),
+    Aws::MakeShared<VitalServicesErrorMarshaller>(ALLOCATION_TAG)),
+    m_executor(clientConfiguration.executor)
+{
+  init(clientConfiguration);
+}
+
+VitalServicesClient::~VitalServicesClient()
+{
+}
+
+void VitalServicesClient::init(const ClientConfiguration& config)
+{
+  Aws::StringStream ss;
+  ss << SchemeMapper::ToString(config.scheme) << "://";
+
+  if(config.endpointOverride.empty())
+  {
+    ss << VitalServicesEndpoint::ForRegion(config.region, config.useDualStack);
+  }
+  else
+  {
+    ss << config.endpointOverride;
+  }
+
+  m_uri = ss.str();
+}
+
+getLoggedInUserOutcome VitalServicesClient::getLoggedInUser(const getLoggedInUserRequest& request) const
+{
+  Aws::StringStream ss;
+  ss << m_uri << "/get-logged-in-user";
+
+  JsonOutcome outcome = MakeRequest(ss.str(), request, HttpMethod::HTTP_POST);
+  if(outcome.IsSuccess())
+  {
+    return getLoggedInUserOutcome(getLoggedInUserResult(outcome.GetResult()));
+  }
+  else
+  {
+    return getLoggedInUserOutcome(outcome.GetError());
+  }
+}
+
+getLoggedInUserOutcomeCallable VitalServicesClient::getLoggedInUserCallable(const getLoggedInUserRequest& request) const
+{
+  auto task = Aws::MakeShared< std::packaged_task< getLoggedInUserOutcome() > >(ALLOCATION_TAG, [this, request](){ return this->getLoggedInUser(request); } );
+  auto packagedFunction = [task]() { (*task)(); };
+  m_executor->Submit(packagedFunction);
+  return task->get_future();
+}
+
+void VitalServicesClient::getLoggedInUserAsync(const getLoggedInUserRequest& request, const getLoggedInUserResponseReceivedHandler& handler, const std::shared_ptr<const Aws::Client::AsyncCallerContext>& context) const
+{
+  m_executor->Submit( [this, request, handler, context](){ this->getLoggedInUserAsyncHelper( request, handler, context ); } );
+}
+
+void VitalServicesClient::getLoggedInUserAsyncHelper(const getLoggedInUserRequest& request, const getLoggedInUserResponseReceivedHandler& handler, const std::shared_ptr<const Aws::Client::AsyncCallerContext>& context) const
+{
+  handler(this, request, getLoggedInUser(request), context);
+}
+
