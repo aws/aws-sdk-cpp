919029eb68af94b5fec847cee17c183bf17d4a7e
Christopher Simmons <christophersimmons@ChristohersMini.home>
2017-02-01 11:17:33 -0500

    Use yapf on translate-vitalserver-spec.py.

--- translate-vitalserver-spec.py
+++ translate-vitalserver-spec.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 """
 A stupid hack of a script that I shouldn't have to write.
 """
@@ -11,6 +10,7 @@ import sys
 
 JSON_REF_KEYWORD = "$ref"
 INTERNAL_DEFINITION_URI_PATTERN = r"#/definitions/(.*)"
+
 
 def get_referenced_definition_name(reference_uri):
     """
@@ -35,11 +35,12 @@ def build_metadata(swagger_schema):
     match = re.match(r"(\d{4}-\d{2}-\d{2})T", version_date_time_string)
     aws_api_version = match.group(1)
     service_name = swagger_schema["info"]["title"]
-    return collections.OrderedDict(
-        [("apiVersion", aws_api_version),
-         ("endpointPrefix", "execute-api"),
-         ("protocol", "json"),
-         ("serviceFullName", service_name)])
+    return collections.OrderedDict([
+        ("apiVersion", aws_api_version),
+        ("endpointPrefix", "execute-api"),
+        ("protocol", "json"),
+        ("serviceFullName", service_name),
+    ])
 
 
 def build_operations(swagger_schema):
@@ -69,11 +70,11 @@ def build_operations(swagger_schema):
             "".join(component.title() for component in name_components[1:])
 
         full_path = "".join((base_path, path))
-        aws_http_info = collections.OrderedDict(
-                            [("method", "POST"),
-                             ("requestUri", full_path),
-                             ("responseCode", 200)])
-
+        aws_http_info = collections.OrderedDict([
+            ("method", "POST"),
+            ("requestUri", full_path),
+            ("responseCode", 200),
+        ])
 
         post_info = path_info["post"]
         if not "parameters" in post_info:
@@ -86,26 +87,29 @@ def build_operations(swagger_schema):
             post_parameter_schema[JSON_REF_KEYWORD]
 
         post_parameter_schema_name = get_referenced_definition_name(
-                                        post_parameter_schema_reference_uri)
+            post_parameter_schema_reference_uri)
 
         aws_input = collections.OrderedDict([("shape",
                                               post_parameter_schema_name)])
 
-        # Get the name of the schema referenced in the swagger schema's 200 response.
+        # Get the name of the schema referenced in the swagger schema's
+        # 200 response.
         response_schema_ref_string = \
             post_info["responses"]["200"]["schema"]["$ref"]
 
-        definition_name = get_referenced_definition_name(response_schema_ref_string)
+        definition_name = get_referenced_definition_name(
+            response_schema_ref_string)
 
         # Make the AWS schema's output a shape of the same name.
         aws_output = {"shape": definition_name}
 
         # Build the operation.
-        operation_info = collections.OrderedDict(
-                            [("name", aws_name),
-                             ("http", aws_http_info),
-                             ("input", aws_input),
-                             ("output", aws_output)])
+        operation_info = collections.OrderedDict([
+            ("name", aws_name),
+            ("http", aws_http_info),
+            ("input", aws_input),
+            ("output", aws_output),
+        ])
 
         operations[aws_name] = operation_info
 
@@ -116,13 +120,16 @@ def build_shapes(swagger_schema):
     """
     Builds the "shapes" portion of the api-definition.
     :param swagger_schema: The API swagger schema as a dictionary.
-    :return An ordered dictionary that represents the api-definition's "shapes".
+    :return An ordered dictionary that represents the api-definition's
+    "shapes".
     """
     shapes = collections.OrderedDict()
     top_level_definitions = swagger_schema["definitions"]
     resolved_definitions = collections.OrderedDict()
     for name, definition in top_level_definitions.items():
-        resolved_definition = resolve_json_refs(definition, top_level_definitions)
+        resolved_definition = resolve_json_refs(definition,
+                                                top_level_definitions)
+
         resolved_definitions[name] = resolved_definition
 
     definitions = resolved_definitions
@@ -138,8 +145,7 @@ def build_shapes(swagger_schema):
 
 def resolve_json_refs(definition, top_level_swagger_definitions):
     if "$ref" in definition:
-        match_result = re.match(r"#/definitions/(.*)",
-                                definition["$ref"])
+        match_result = re.match(r"#/definitions/(.*)", definition["$ref"])
 
         referenced_key = match_result.group(1)
         return top_level_swagger_definitions[referenced_key]
@@ -151,15 +157,13 @@ def resolve_json_refs(definition, top_level_swagger_definitions):
         new_properties = collections.OrderedDict()
         for property_name, property_definition in old_properties.items():
             new_properties[property_name] = resolve_json_refs(
-                                                property_definition,
-                                                top_level_swagger_definitions)
+                property_definition, top_level_swagger_definitions)
 
         new_definition["properties"] = new_properties
 
     elif definition_type == "array":
         new_definition["items"] = resolve_json_refs(
-                                    definition["items"],
-                                    top_level_swagger_definitions)
+            definition["items"], top_level_swagger_definitions)
 
     else:
         pass
@@ -179,23 +183,25 @@ def add_shape_from_swagger_definition(name, definition, shapes):
             member_shape_name = '_'.join((name, property_name))
             shape_members[property_name] = {"shape": member_shape_name}
             add_shape_from_swagger_definition(member_shape_name,
-                                              property_definition,
-                                              shapes)
+                                              property_definition, shapes)
 
-        shapes[name] = collections.OrderedDict([("type", "structure"),
-                                                ("required", shape_required),
-                                                ("members", shape_members)])
+        shapes[name] = collections.OrderedDict([
+            ("type", "structure"),
+            ("required", shape_required),
+            ("members", shape_members),
+        ])
 
     elif definition_type == "array":
         array_item_type = definition["items"]["type"]
         array_item_shape_name = name + "Item"
         shape_member = {"shape": array_item_shape_name}
         add_shape_from_swagger_definition(array_item_shape_name,
-                                          definition["items"],
-                                          shapes)
+                                          definition["items"], shapes)
 
-        shapes[name] = collections.OrderedDict([("type", "list"),
-                                                ("member", shape_member)])
+        shapes[name] = collections.OrderedDict([
+            ("type", "list"),
+            ("member", shape_member),
+        ])
 
     elif definition_type == "number":
         shapes[name] = {"type": "double"}
@@ -213,20 +219,24 @@ def build_aws_schema(swagger_schema):
     aws_metadata = build_metadata(swagger_schema)
     aws_operations = build_operations(swagger_schema)
     aws_shapes = build_shapes(swagger_schema)
-    return collections.OrderedDict(
-        [("version", "2.0"),
-         ("metadata", aws_metadata),
-         ("operations", aws_operations),
-         ("shapes", aws_shapes)])
+    return collections.OrderedDict([
+        ("version", "2.0"),
+        ("metadata", aws_metadata),
+        ("operations", aws_operations),
+        ("shapes", aws_shapes),
+    ])
 
 
 def main():
     with open("./VitalServices-prod-swagger.json", "r") as swagger_file:
-        swagger_schema = json.load(swagger_file, object_pairs_hook=collections.OrderedDict)
+        swagger_schema = json.load(
+            swagger_file, object_pairs_hook=collections.OrderedDict)
+
         aws_schema = build_aws_schema(swagger_schema)
         print(json.dumps(aws_schema, indent=4))
 
     return 0
+
 
 if __name__ == '__main__':
     sys.exit(main())
