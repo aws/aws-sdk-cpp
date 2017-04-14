ed885b365cf0fc687d0da0b1b0e6daea83a568df
Jonathan M. Henson <jonathan.michael.henson@gmail.com>
2017-04-14 13:18:13 -0700

    Revert "Improving CMake user experience"

--- cmake/platform/windows.cmake
+++ cmake/platform/windows.cmake
@@ -17,6 +17,5 @@ macro(apply_post_project_platform_settings)
     endif()    
    
     set(PLATFORM_DEP_LIBS Userenv Rpcrt4 version)    
-    set(PLATFORM_DEP_LIBS_ABSTRACT_NAME Userenv Rpcrt4 version)
 
 endmacro()
