2c99f1fe0c4b4683280caeb161538d4724d6a179
André Draszik <git@andred.net>
2017-04-20 10:50:56 +0100

    CryptoImpl: support LibreSSL again
    While LibreSSL strives to be API compatible with OpenSSL,
    it does so against the version when it was forked, 1.0.1g
    
    LibreSSL defines its own LIBRESSL_VERSION_NUMBER but at
    the same time it defines OPENSSL_VERSION_NUMBER to an
    insane value, which causes issues now.
    
    By detecting this case and setting a more reasonable
    version number, we can compile against LibreSSL again.
    
    Signed-off-by: André Draszik <git@andred.net>

--- aws-cpp-sdk-core/include/aws/core/utils/crypto/openssl/CryptoImpl.h
+++ aws-cpp-sdk-core/include/aws/core/utils/crypto/openssl/CryptoImpl.h
@@ -26,6 +26,11 @@
 #include <atomic>
 #include <mutex>
 
+#if defined(LIBRESSL_VERSION_NUMBER) && (OPENSSL_VERSION_NUMBER == 0x20000000L)
+#undef OPENSSL_VERSION_NUMBER
+#define OPENSSL_VERSION_NUMBER 0x1000107fL
+#endif
+
 namespace Aws
 {
     namespace Utils
