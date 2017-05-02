fa1863b4ddac1fcdc44a43817f1bcc6323e15e8b
Andrew (Liang) Tang <tangliang551@gmail.com>
2017-05-01 20:38:03 -0700

    Update CryptoImpl.cpp

--- aws-cpp-sdk-core/source/utils/crypto/openssl/CryptoImpl.cpp
+++ aws-cpp-sdk-core/source/utils/crypto/openssl/CryptoImpl.cpp
@@ -35,13 +35,13 @@ namespace Aws
         {
             namespace OpenSSL
             {
-				/**
-				* openssl with OPENSSL_VERSION_NUMBER < 0x10100003L made data type details unavailable
-				* libressl use openssl with data type details available, but mandatorily set 
-				* OPENSSL_VERSION_NUMBER = 0x20000000L, insane!
-				* https://github.com/aws/aws-sdk-cpp/pull/507/commits/2c99f1fe0c4b4683280caeb161538d4724d6a179
-				*/
-				#if defined(LIBRESSL_VERSION_NUMBER) && (OPENSSL_VERSION_NUMBER == 0x20000000L)
+		/**
+		* openssl with OPENSSL_VERSION_NUMBER < 0x10100003L made data type details unavailable
+		* libressl use openssl with data type details available, but mandatorily set 
+		* OPENSSL_VERSION_NUMBER = 0x20000000L, insane!
+		* https://github.com/aws/aws-sdk-cpp/pull/507/commits/2c99f1fe0c4b4683280caeb161538d4724d6a179
+		*/
+		#if defined(LIBRESSL_VERSION_NUMBER) && (OPENSSL_VERSION_NUMBER == 0x20000000L)
                 #undef OPENSSL_VERSION_NUMBER
                 #define OPENSSL_VERSION_NUMBER 0x1000107fL
                 #endif
