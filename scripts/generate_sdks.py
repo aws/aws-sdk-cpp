9db511693cbbe1fef87761aba87b080cfe5e52c1
Andrew (Liang) Tang <tangliang551@gmail.com>
2017-05-16 16:48:52 -0400

    License update (#104)
    Update license year

--- scripts/generate_sdks.py
+++ scripts/generate_sdks.py
@@ -1,7 +1,7 @@
 ﻿#!/usr/bin/env python
 
 #
-# Copyright 2010-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# Copyright 2010-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License").
 # You may not use this file except in compliance with the License.
