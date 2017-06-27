fb3dcf870722f1dd073bbf9fc3d9010a33ebb6ee
Jonathan M. Henson <henso@amazon.com>
2017-06-27 19:17:50 +0000

    Auto commit from CI.

--- aws-cpp-sdk-waf/source/WAFEndpoint.cpp
+++ aws-cpp-sdk-waf/source/WAFEndpoint.cpp
@@ -28,11 +28,19 @@ namespace WAFEndpoint
 {
   static const int CN_REGION_HASH = Aws::Utils::HashingUtils::HashString("cn-north-1");
   
+  static const int US_EAST_1_HASH = Aws::Utils::HashingUtils::HashString("us-east-1");
 
   Aws::String ForRegion(const Aws::String& regionName, bool useDualStack)
   {
     auto hash = Aws::Utils::HashingUtils::HashString(regionName.c_str());
     
+    if(!useDualStack)
+    {      
+      if(hash == US_EAST_1_HASH)
+      {
+        return "waf.amazonaws.com";
+      }
+    }
     Aws::StringStream ss;
     ss << "waf" << ".";
 
