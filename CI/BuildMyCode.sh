8823e65cf596c55da063cb5aa58a653b13b85efa
Andrew Tang <tangliang551@gmail.com>
2018-03-16 16:17:40 -0700

    Added missing regions definitions in core (Even though they are not all globally available)

--- CI/BuildMyCode.sh
+++ CI/BuildMyCode.sh
@@ -5,7 +5,7 @@ if [ $# -ne 2 ]; then
 fi
 branch=$1
 cmakeFlags=$2
-json='{ "branch": "'$branch'", "cmakeFlags": "'$cmakeFlags'"}'
+json='{ "branch": "'$branch'", "cmakeFlags": "'$cmakeFlags' "}'
 echo $json >BuildSpec.json
 zip -r BuildSpec.zip BuildSpec.json
 aws s3 cp BuildSpec.zip s3://aws-sdk-cpp-pipeline-sdks-team/BuildSpec.zip
