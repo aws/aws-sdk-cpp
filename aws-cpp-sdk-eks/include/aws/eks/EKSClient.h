157e17b5102ed72d326c5ea943d83a8333df956c
aws-sdk-cpp-automation <aws-sdk-cpp-automation@github.com>
2019-06-19 20:06:16 +0000

    Changing Amazon EKS full service name to Amazon Elastic Kubernetes Service.

--- aws-cpp-sdk-eks/include/aws/eks/EKSClient.h
+++ aws-cpp-sdk-eks/include/aws/eks/EKSClient.h
@@ -108,17 +108,17 @@ namespace Model
     typedef std::function<void(const EKSClient*, const Model::UpdateClusterVersionRequest&, const Model::UpdateClusterVersionOutcome&, const std::shared_ptr<const Aws::Client::AsyncCallerContext>&) > UpdateClusterVersionResponseReceivedHandler;
 
   /**
-   * <p>Amazon Elastic Container Service for Kubernetes (Amazon EKS) is a managed
-   * service that makes it easy for you to run Kubernetes on AWS without needing to
-   * stand up or maintain your own Kubernetes control plane. Kubernetes is an
-   * open-source system for automating the deployment, scaling, and management of
-   * containerized applications. </p> <p>Amazon EKS runs up-to-date versions of the
-   * open-source Kubernetes software, so you can use all the existing plugins and
-   * tooling from the Kubernetes community. Applications running on Amazon EKS are
-   * fully compatible with applications running on any standard Kubernetes
-   * environment, whether running in on-premises data centers or public clouds. This
-   * means that you can easily migrate any standard Kubernetes application to Amazon
-   * EKS without any code modification required.</p>
+   * <p>Amazon Elastic Kubernetes Service (Amazon EKS) is a managed service that
+   * makes it easy for you to run Kubernetes on AWS without needing to stand up or
+   * maintain your own Kubernetes control plane. Kubernetes is an open-source system
+   * for automating the deployment, scaling, and management of containerized
+   * applications. </p> <p>Amazon EKS runs up-to-date versions of the open-source
+   * Kubernetes software, so you can use all the existing plugins and tooling from
+   * the Kubernetes community. Applications running on Amazon EKS are fully
+   * compatible with applications running on any standard Kubernetes environment,
+   * whether running in on-premises data centers or public clouds. This means that
+   * you can easily migrate any standard Kubernetes application to Amazon EKS without
+   * any code modification required.</p>
    */
   class AWS_EKS_API EKSClient : public Aws::Client::AWSJsonClient
   {
