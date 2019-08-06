116b1562faf530942acf1c270324e0978f8c17ca
Pushen Wang <wps132230@hotmail.com>
2019-08-05 17:19:27 -0700

    Increase the timeout to wait until consumer is active in Kinesis integration tests.

--- aws-cpp-sdk-kinesis-integration-tests/KinesisTests.cpp
+++ aws-cpp-sdk-kinesis-integration-tests/KinesisTests.cpp
@@ -68,7 +68,7 @@ protected:
     {
         DescribeStreamConsumerRequest request;
         request.SetConsumerARN(consumerARN);
-        int maxRetries = 30;
+        int maxRetries = 18;
         while(maxRetries-- > 0)
         {
             const auto& outcome = m_client->DescribeStreamConsumer(request);
@@ -84,7 +84,7 @@ protected:
             {
                 FAIL();
             }
-            std::this_thread::sleep_for(std::chrono::seconds(2));
+            std::this_thread::sleep_for(std::chrono::seconds(10));
         }
         ASSERT_GT(maxRetries, 0);
     }
