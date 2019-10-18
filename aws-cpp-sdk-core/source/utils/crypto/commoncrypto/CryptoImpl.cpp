b671e22a5bd82dc5ed27797d21e3e5febe136e7a
Major Murphy <mmurphy@sawgrassink.com>
2019-10-18 16:58:17 -0400

    Fixed MD5 Deprectation Errors on Mac

--- aws-cpp-sdk-core/source/utils/crypto/commoncrypto/CryptoImpl.cpp
+++ aws-cpp-sdk-core/source/utils/crypto/commoncrypto/CryptoImpl.cpp
@@ -22,6 +22,12 @@
 #include <CommonCrypto/CommonCryptor.h>
 #include <CommonCrypto/CommonSymmetricKeywrap.h>
 
+// Fix for deprectation errors in OSX 10.15: https://github.com/aws/aws-sdk-cpp/issues/1259
+#if defined(__APPLE__) && defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+#endif
+
 //for OSX < 10.10 compatibility
 typedef int32_t CCStatus;
 typedef int32_t CCCryptorStatus;
@@ -541,3 +547,7 @@ namespace Aws
         }
     }
 }
+
+#if defined(__APPLE__) && defined(__clang__)
+#pragma clang diagnostic pop
+#endif
