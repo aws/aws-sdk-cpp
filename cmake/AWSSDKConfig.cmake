b102aaf5693c4165c84b616ab9ffb9edfb705239
Gilberto Lopez Zayas <vuner.lopez@gmail.com>
2021-12-09 14:06:24 -0800

    Fix build break in cmake 3.22.0-rc2 (#837)
     cmake 3.22.0-rc2 - Adding quotes to LIB_SEARCH_PATH to ensure there is always a string to compare to.
    
    Co-authored-by: David Watson <dvd_anstow@hotmail.co.uk>

--- cmake/AWSSDKConfig.cmake
+++ cmake/AWSSDKConfig.cmake
@@ -129,7 +129,7 @@ endif()
 get_filename_component(TEMP_PATH "${AWSSDK_CORE_LIB_FILE}" PATH)
 get_filename_component(TEMP_NAME "${TEMP_PATH}" NAME)
 
-while (NOT TEMP_NAME STREQUAL ${LIB_SEARCH_PREFIX})
+while (NOT TEMP_NAME STREQUAL "${LIB_SEARCH_PREFIX}")
     set(TEMP_PLATFORM_PREFIX "${TEMP_NAME}/${TEMP_PLATFORM_PREFIX}")
     get_filename_component(TEMP_PATH "${TEMP_PATH}" PATH)
     get_filename_component(TEMP_NAME "${TEMP_PATH}" NAME)
