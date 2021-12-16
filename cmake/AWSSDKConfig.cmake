4fae645b257eee9dc557626f81565b86baaae67f
Sergey Ryabinin <srryabin@amazon.com>
2021-12-16 00:15:39 +0000

    Fix CI install-test build on Windows shared library config: platform prefix maybe missing

--- cmake/AWSSDKConfig.cmake
+++ cmake/AWSSDKConfig.cmake
@@ -129,11 +129,13 @@ endif()
 get_filename_component(TEMP_PATH "${AWSSDK_CORE_LIB_FILE}" PATH)
 get_filename_component(TEMP_NAME "${TEMP_PATH}" NAME)
 
-while (NOT TEMP_NAME STREQUAL "${LIB_SEARCH_PREFIX}")
-    set(TEMP_PLATFORM_PREFIX "${TEMP_NAME}/${TEMP_PLATFORM_PREFIX}")
-    get_filename_component(TEMP_PATH "${TEMP_PATH}" PATH)
-    get_filename_component(TEMP_NAME "${TEMP_PATH}" NAME)
-endwhile()
+if (LIB_SEARCH_PREFIX)
+    while (NOT TEMP_NAME STREQUAL "${LIB_SEARCH_PREFIX}")
+        set(TEMP_PLATFORM_PREFIX "${TEMP_NAME}/${TEMP_PLATFORM_PREFIX}")
+        get_filename_component(TEMP_PATH "${TEMP_PATH}" PATH)
+        get_filename_component(TEMP_NAME "${TEMP_PATH}" NAME)
+    endwhile()
+endif()
 
 set(AWSSDK_PLATFORM_PREFIX "${TEMP_PLATFORM_PREFIX}")
 
