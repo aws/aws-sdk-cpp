001a80985281d73af74115b7c58cd340a70bd8c4
Damian Vicino <vicinod@amazon.com>
2022-05-20 15:32:03 -0400

    Adding support for inferring version validating build_types

--- src/aws-cpp-sdk-core/include/aws/core/VersionConfig.h
+++ src/aws-cpp-sdk-core/include/aws/core/VersionConfig.h
@@ -3,7 +3,14 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 
+// DEPRECATED: This file is used by legacy_build generators only
 #define AWS_SDK_VERSION_STRING 1.9.338
 #define AWS_SDK_VERSION_MAJOR 1
 #define AWS_SDK_VERSION_MINOR 9
 #define AWS_SDK_VERSION_PATCH 338
+// Forward compatible definitions
+#define AWS_CPP_SDK_VERSION_STRING AWS_SDK_VERSION_STRING
+#define AWS_CPP_SDK_VERSION_MAJOR AWS_SDK_VERSION_MAJOR
+#define AWS_CPP_SDK_VERSION_MINOR AWS_SDK_VERSION_MINOR
+#define AWS_CPP_SDK_VERSION_PATCH AWS_SDK_VERSION_PATCH
+#define AWS_CPP_SDK_GIT_HASH "NoHashAvailableInLegacyBuild"
