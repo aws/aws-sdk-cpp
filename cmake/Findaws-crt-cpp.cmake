4f4f3dd9044fcca7aa8e99af500f30b264d030be
Damian Vicino <vicinod@amazon.com>
2022-05-26 14:59:41 -0400

    Preparing toolchain and dependencies

---
+++ cmake/Findaws-crt-cpp.cmake
@@ -0,0 +1,3 @@
+# This is a temporary mock file to be able to test our current build setup.
+# The file will later be read from CRTpp installation
+message(WARNING "AWSCRTpp is mocked for testing of experimental feature branch. Remove this file when CRT starts providing it.")
