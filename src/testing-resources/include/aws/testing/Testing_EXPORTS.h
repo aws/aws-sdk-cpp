dcb101ba1e5d531451401d6ece7102248f574a18
Damian Vicino <vicinod@amazon.com>
2022-08-24 10:56:45 -0400

    Made Testing-Resources a library, DDB integ test works
    Moved Testing-Resources target to be a library since we use
    it in all the tests, integ and unit.
    Fixed missed inclussion in platform testing that prevented
    symbols to be accessible for the consumers of the library.
    Added generated Testing-Resources export.
    Added build of DynamoDB integration tests.

---
+++ src/testing-resources/include/aws/testing/Testing_EXPORTS.h
@@ -0,0 +1,28 @@
+/**
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+#pragma once
+
+#ifdef NON_LEGACY_BUILD
+#include "aws-sdk-cpp-testing-resources_export.h"
+#else
+    #if defined (USE_WINDOWS_DLL_SEMANTICS) || defined (_WIN32)
+    #ifdef _MSC_VER
+    #pragma warning(disable : 4251)
+    #endif // MSVC
+
+    #ifdef USE_IMPORT_EXPORT
+    #ifdef AWS_TESTING_EXPORTS
+    #define  AWS_TESTING_API __declspec(dllexport)
+    #else
+    #define  AWS_TESTING_API __declspec(dllimport)
+    #endif /* AWS_TESTING_EXPORTS */
+    #else
+    #define AWS_TESTING_API
+    #endif // USE_IMPORT_EXPORT
+    #else // defined (USE_WINDOWS_DLL_SEMANTICS) || defined (_WIN32)
+    #define AWS_TESTING_API
+    #endif // defined (USE_WINDOWS_DLL_SEMANTICS) || defined (_WIN32)
+#endif
