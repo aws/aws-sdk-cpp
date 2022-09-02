2420fad9ac374eed43e0aded0dae433cd9e421da
Damian Vicino <vicinod@amazon.com>
2022-09-02 15:11:02 -0400

    Small adjustments post rebase to 1.9.338

--- src/aws-cpp-sdk-core/include/aws/core/VersionConfig.h
+++ src/aws-cpp-sdk-core/include/aws/core/VersionConfig.h
@@ -4,7 +4,7 @@
  */
 
 // DEPRECATED: This file is used by legacy_build generators only
-#define AWS_SDK_VERSION_STRING 1.9.338
+#define AWS_SDK_VERSION_STRING "1.9.338"
 #define AWS_SDK_VERSION_MAJOR 1
 #define AWS_SDK_VERSION_MINOR 9
 #define AWS_SDK_VERSION_PATCH 338
