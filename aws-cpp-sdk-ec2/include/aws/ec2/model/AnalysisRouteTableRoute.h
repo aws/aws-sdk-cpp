a7de1c292661680e73f21b60fbb6597abec64de2
aws-sdk-cpp-automation <aws-sdk-cpp-automation@github.com>
2022-09-20 18:15:25 +0000

    This release adds support for blocked paths to Amazon VPC Reachability Analyzer.

--- aws-cpp-sdk-ec2/include/aws/ec2/model/AnalysisRouteTableRoute.h
+++ aws-cpp-sdk-ec2/include/aws/ec2/model/AnalysisRouteTableRoute.h
@@ -488,6 +488,55 @@ namespace Model
      */
     inline AnalysisRouteTableRoute& WithVpcPeeringConnectionId(const char* value) { SetVpcPeeringConnectionId(value); return *this;}
 
+
+    /**
+     * <p>The state. The following are the possible values:</p> <ul> <li> <p>active</p>
+     * </li> <li> <p>blackhole</p> </li> </ul>
+     */
+    inline const Aws::String& GetState() const{ return m_state; }
+
+    /**
+     * <p>The state. The following are the possible values:</p> <ul> <li> <p>active</p>
+     * </li> <li> <p>blackhole</p> </li> </ul>
+     */
+    inline bool StateHasBeenSet() const { return m_stateHasBeenSet; }
+
+    /**
+     * <p>The state. The following are the possible values:</p> <ul> <li> <p>active</p>
+     * </li> <li> <p>blackhole</p> </li> </ul>
+     */
+    inline void SetState(const Aws::String& value) { m_stateHasBeenSet = true; m_state = value; }
+
+    /**
+     * <p>The state. The following are the possible values:</p> <ul> <li> <p>active</p>
+     * </li> <li> <p>blackhole</p> </li> </ul>
+     */
+    inline void SetState(Aws::String&& value) { m_stateHasBeenSet = true; m_state = std::move(value); }
+
+    /**
+     * <p>The state. The following are the possible values:</p> <ul> <li> <p>active</p>
+     * </li> <li> <p>blackhole</p> </li> </ul>
+     */
+    inline void SetState(const char* value) { m_stateHasBeenSet = true; m_state.assign(value); }
+
+    /**
+     * <p>The state. The following are the possible values:</p> <ul> <li> <p>active</p>
+     * </li> <li> <p>blackhole</p> </li> </ul>
+     */
+    inline AnalysisRouteTableRoute& WithState(const Aws::String& value) { SetState(value); return *this;}
+
+    /**
+     * <p>The state. The following are the possible values:</p> <ul> <li> <p>active</p>
+     * </li> <li> <p>blackhole</p> </li> </ul>
+     */
+    inline AnalysisRouteTableRoute& WithState(Aws::String&& value) { SetState(std::move(value)); return *this;}
+
+    /**
+     * <p>The state. The following are the possible values:</p> <ul> <li> <p>active</p>
+     * </li> <li> <p>blackhole</p> </li> </ul>
+     */
+    inline AnalysisRouteTableRoute& WithState(const char* value) { SetState(value); return *this;}
+
   private:
 
     Aws::String m_destinationCidr;
@@ -519,6 +568,9 @@ namespace Model
 
     Aws::String m_vpcPeeringConnectionId;
     bool m_vpcPeeringConnectionIdHasBeenSet = false;
+
+    Aws::String m_state;
+    bool m_stateHasBeenSet = false;
   };
 
 } // namespace Model
