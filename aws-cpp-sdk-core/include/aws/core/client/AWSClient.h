327cac8819492f473fceb5541c0f2244e6f2f868
SergeyRyabinin <serzhick@gmail.com>
2022-11-16 17:07:04 +0000

    Add virtual constructor to request structures, refactor Async and Callable Operation functions

--- aws-cpp-sdk-core/include/aws/core/client/AWSClient.h
+++ aws-cpp-sdk-core/include/aws/core/client/AWSClient.h
@@ -13,6 +13,7 @@
 #include <aws/core/utils/crypto/Hash.h>
 #include <aws/core/auth/AWSAuthSignerProvider.h>
 #include <aws/core/endpoint/AWSEndpoint.h>
+#include <future>
 #include <memory>
 #include <atomic>
 
@@ -298,6 +299,123 @@ namespace Aws
              */
             Aws::Client::AWSAuthSigner* GetSignerByName(const char* name) const;
         protected:
+
+            /**
+             * A template function that is used to create an Async Operation function body for AWS Operations
+             */
+            template<typename ClientT,
+                     typename RequestT,
+                     typename HandlerT,
+                     typename HandlerContextT,
+                     typename OperationFuncT,
+                     typename ExecutorT>
+            void MakeAsyncOperation(OperationFuncT&& operationFunc,
+                                    const ClientT* clientThis,
+                                    const RequestT& request,
+                                    const HandlerT& handler,
+                                    const HandlerContextT& context,
+                                    ExecutorT* pExecutor) const
+            {
+                /** Call a virtual constructor just in case customer has inherited the request class.
+                    Use shared_ptr because unique_ptr is not copy-constructible, and a "packed" task for the Executor interface
+                      is represented by a std::function object (that must be copy-constructible). */
+                std::shared_ptr<RequestT> pRequest(request.Clone());
+                pExecutor->Submit( [operationFunc, clientThis, pRequest, handler, context]()
+                                   {
+                                       handler(clientThis,
+                                               *pRequest,
+                                               (clientThis->*operationFunc)(*pRequest),
+                                               context);
+                                   } );
+            }
+
+            /**
+             * A template function that is used to create an Async Operation function body for AWS Streaming Operations
+             *   The only difference compared to a regular non-streaming Operation is that
+             *   the request is passed by non-const reference, therefore virtual copy constructor is not needed.
+             *   However, caller code must ensure the life time of the request object is maintained during the Async execution.
+             */
+            template<typename ClientT,
+                     typename RequestT,
+                     typename HandlerT,
+                     typename HandlerContextT,
+                     typename OperationFuncT,
+                     typename ExecutorT>
+            void MakeAsyncStreamingOperation(OperationFuncT&& operationFunc,
+                                             const ClientT* clientThis,
+                                             RequestT& request, // note non-const ref
+                                             const HandlerT& handler,
+                                             const HandlerContextT& context,
+                                             ExecutorT* pExecutor) const
+            {
+                pExecutor->Submit( [operationFunc, clientThis, &request, handler, context]()  // note capture by ref
+                                   {
+                                       handler(clientThis,
+                                               request,
+                                               (clientThis->*operationFunc)(request),
+                                               context);
+                                   } );
+            }
+
+            /**
+             * A template function that is used to create a Callable Operation function body for AWS Operations
+             */
+            template<typename ClientT,
+                     typename RequestT,
+                     typename OperationFuncT,
+                     typename ExecutorT>
+            auto MakeCallableOperation(const char* ALLOCATION_TAG,
+                                       OperationFuncT&& operationFunc,
+                                       const ClientT* clientThis,
+                                       const RequestT& request,
+                                       ExecutorT* pExecutor) const -> std::future<decltype((clientThis->*operationFunc)(request))>
+            {
+                using OperationOutcomeT = decltype((clientThis->*operationFunc)(request));
+                /** Call a virtual copy constructor just in case customer has inherited the request class.
+                    For the same reason as in the template above. */
+                std::shared_ptr<RequestT> pRequest(request.Clone());
+
+                auto task = Aws::MakeShared< std::packaged_task< OperationOutcomeT() > >(
+                        ALLOCATION_TAG,
+                        [clientThis, operationFunc, pRequest]()
+                        {
+                            return (clientThis->*operationFunc)(*pRequest);
+                        } );
+
+                auto packagedFunction = [task]() { (*task)(); };
+                pExecutor->Submit(packagedFunction);
+                return task->get_future();
+            }
+
+            /**
+             * A template function that is used to create a Callable Operation function body for AWS Streaming Operations
+             *   The only difference compared to a regular non-streaming Operation is that
+             *   the request is passed by non-const reference, therefore virtual copy constructor is not needed.
+             *   However, caller code must ensure the life time of the request object is maintained during the Async execution.
+             */
+            template<typename ClientT,
+                     typename RequestT,
+                     typename OperationFuncT,
+                     typename ExecutorT>
+            auto MakeCallableStreamingOperation(const char* ALLOCATION_TAG,
+                                                OperationFuncT&& operationFunc,
+                                                const ClientT* clientThis,
+                                                RequestT& request,  // note non-const ref
+                                                ExecutorT* pExecutor) const -> std::future<decltype((clientThis->*operationFunc)(request))>
+            {
+                using OperationOutcomeT = decltype((clientThis->*operationFunc)(request));
+
+                auto task = Aws::MakeShared< std::packaged_task< OperationOutcomeT() > >(
+                        ALLOCATION_TAG,
+                        [clientThis, operationFunc, &request]()  // note capture by ref
+                        {
+                            return (clientThis->*operationFunc)(request);
+                        } );
+
+                auto packagedFunction = [task]() { (*task)(); };
+                pExecutor->Submit(packagedFunction);
+                return task->get_future();
+            }
 
             /**
               * Creates an HttpRequest instance with the given URI and sets the proper headers from the
