243fd8ddc0df32f6cd7d02d8c0a9cc5cb3c18625
Damian Vicino <vicinod@amazon.com>
2022-12-21 04:15:18 +0000

    Ignoring SigPipe in integration tests for Linux

--- aws-cpp-sdk-lambda-integration-tests/RunTests.cpp
+++ aws-cpp-sdk-lambda-integration-tests/RunTests.cpp
@@ -11,6 +11,7 @@
 
 int main(int argc, char** argv)
 {
+    Aws::Testing::SetDefaultSigPipeHandler();
     Aws::SDKOptions options;
     options.loggingOptions.logLevel = Aws::Utils::Logging::LogLevel::Trace;
     AWS_BEGIN_MEMORY_TEST_EX(options, 1024, 128);
