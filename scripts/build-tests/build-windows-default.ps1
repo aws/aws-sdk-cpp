6f17cd2018e2c72b5e00979779b5dc9e69ee8720
Damian Vicino <vicinod@amazon.com>
2023-01-11 00:16:02 -0500

    Adding build examples for Windows

---
+++ scripts/build-tests/build-windows-default.ps1
@@ -0,0 +1,13 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0.
+#
+# This script is an example of how the simplest build of the SDK using all default parameters
+# Directories created and files are prefixed with PREFIX_DIR argument
+# A clone of aws-sdk-cpp is expected to be in ${PREFIX_DIR}/aws-sdk-cpp
+# Platform: Windows
+param($PREFIX_DIR)
+
+mkdir "${PREFIX_DIR}/win-build"
+cd "${PREFIX_DIR}/win-build"
+&'C:\\Program Files\\CMake\\bin\\cmake.exe' ../aws-sdk-cpp
+&'C:\\Program Files\\CMake\\bin\\cmake.exe' --build . -j 8
---
+++ scripts/build-tests/run-windows-integ-tests.ps1
@@ -0,0 +1,22 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0.
+#
+# This script runs integration tests previously build
+# Directories created and files are prefixed with PREFIX_DIR argument
+# SDK build to tests is expected to be in ${PREFIX_DIR}/win-build, the source project in ${PREFIX_DIR}/aws-sdk-cpp,
+# A AWS_ACCOUNT with proper role setup is required to run the tests
+# The following varibles are expected in the environment PREFIX_DIR AWS_ACCOUNT AWS_ROLE_SESSION_NAME
+# Platform: Windows
+
+echo "Setting the run environment"
+$TEST_ASSUME_ROLE_ARN = "arn:aws:iam::${env:AWS_ACCOUNT}:role/IntegrationTest"
+$TEST_LAMBDA_CODE_PATH = "${env:PREFIX_DIR}/aws-sdk-cpp/aws-cpp-sdk-lambda-integration-tests/resources"
+$sts = aws sts assume-role --role-arn "${TEST_ASSUME_ROLE_ARN}" --role-session-name "${env:AWS_ROLE_SESSION_NAME}" --query "Credentials.[AccessKeyId, SecretAccessKey, SessionToken]"
+$sts
+aws configure set aws_access_key_id (${sts}[1] -replace " " -replace "`"" -replace ",")
+aws configure set aws_secret_access_key (${sts}[2] -replace " " -replace "`"" -replace ",")
+aws configure set aws_session_token (${sts}[3] -replace " " -replace "`"" -replace ",")
+aws configure list
+# Run tests
+cd "${env:PREFIX_DIR}\\win-build"
+python ../aws-sdk-cpp/scripts/run_integration_tests.py --testDir ./bin/Debug
