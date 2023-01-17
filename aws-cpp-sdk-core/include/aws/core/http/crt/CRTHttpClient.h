884044c655d121cfb293f6f806ddb1cfea8128c8
Jonathan M. Henson <henso@amazon.com>
2023-01-17 15:19:30 -0800

    Stuff to make linters pass.

--- aws-cpp-sdk-core/include/aws/core/http/crt/CRTHttpClient.h
+++ aws-cpp-sdk-core/include/aws/core/http/crt/CRTHttpClient.h
@@ -2,6 +2,7 @@
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0.
  */
+
 #include <aws/core/Core_EXPORTS.h>
 
 #include <aws/core/http/HttpClient.h>
