d98442d538676a6b29c19adc99495fc506ab548c
Damian Vicino <vicinod@amazon.com>
2023-01-23 18:20:41 -0500

    Fixing doc output path for build example script

--- tools/scripts/build-tests/build-al2-doxygen-docs.sh
+++ tools/scripts/build-tests/build-al2-doxygen-docs.sh
@@ -16,30 +16,26 @@ if [ "$#" -ne 2 ]; then
 fi
 PREFIX_DIR="$1"
 OUTPUT_DIR="${PREFIX_DIR}/$2"
+SRC_DIR="${PREFIX_DIR}/aws-sdk-cpp"
 
-mkdir "${PREFIX_DIR}/al2-build"
-mkdir "${PREFIX_DIR}/al2-install"
-cd "${PREFIX_DIR}/al2-build"
-cmake -GNinja ../aws-sdk-cpp -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="-ggdb -fsanitize=address" -DMINIMIZE_SIZE=ON -DCMAKE_INSTALL_PREFIX="${PREFIX_DIR}/al2-install"
-ninja-build -j $(grep -c ^processor /proc/cpuinfo)
-ninja-build install
-
-export VERSION_NUM=$(grep AWS_SDK_VERSION_STRING "${PREFIX_DIR}/aws-sdk-cpp/src/aws-cpp-sdk-core/include/aws/core/VersionConfig.h" | cut -d '\"' -f2)
+export VERSION_NUM=$(grep AWS_SDK_VERSION_STRING "${SRC_DIR}/src/aws-cpp-sdk-core/include/aws/core/VersionConfig.h" | cut -d '"' -f2)
 echo "Setting config to build docs for version: ${VERSION_NUM}"
-sed -i "s/PROJECT_NUMBER .*/PROJECT_NUMBER         = $VERSION_NUM/" "${PREFIX_DIR}/aws-sdk-cpp/tools/doxygen/modules/static/root.config"
-sed -i "s/PROJECT_NUMBER .*/PROJECT_NUMBER         = $VERSION_NUM/" "${PREFIX_DIR}/aws-sdk-cpp/tools/doxygen/modules/template/module-template.config"
+sed -i "s/PROJECT_NUMBER .*/PROJECT_NUMBER         = ${VERSION_NUM}/" "${SRC_DIR}/tools/doxygen/modules/static/root.config"
+sed -i "s/PROJECT_NUMBER .*/PROJECT_NUMBER         = ${VERSION_NUM}/" "${SRC_DIR}/tools/doxygen/modules/template/module-template.config"
+
 echo "Running doc generation"
-cd "${PREFIX_DIR}/aws-sdk-cpp"
-python3 "${PREFIX_DIR}/aws-sdk-cpp/tools/doxygen/modules/scripts/run-doc-generation.py"
+cd "${SRC_DIR}"
+python3 "${SRC_DIR}/tools/doxygen/modules/scripts/run-doc-generation.py"
 echo "Staging static files"
-cp "${PREFIX_DIR}/aws-sdk-cpp/LICENSE" "${PREFIX_DIR}/aws-sdk-cpp/tools/doxygen/output/root/html"
+mkdir -p "${OUTPUT_DIR}/root/html"
+cp "${SRC_DIR}/LICENSE" "${OUTPUT_DIR}/root/html"
 mkdir -p "${OUTPUT_DIR}/${VERSION_NUM}"
 mkdir -p "${OUTPUT_DIR}/LATEST"
-rsync -av "${PREFIX_DIR}"/aws-sdk-cpp/tools/doxygen/output/* "${OUTPUT_DIR}/${VERSION_NUM}"
-rsync -av "${PREFIX_DIR}"/aws-sdk-cpp/tools/doxygen/output/* "${OUTPUT_DIR}/LATEST"
+rsync -av "${SRC_DIR}"/doxygen/output/* "${OUTPUT_DIR}/${VERSION_NUM}"
+rsync -av "${SRC_DIR}"/doxygen/output/* "${OUTPUT_DIR}/LATEST"
 echo "Creating Cross Links"
-python3 doc_crosslinks_new/generate_cross_link_data.py --apiDefinitionsPath ./code-generation/api-descriptions/ --templatePath ./doc_crosslinks_new/crosslink_redirect.html --outputPath ./crosslink_redirect.html
-cp "${PREFIX_DIR}/aws-sdk-cpp/tools/doxygen/modules/static/index.html" -p "${OUTPUT_DIR}/"
-cp "${PREFIX_DIR}/aws-sdk-cpp/tools/doxygen/modules/static/local-index.html" "${OUTPUT_DIR}/LATEST/index.html"
-cp "${PREFIX_DIR}/aws-sdk-cpp/tools/doxygen/modules/static/local-index.html" "${OUTPUT_DIR}/${VERSION_NUM}/index.html"
-cp "${PREFIX_DIR}/aws-sdk-cpp/crosslink_redirect.html" "${OUTPUT_DIR}/"
+python3 doc_crosslinks_new/generate_cross_link_data.py --apiDefinitionsPath ./tools/code-generation/api-descriptions/ --templatePath ./doc_crosslinks_new/crosslink_redirect.html --outputPath ./crosslink_redirect.html
+cp "${SRC_DIR}/doxygen/modules/static/index.html" -p "${OUTPUT_DIR}/"
+cp "${SRC_DIR}/doxygen/modules/static/local-index.html" "${OUTPUT_DIR}/LATEST/index.html"
+cp "${SRC_DIR}/doxygen/modules/static/local-index.html" "${OUTPUT_DIR}/${VERSION_NUM}/index.html"
+cp "${SRC_DIR}/crosslink_redirect.html" "${OUTPUT_DIR}/"
