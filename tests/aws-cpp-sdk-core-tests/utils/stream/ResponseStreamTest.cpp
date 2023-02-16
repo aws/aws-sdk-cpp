80e7d980276369c75f3556c1044ae35663facf6f
SergeyRyabinin <serzhick@gmail.com>
2023-02-16 17:32:56 +0000

    ResponseStream to register callback on underlying stream pointer to avoid double free

---
+++ tests/aws-cpp-sdk-core-tests/utils/stream/ResponseStreamTest.cpp
@@ -0,0 +1,51 @@
+/**
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+#include <gtest/gtest.h>
+#include <aws/core/utils/stream/ResponseStream.h>
+#include <aws/core/utils/memory/stl/AWSStringStream.h>
+
+using Aws::Utils::Stream::ResponseStream;
+using Aws::StringStream;
+
+static_assert(!std::is_copy_constructible<ResponseStream>::value, "This is a move only type.");
+static_assert(std::is_move_constructible<ResponseStream>::value, "This is a move only type.");
+static_assert(!std::is_assignable<ResponseStream, ResponseStream&>::value, "This is a move only type.");
+static_assert(!std::is_assignable<ResponseStream, const ResponseStream&>::value, "This is a move only type.");
+static_assert(std::is_assignable<ResponseStream, ResponseStream&&>::value, "This is a move only type.");
+static_assert(std::is_default_constructible<ResponseStream>::value, "Must support default construction.");
+
+TEST(ResponseStreamTest, TestResponseStreamBasic)
+{
+    ResponseStream defaultResponseStream;
+
+    Aws::IOStream* pStream = Aws::Utils::Stream::DefaultResponseStreamFactoryMethod();
+    ResponseStream defaultResponseStreamWithStream(pStream);
+    EXPECT_EQ(&defaultResponseStreamWithStream.GetUnderlyingStream(), pStream);
+
+    defaultResponseStream = std::move(defaultResponseStreamWithStream);
+    EXPECT_EQ(&defaultResponseStream.GetUnderlyingStream(), pStream);
+
+    ResponseStream moveCtorResponseStream(std::move(defaultResponseStream));
+    EXPECT_EQ(&moveCtorResponseStream.GetUnderlyingStream(), pStream);
+}
+
+TEST(ResponseStreamTest, TestUnderlyingStreamGoneBeforeWrapper)
+{
+    Aws::IOStream* pStream = Aws::Utils::Stream::DefaultResponseStreamFactoryMethod();
+    ResponseStream responseStream(pStream);
+    Aws::Delete(pStream);
+    // must not crash in ~ResponseStream
+}
+
+TEST(ResponseStreamTest, TestUnderlyingStreamOutlivesWrapper)
+{
+    Aws::IOStream* pStream = Aws::Utils::Stream::DefaultResponseStreamFactoryMethod();
+    {
+        ResponseStream responseStream(pStream);
+    }
+    // must not crash and must not memory leak, pStream is deleted by ~ResponseStream
+}
+
