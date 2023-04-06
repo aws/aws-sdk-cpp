1e06dbfbf28fd2836008b102b1c5ff99305edcf4
Damian Vicino <vicinod@amazon.com>
2023-04-06 15:16:07 -0400

    Starting to add core to the non-legacy build

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,3 +1,34 @@
+if (NOT LEGACY_BUILD)
+    # Project dependencies http library (curl or OS), tinyxml2, cJSON, ssl (open or other), CRT
+    find_package(CURL REQUIRED)
+    find_package(ZLIB REQUIRED)
+    find_package(OpenSSL REQUIRED COMPONENTS Crypto)
+    find_package(tinyxml2 9.0.0 REQUIRED)
+    find_package(cJSON 1.7.15 REQUIRED)
+    message(STATUS "Checking for aws-crt-cpp version 0.19.8")
+    find_package(aws-crt-cpp 0.20.0 EXACT REQUIRED)
+    message(STATUS "  Found aws-crt-cpp version ${aws-crt-cpp_VERSION}")
+    # Infer dirs and libs temporarily from aws-crt-cpp_DIR until they get shipped in the new CRT version.
+    set(aws-crt-cpp_INCLUDE_DIR "${aws-crt-cpp_DIR}/../../../include")
+    set(aws-crt-cpp_INCLUDE_DIRS "${aws-crt-cpp_DIR}/../../../include")
+    set(aws-crt-cpp_LIBRARY_DIRS "${aws-crt-cpp_DIR}/../..")
+    set(aws-crt-cpp_LIBRARIES aws-crt-cpp aws-c-common aws-c-event-stream aws-c-mqtt aws-c-s3 aws-c-auth aws-c-sdkutils aws-c-http aws-c-compression aws-c-io aws-checksums aws-c-common aws-c-cal)
+    set(aws-crt-cpp_LIBRARY_DIRS "${aws-crt-cpp_DIR}/../..")
+    find_package(s2n QUIET)
+    if (s2n_FOUND)
+        message(STATUS "  Found s2n version ${s2n_VERSION}")
+        set(aws-crt-cpp_LIBRARIES ${aws-crt-cpp_LIBRARIES} s2n)
+    endif ()
+    # Up to here can be removed once CRT updated to provide this data
+
+    if (BUILD_TESTING)
+        message(STATUS "Building tests")
+        # Testing dependency
+        find_package(GTest 1.11 REQUIRED)
+        include(GoogleTest) # for gtest_discover_tests()
+    endif ()
+
+else() #NOT LEGACY_BUILD (so, legacy build in the else)
 # from http://stackoverflow.com/questions/34637980/cmake-sort-a-list-of-target-considering-their-dependencies
 
 define_property(TARGET PROPERTY LINK_LIBRARIES_ALL
@@ -31,7 +62,7 @@ function(compute_links lib)
 
     get_target_property(links ${lib} LINK_LIBRARIES)
 
-    if(NOT links) 
+    if(NOT links)
         return() # Do not iterate over `-NOTFOUND` value in case of absence of the property.
     endif()
 
@@ -104,3 +135,6 @@ function(sort_links targets_list)
     # Directly append the only element in the current input list to the resulted variable.
     set(${targets_list} ${output_list} ${current_input_list} PARENT_SCOPE)
 endfunction(sort_links)
+endif() #NOT LEGACY_BUILD
+
+
