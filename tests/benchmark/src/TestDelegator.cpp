2cad4b8d796888bcfb23f9936570d6f746d8d92a
sbiscigl <sbiscigl@amazon.com>
2023-04-13 11:17:01 -0400

    Add basic benchmark test for s3 get object

--- tests/benchmark/src/TestDelegator.cpp
+++ tests/benchmark/src/TestDelegator.cpp
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 #include <service/S3PutObject.h>
+#include <service/S3GetObject.h>
 #include <TestDelegator.h>
 #include <iostream>
 
@@ -25,6 +26,7 @@ Benchmark::TestDelegator::GetTestRunForConfiguration(const Benchmark::Configurat
 
 Benchmark::TestDelegator Benchmark::TestDelegator::BuildTestDelegator() {
     return Benchmark::TestDelegator({
-        {std::make_pair("s3", "PutObject"), S3PutObject::CreateTestFunction()}
+        {std::make_pair("s3", "PutObject"), S3PutObject::CreateTestFunction()},
+        {std::make_pair("s3", "GetObject"), S3GetObject::CreateTestFunction()}
     });
 }
