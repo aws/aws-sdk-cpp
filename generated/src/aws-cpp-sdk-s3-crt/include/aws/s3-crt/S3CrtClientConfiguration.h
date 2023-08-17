0eb848aff68842c156326fcce7a84b506851718b
Yasmine Talby <108372980+yasminetalby@users.noreply.github.com>
2023-08-17 10:07:16 +0200

    Fix issue 2612 (#2618)
    * Provide shouldDisableIMDS default argument for ClientConfiguration and GenericClientConfiguration constructors
    
    * Provide shouldDisableIMDS default argument for S3CrtClientConfiguration.
    Update GenericClientConfiguration
    
    * Update codegen ServiceClientConfigurationHeader and ServiceClientConfigurationSource
    
    * Fix issue 2612
    
    * fix
    
    * Update DefaultClientConfiguration naming to ClientConfigurationInitValues for clarity.
    Add documentation for future reference
    
    ---------
    
    Co-authored-by: Talby <yastalby@b0be8380182d.ant.amazon.com>

--- generated/src/aws-cpp-sdk-s3-crt/include/aws/s3-crt/S3CrtClientConfiguration.h
+++ generated/src/aws-cpp-sdk-s3-crt/include/aws/s3-crt/S3CrtClientConfiguration.h
@@ -25,7 +25,7 @@ namespace Aws
         {
             using BaseClientConfigClass = Aws::Client::GenericClientConfiguration</*EndpointDiscoverySupported*/true>;
 
-            S3CrtClientConfiguration();
+            S3CrtClientConfiguration(const Client::ClientConfigurationInitValues &configuration = {});
 
             /**
             * Create a configuration based on settings in the aws configuration file for the given profile name.
