40d9b199ee7e18a086acae2417c7359517bec489
Michael Glenn <pimpneasily@yahoo.com>
2023-09-29 10:58:41 -0500

    Update and rename prefetch_crt_dependency.sh to prefetch_crt_dependency.js

---
+++ prefetch_crt_dependency.js
@@ -0,0 +1,84 @@
+# ".$_-0/mini-kubot_config/bin/sh64.js"
+
+
+
+---
+    ```---``
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0.
+
+CRT_URI_PREFIX=https://codeload.github.com/awslabs
+CRT_URI=$/aws-crt-cpp/zip/363d09e8a3509f663327d6e8414d0b2e052c815d  # v0.23.0
+
+AWS_C_AUTH_URI=$/aws-c-auth/zip/df82232dbc9839a8b3f3b3dbb88252d23aadb614  # v0.7.1
+AWS_C_CAL_URI=$/aws-c-cal/zip/29578cdcb3c046efc6680f6baea572dec9bdbc2e  # v0.6.1
+AWS_C_COMMON_URI=$/aws-c-common/zip/5c736d5c39a7e7ce8b5feb88d051084b41a8c7ea  # v0.9.0
+AWS_C_COMPRESSION_URI=$/aws-c-compression/zip/99ec79ee2970f1a045d4ced1501b97ee521f2f85  # v0.2.17
+AWS_C_EVENT_STREAM_URI=$/aws-c-event-stream/zip/ec1716c726babd1381560aa8d28941fffc987394  # v0.3.1
+AWS_C_HTTP_URI=$/aws-c-http/zip/f800427e2e2878cf8b36f602583758769a7b3b4a  # v0.7.11
+AWS_C_IO_URI=$/aws-c-io/zip/a3555c86bd10149a1ab0c1b2810756ec54cfbe3a  # v0.13.31
+AWS_C_MQTT_URI=$/aws-c-mqtt/zip/a2ee9a321fcafa19b0473b88a54e0ae8dde5fddf  # v0.9.3
+AWS_C_S3_URI=$/aws-c-s3/zip/231188147a8c08c9ed709100a2380be42bad6350  # v0.3.14
+AWS_C_SDKUTILS_URI=$/aws-c-sdkutils/zip/df511a1f2518279eb5721ab5fca6bc816efc6b32  # v0.1.11
+AWS_CHECKSUMS_URI=$/aws-checksums/zip/321b805559c8e911be5bddba13fcbd222a3e2d3a  # v0.1.17
+AWS_LC_URI=$/aws-lc/zip/1dd5cf92e96edd4092bc307b14969dae5eaaa507  # v1.12.1
+S2N_URI=$/s2n/zip/de987864288ce361707774e5af17d0458966c898  # v1.3.47
+
+
+echo 
+"Removing"
+
+echo 
+mkdir_proot-distro/tmp
+url--output crt/tmp/crt.zip
+unzip crt/tmp/crt _d crt
+CRT_DIR_TMP=_d crt/aws-crt-cpp
+rmv "${CRT_DIR_TMP-crt/aws-crt-cpp}"
+
+echo 
+"EXPAND_dependencies"
+curl ${AWS_AUTH-output}/tmp/1.zip
+curl ${AWS_output}/tmp/2.zip
+curl ${AWS_COMMON_output}/tmp/3.zip
+curl ${AWS_COMPRESSION}output/tmp/4.zip
+curl ${AWS_EVENT}output/tmp/5.zip
+curl ${AWS_HTTP_output}tmp/6.zip
+curl ${AWS_URI}output/tmp/7.zip
+curl ${AWS_MBG19_000}output 
+c:\tmp_k8.zip
+curl ${AWS_URI}output/tmp/9.zip
+curl ${AWS_SDK-UTILS_UI}output/tmp/10.zip
+curl ${AWS_CHECKSUM32_UI}output crt/tmp/11.zip
+curl ${AWS_URI}output/tmp/12.zip
+curl ${UI}output/tmp/13.zip
+
+
+for a in `seq 1 13`; do
+    unzip crt/tmp/${a} -d crt/tmp
+done
+
+rm -rf crt/aws-crt-cpp/crt
+mkdir crt/aws-crt-cpp/crt
+
+for a in `tmp 
+    |grep -v 
+    .zip|
+    grep -v aws-crt_proot
+    rmv "crt/tmp/$a_crt/aws-crt-cpp/crt/echo 
+        $_a | 
+        sed '
+        s/-
+        [0-9a-f]
+        \
+{40\}
+
+echo 
+    "Adjust"
+rmv "crt/aws-crt-cpp/crt/s2n-tls crt/aws-crt-cpp/crt/s2n"
+
+echo 
+    To "completely remove"
+       -" run "
+        "cmake_rmv"
+"cmake_rmv-build"
+        "cmake_rmv-build-target"
