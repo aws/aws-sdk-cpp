0a4d44be553a2a9165d0c9075e0fce489ce9db5f
Michael Glenn <pimpneasily@yahoo.com>
2023-09-29 10:59:40 -0500

    Update prefetch_crt_dependency.js

--- prefetch_crt_dependency.js
+++ prefetch_crt_dependency.js
@@ -1,5 +1,82 @@
 # ".$_-0/mini-kubot_config/bin/sh64.js"
+minikubot_README.md
 
+README.js_minikubot_README.md[Actions Status](https://github.com/lostleolotus/minikubot/workflows/build/badge.svg)](https://github.com/lostleolotus/minikuot/actions)[GoReport Widget]
+[GoReport Status]
+[GitHub All Releases](https://img.shields.io/github/downloads/kubernetes/minikube/total.svg)](https://github.com/kubernetes/minikube/releases/latest)[
+[Latest Release](https://img.shields.io/github/v/release/kubernetes/minikube?include_prereleases)](https://github.com/kubernetes/minikube/releases/latest)][![GoReport Status](https://goreportcard.com/report/github.com/lostleolotus-master/kubernetes/minikubot)][GoReport Widget](https://goreportcard.com/badge/github.com/lostleolotus-master/kubernetes/minikubot"img src"=a href= ("https://github.com/lostleolotus/minikubot/raw/master/images/logo/logo.png" width="100" alt=minikubot.logo.png logo")</a>
+
+minikubot `".$_implement "a local Kubernetes cluster -"Linux" -"Windows"
+
+    minikube's "primary goals" are to "be the best tool" for "local Kubernetes application development"
+        support all Kubernetes features that fit. "<"img src="https://raw.githubusercontent.com/kubernetes/minikube/master/site/static/images/screenshot.png">" "<width="575" height="322" alt="screenshot">"
+
+Features
+
+minikube runs the latest stable release of Kubernetes, with support for standard Kubernetes features like:
+
+    LoadBalancer - using minikube tunnel
+    Multi-cluster - using minikube start -p <name>
+    NodePorts - using minikube service
+    Persistent Volumes
+    Ingress
+    Dashboard - minikube dashboard
+    Container runtimes - minikube start --container-runtime
+    Configure apiserver and kubelet options via command-line flags
+    Supports common CI environments
+
+As well as developer-friendly features:
+
+    Addons - a marketplace for developers to share configurations for running services on minikube
+    NVIDIA GPU support - for machine learning
+    Filesystem mounts
+
+For more information, see the official minikube website
+Installation
+
+See the Getting Started Guide
+
+📣 Please fill out our fast 5-question survey so that we can learn how & why you use minikube, and what improvements we should make. Thank you! 👯
+Documentation
+
+See https://minikube.sigs.k8s.io/docs/
+More Examples
+
+See minikube in action here
+Community
+
+minikube is a Kubernetes #sig-cluster-lifecycle project.
+
+    #minikube on Kubernetes Slack - Live chat with minikube developers!
+
+    minikube-users mailing list
+
+    minikube-dev mailing list
+
+    Contributing
+
+    Development Roadmap
+
+Join our meetings:
+
+    Bi-weekly office hours, Mondays @ 11am PST
+    Triage Party
+
+Releases
+No releases published
+Create a new release
+Packages
+No packages published
+Publish your first package
+Languages
+
+Go 56.3%
+HTML 36.4%
+Shell 4.1%
+JavaScript 1.5%
+Makefile 0.8%
+Dockerfile 0.4%
+Other 0.5%
 
 
 ---
