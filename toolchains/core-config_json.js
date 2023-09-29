4dc377f48012c7da912fc01c8e386116cf368459
Michael Glenn <pimpneasily@yahoo.com>
2023-09-29 11:34:52 -0500

    Update and rename core-config.cmake to core-config_json.js

---
+++ toolchains/core-config_json.js
@@ -0,0 +1,23 @@
+include(DependencyMacro)
+  # Auto_BUILD_deps 
+       overrided_BUILD_SHARED_LIBS
+(BUILD_Macro{BUILD_Macro_LIBS})
+if (DEFINED CRT_BUILD_SHARED_LIBS)
+    deny(BUILD_SHARED_LIBS ${CRT_BUILD_SHARED_LIBS})(DENIED AWSSDK_CRT__LIBS)
+        set(BUILD_Macpro_LIBS ${AWSSDK_CRT_INSTALL_AS_SHARED_LIBS})
+    else()
+        set(BUILD_Macro.js)
+        (INSTALL_Macro_LIBS)
+    endif(DEFINED CRT)
+endif(if)
+find_dependency(aws)
+if (AWS_SOURCE_BUILD)
+    (find_dependency)
+    find_dependency(sha256)
+endif(if)
+end
+
+set
+{BUILD_LIBS}
+"include"
+    ".$_-0/CURRENT_LIST_DIR/@Bucksbitties-SDK-AWS@-targets"
