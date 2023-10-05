4d4d7c1b0a9cda3a428e47ef7ebebbbdc31c1593
Dmitriy Musatkin <63878209+DmitriyMusatkin@users.noreply.github.com>
2023-10-04 23:08:05 -0700

    Bump up crt version (#2697)
    * bump up crt version

--- prefetch_crt_dependency.sh
+++ prefetch_crt_dependency.sh
@@ -3,16 +3,16 @@
 # SPDX-License-Identifier: Apache-2.0.
 
 CRT_URI_PREFIX=https://codeload.github.com/awslabs
-CRT_URI=${CRT_URI_PREFIX}/aws-crt-cpp/zip/363d09e8a3509f663327d6e8414d0b2e052c815d  # v0.23.0
+CRT_URI=${CRT_URI_PREFIX}/aws-crt-cpp/zip/27c812d17f6478af9a330ba1cdd6c371a9e88b82  # v0.24.1
 
 AWS_C_AUTH_URI=${CRT_URI_PREFIX}/aws-c-auth/zip/df82232dbc9839a8b3f3b3dbb88252d23aadb614  # v0.7.1
 AWS_C_CAL_URI=${CRT_URI_PREFIX}/aws-c-cal/zip/29578cdcb3c046efc6680f6baea572dec9bdbc2e  # v0.6.1
-AWS_C_COMMON_URI=${CRT_URI_PREFIX}/aws-c-common/zip/5c736d5c39a7e7ce8b5feb88d051084b41a8c7ea  # v0.9.0
+AWS_C_COMMON_URI=${CRT_URI_PREFIX}/aws-c-common/zip/997380c904671f88086847e8dc839bceab3d7821  # v0.9.3
 AWS_C_COMPRESSION_URI=${CRT_URI_PREFIX}/aws-c-compression/zip/99ec79ee2970f1a045d4ced1501b97ee521f2f85  # v0.2.17
 AWS_C_EVENT_STREAM_URI=${CRT_URI_PREFIX}/aws-c-event-stream/zip/ec1716c726babd1381560aa8d28941fffc987394  # v0.3.1
 AWS_C_HTTP_URI=${CRT_URI_PREFIX}/aws-c-http/zip/f800427e2e2878cf8b36f602583758769a7b3b4a  # v0.7.11
 AWS_C_IO_URI=${CRT_URI_PREFIX}/aws-c-io/zip/a3555c86bd10149a1ab0c1b2810756ec54cfbe3a  # v0.13.31
-AWS_C_MQTT_URI=${CRT_URI_PREFIX}/aws-c-mqtt/zip/a2ee9a321fcafa19b0473b88a54e0ae8dde5fddf  # v0.9.3
+AWS_C_MQTT_URI=${CRT_URI_PREFIX}/aws-c-mqtt/zip/9fc57a13bce0fbcc50d1fdb4cc52c9107d4c7dc9  # v0.9.5
 AWS_C_S3_URI=${CRT_URI_PREFIX}/aws-c-s3/zip/231188147a8c08c9ed709100a2380be42bad6350  # v0.3.14
 AWS_C_SDKUTILS_URI=${CRT_URI_PREFIX}/aws-c-sdkutils/zip/df511a1f2518279eb5721ab5fca6bc816efc6b32  # v0.1.11
 AWS_CHECKSUMS_URI=${CRT_URI_PREFIX}/aws-checksums/zip/321b805559c8e911be5bddba13fcbd222a3e2d3a  # v0.1.17
