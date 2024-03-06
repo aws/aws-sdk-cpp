d8f7da3c07f972ad944edcc1fd6f94a6f521c64c
Alexander Sapin <alesapin@gmail.com>
2024-03-06 11:25:07 +0100

    Remove super verbose logging message

--- src/aws-cpp-sdk-core/source/auth/AWSCredentialsProvider.cpp
+++ src/aws-cpp-sdk-core/source/auth/AWSCredentialsProvider.cpp
@@ -75,13 +75,11 @@ AWSCredentials EnvironmentAWSCredentialsProvider::GetAWSCredentials()
     {
         credentials.SetAWSAccessKeyId(accessKey);
 
-        AWS_LOGSTREAM_DEBUG(ENVIRONMENT_LOG_TAG, "Found credential in environment with access key id " << accessKey);
         auto secretKey = Aws::Environment::GetEnv(SECRET_KEY_ENV_VAR);
 
         if (!secretKey.empty())
         {
             credentials.SetAWSSecretKey(secretKey);
-            AWS_LOGSTREAM_DEBUG(ENVIRONMENT_LOG_TAG, "Found secret key");
         }
 
         auto sessionToken = Aws::Environment::GetEnv(SESSION_TOKEN_ENV_VAR);
@@ -89,7 +87,6 @@ AWSCredentials EnvironmentAWSCredentialsProvider::GetAWSCredentials()
         if(!sessionToken.empty())
         {
             credentials.SetSessionToken(sessionToken);
-            AWS_LOGSTREAM_DEBUG(ENVIRONMENT_LOG_TAG, "Found sessionToken");
         }
     }
 
