1d2e2cbe1afb5f1df3c5849a8d33748c7b967d4a
Fabian Wosar <fwosar@users.noreply.github.com>
2024-06-03 17:46:14 +0100

    Add fallback to std::getenv in Aws::Environment::GetEnv for Windows (#2982)

--- src/aws-cpp-sdk-core/source/platform/windows/Environment.cpp
+++ src/aws-cpp-sdk-core/source/platform/windows/Environment.cpp
@@ -16,9 +16,12 @@ namespace Environment
 /*
 using std::getenv generates a warning on windows so we use _dupenv_s instead.  The character array returned by this function is our responsibility to clean up, so rather than returning raw strings
 that would need to be manually freed in all the client functions, just copy it into a Aws::String instead, freeing it here.
+
+since _dupenv_s is a non-standard function, on non-Microsoft compilers we will fall back to using std::getenv instead.
 */
 Aws::String GetEnv(const char *variableName)
 {
+#ifdef _MSC_VER    
     char* variableValue = nullptr;
     std::size_t valueSize = 0;
     auto queryResult = _dupenv_s(&variableValue, &valueSize, variableName);
@@ -31,6 +34,10 @@ Aws::String GetEnv(const char *variableName)
     }
 
     return result;
+#else
+    auto variableValue = std::getenv(variableName);
+    return Aws::String( variableValue ? variableValue : "" );
+#endif
 }
 
 } // namespace Environment
