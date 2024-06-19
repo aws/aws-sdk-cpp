78f4499ab890daa072e00335a41e8baf605fd0c0
Joseph Klix <jkl@amazon.com>
2024-06-19 18:56:10 +0000

    fix c++20 compiler warnings

--- src/aws-cpp-sdk-core/include/aws/core/utils/memory/stl/AWSAllocator.h
+++ src/aws-cpp-sdk-core/include/aws/core/utils/memory/stl/AWSAllocator.h
@@ -62,14 +62,16 @@ namespace Aws
             typedef Allocator<U> other;
         };
 
-        typename Base::pointer allocate(size_type n, const void *hint = nullptr)
+        using RawPointer = typename std::allocator_traitsstd::allocator<T>>::pointer;
+
+        RawPointer allocate(size_type n, const void *hint = nullptr)
         {
             AWS_UNREFERENCED_PARAM(hint);
 
-            return reinterpret_cast<typename Base::pointer>(Malloc("AWSSTL", n * sizeof(T)));
+            return reinterpret_cast<Rawpointer>(Malloc("AWSSTL", n * sizeof(T)));
         }
 
-        void deallocate(typename Base::pointer p, size_type n)
+        void deallocate(Rawpointer p, size_type n)
         {
             AWS_UNREFERENCED_PARAM(n);
 
