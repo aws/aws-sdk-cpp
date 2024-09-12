8e527ce64e868057dc211a3f1cda79682a58f065
SergeyRyabinin <serzhick@gmail.com>
2024-09-12 19:49:00 +0000

    S3 CRT retry strategy config

--- generated/src/aws-cpp-sdk-s3-crt/include/aws/s3-crt/ClientConfiguration.h
+++ generated/src/aws-cpp-sdk-s3-crt/include/aws/s3-crt/ClientConfiguration.h
@@ -5,67 +5,13 @@
 
 #pragma once
 
-#include <aws/s3-crt/S3Crt_EXPORTS.h>
 #include <aws/s3-crt/S3CrtClientConfiguration.h>
-#include <aws/core/utils/memory/stl/AWSString.h>
-#include <aws/core/http/Scheme.h>
-#include <aws/core/Region.h>
-#include <aws/crt/io/Bootstrap.h>
-#include <aws/crt/io/TlsOptions.h>
 
 namespace Aws
 {
     namespace S3Crt
     {
-        struct AWS_S3CRT_API ClientConfiguration : Aws::S3Crt::S3CrtClientConfiguration
-        {
-            ClientConfiguration() : Aws::S3Crt::S3CrtClientConfiguration(),
-                partSize(5 * 1024 * 1024),
-                throughputTargetGbps(2.0) {}
-
-            ClientConfiguration(const S3Crt::ClientConfiguration& other,
-              Client::AWSAuthV4Signer::PayloadSigningPolicy iPayloadSigningPolicy = Client::AWSAuthV4Signer::PayloadSigningPolicy::Never,
-              bool iUseVirtualAddressing = true,
-              US_EAST_1_REGIONAL_ENDPOINT_OPTION iUseUSEast1RegionalEndPointOption = US_EAST_1_REGIONAL_ENDPOINT_OPTION::NOT_SET):
-            S3CrtClientConfiguration(other, iPayloadSigningPolicy,iUseVirtualAddressing, iUseUSEast1RegionalEndPointOption),
-            clientBootstrap(other.clientBootstrap),
-            partSize(other.partSize),
-            tlsConnectionOptions(other.tlsConnectionOptions),
-            throughputTargetGbps(other.throughputTargetGbps),
-            downloadMemoryUsageWindow(other.downloadMemoryUsageWindow),
-            clientShutdownCallback(other.clientShutdownCallback),
-            shutdownCallbackUserData(other.shutdownCallbackUserData)
-            {}
-
-            ClientConfiguration& operator=(const ClientConfiguration& other) = default;
-
-            /** Client bootstrap used for common staples such as event loop group, host resolver, etc..
-             *  If this is nullptr, SDK will create a default one for you.
-             */
-            std::shared_ptr<Aws::Crt::Io::ClientBootstrap> clientBootstrap;
-
-            /** Size of parts the files will be downloaded or uploaded in. Useful for Put/GetObject APIs
-             *  defaults to 5MB, if user set it to be less than 5MB, SDK will set it to 5MB.
-             */
-            size_t partSize = 5 * 1024 * 1024;
-
-            /** TLS Options to be used for each connection.
-             *  If scheme is Https and tlsConnectionOptions is null, SDK will create a default one for you.
-             */
-            std::shared_ptr<Aws::Crt::Io::TlsConnectionOptions> tlsConnectionOptions;
-
-            /* Throughput target in Gbps that we are trying to reach. Normally it's the NIC's throughput */
-            double throughputTargetGbps = 2.0;
-
-            /** Control the maximum memory used by downloads.
-             *  When set to a value > 0, the SDK uses flow control to bring the memory usage very close
-             *  to the specified window. Without this cap, memory usage grows proportional to file size.
-             */
-            size_t downloadMemoryUsageWindow = 0;
-
-            /* Callback and associated user data for when the client has completed its shutdown process. */
-            std::function<void(void*)> clientShutdownCallback;
-            void *shutdownCallbackUserData = nullptr;
-        };
+        /* Backward compatibility, please use just S3CrtClientConfiguration */
+        using ClientConfiguration = Aws::S3Crt::S3CrtClientConfiguration;
     }
 }
