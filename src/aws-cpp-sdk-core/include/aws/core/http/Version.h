50958a4a21335b809d1b391e4b7037fcc13d5a0d
Joseph Klix <jkl@amazon.com>
2024-09-26 14:51:04 -0700

    handle windows enum conflict

--- src/aws-cpp-sdk-core/include/aws/core/http/Version.h
+++ src/aws-cpp-sdk-core/include/aws/core/http/Version.h
@@ -1,5 +1,5 @@
 /**
- * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0.
  */
 
@@ -10,6 +10,15 @@ namespace Aws {
         /**
          * Enum to represent version of the http protocol to use
          */
+
+#ifdef _WIN32
+#pragma push_macro("WIN_HTTP_VERSION")
+#undef HTTP_VERSION_1_0
+#undef HTTP_VERSION_1_1
+#undef HTTP_VERSION_2_0
+
+#pragma pop_macro("WIN_HTTP_VERSION")
+#endif //#ifdef _WIN32
         enum class Version {
             HTTP_VERSION_NONE,
             HTTP_VERSION_1_0,
