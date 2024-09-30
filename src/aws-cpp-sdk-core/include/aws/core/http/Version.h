6f321e0acfd7ca119333514187189d74d7f0b163
Joseph Klix <jkl@amazon.com>
2024-09-30 09:35:44 -0700

    Fix windows enums conflict  (#3129)
    * handle windows enum conflict
    
    * fix spacing typo

--- src/aws-cpp-sdk-core/include/aws/core/http/Version.h
+++ src/aws-cpp-sdk-core/include/aws/core/http/Version.h
@@ -10,6 +10,15 @@ namespace Aws {
         /**
          * Enum to represent version of the http protocol to use
          */
+
+#ifdef _WIN32
+#pragma push_macro("WIN_HTTP_VERSION")
+#undef HTTP_VERSION_1_0
+#undef HTTP_VERSION_1_1
+#undef HTTP_VERSION_2_0
+
+#pragma pop_macro("WIN_HTTP_VERSION")
+#endif //#ifdef _WIN32
         enum class Version {
             HTTP_VERSION_NONE,
             HTTP_VERSION_1_0,
