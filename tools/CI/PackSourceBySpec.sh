932990ea020e49542181cd7499960f77334d026b
sbiscigl <sbiscigl@amazon.com>
2024-10-23 13:24:46 -0400

    remove old unused CI scripts

--- tools/CI/PackSourceBySpec.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-branch =$(python aws-sdk-cpp/CI/ExtractBuildArgs.py branch)
-git clone git@github.com:awslabs/aws-sdk-cpp-staging.git aws-sdk-cpp
-cd aws-sdk-cpp
-git reset --hard HEAD
-git checkout master
-git pull
-git checkout $branch
