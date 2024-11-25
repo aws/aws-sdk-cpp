17336163b3cc99032fddddf14f5784c0b84897b2
Soumava Bera <sberas@amazon.com>
2024-11-25 10:01:00 -0500

    restore file

--- src/aws-cpp-sdk-core/source/internal/AWSHttpResourceClient.cpp
+++ src/aws-cpp-sdk-core/source/internal/AWSHttpResourceClient.cpp
@@ -602,33 +602,41 @@ namespace Aws
         SSOCredentialsClient::SSOCredentialsClient(const Aws::Client::ClientConfiguration& clientConfiguration, Aws::Http::Scheme scheme, const Aws::String& region)
                 : AWSHttpResourceClient(clientConfiguration, SSO_RESOURCE_CLIENT_LOG_TAG)
         {
-          (Aws::MakeUnique<Aws::Client::JsonErrorMarshaller>(SSO_RESOURCE_CLIENT_LOG_TAG));
+            SetErrorMarshaller(Aws::MakeUnique<Aws::Client::JsonErrorMarshaller>(SSO_RESOURCE_CLIENT_LOG_TAG));
 
-          m_endpoint = buildEndpoint(scheme, region, "portal.sso.", "federation/credentials");
-          m_oidcEndpoint = buildEndpoint(scheme, region, "oidc.", "token");
+            m_endpoint = buildEndpoint(scheme, region, "portal.sso.", "federation/credentials");
+            m_oidcEndpoint = buildEndpoint(scheme, region, "oidc.", "token");
 
-          AWS_LOGSTREAM_INFO(SSO_RESOURCE_CLIENT_LOG_TAG, "Creating SSO ResourceClient with endpoint: " << m_endpoint);
+            AWS_LOGSTREAM_INFO(SSO_RESOURCE_CLIENT_LOG_TAG, "Creating SSO ResourceClient with endpoint: " << m_endpoint);
         }
 
-        Aws::String SSOCredentialsClient::buildEndpoint(Aws::Http::Scheme scheme, const Aws::String& region, const Aws::String& domain,
-                                                        const Aws::String& endpoint) {
-          Aws::StringStream ss;
-          if (scheme == Aws::Http::Scheme::HTTP) {
-            ss << "http://";
-          } else {
-            ss << "https://";
-          }
+        Aws::String SSOCredentialsClient::buildEndpoint(
+            Aws::Http::Scheme scheme,
+            const Aws::String& region,
+            const Aws::String& domain,
+            const Aws::String& endpoint)
+        {
+            Aws::StringStream ss;
+            if (scheme == Aws::Http::Scheme::HTTP)
+            {
+                ss << "http://";
+            }
+            else
+            {
+                ss << "https://";
+            }
 
-          static const int CN_NORTH_1_HASH = Aws::Utils::HashingUtils::HashString(Aws::Region::CN_NORTH_1);
-          static const int CN_NORTHWEST_1_HASH = Aws::Utils::HashingUtils::HashString(Aws::Region::CN_NORTHWEST_1);
-          auto hash = Aws::Utils::HashingUtils::HashString(region.c_str());
+            static const int CN_NORTH_1_HASH = Aws::Utils::HashingUtils::HashString(Aws::Region::CN_NORTH_1);
+            static const int CN_NORTHWEST_1_HASH = Aws::Utils::HashingUtils::HashString(Aws::Region::CN_NORTHWEST_1);
+            auto hash = Aws::Utils::HashingUtils::HashString(region.c_str());
 
-          AWS_LOGSTREAM_DEBUG(SSO_RESOURCE_CLIENT_LOG_TAG, "Preparing SSO client for region: " << region);
-          ss << domain << region << ".amazonaws.com/" << endpoint;
-          if (hash == CN_NORTH_1_HASH || hash == CN_NORTHWEST_1_HASH) {
-            ss << ".cn";
-          }
-          return ss.str();
+            AWS_LOGSTREAM_DEBUG(SSO_RESOURCE_CLIENT_LOG_TAG, "Preparing SSO client for region: " << region);
+            ss << domain << region << ".amazonaws.com/" << endpoint;
+            if (hash == CN_NORTH_1_HASH || hash == CN_NORTHWEST_1_HASH)
+            {
+                ss << ".cn";
+            }
+            return ss.str();
         }
 
         SSOCredentialsClient::SSOGetRoleCredentialsResult SSOCredentialsClient::GetSSOCredentials(const SSOGetRoleCredentialsRequest &request)
