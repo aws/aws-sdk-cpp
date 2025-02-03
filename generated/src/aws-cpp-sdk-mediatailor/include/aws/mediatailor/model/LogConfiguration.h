918b5a9b091969cc1f39108bcea236c414b4150f
aws-sdk-cpp-automation <github-aws-sdk-cpp-automation@amazon.com>
2025-02-03 19:14:26 +0000

    Add support for CloudWatch Vended Logs which allows for delivery of customer logs to CloudWatch Logs, S3, or Firehose.

--- generated/src/aws-cpp-sdk-mediatailor/include/aws/mediatailor/model/LogConfiguration.h
+++ generated/src/aws-cpp-sdk-mediatailor/include/aws/mediatailor/model/LogConfiguration.h
@@ -5,6 +5,9 @@
 
 #pragma once
 #include <aws/mediatailor/MediaTailor_EXPORTS.h>
+#include <aws/core/utils/memory/stl/AWSVector.h>
+#include <aws/mediatailor/model/LoggingStrategy.h>
+#include <utility>
 
 namespace Aws
 {
@@ -52,10 +55,32 @@ namespace Model
     inline void SetPercentEnabled(int value) { m_percentEnabledHasBeenSet = true; m_percentEnabled = value; }
     inline LogConfiguration& WithPercentEnabled(int value) { SetPercentEnabled(value); return *this;}
     ///@}
+
+    ///@{
+    /**
+     * <p>The method used for collecting logs from AWS Elemental MediaTailor.
+     * <code>LEGACY_CLOUDWATCH</code> indicates that MediaTailor is sending logs
+     * directly to Amazon CloudWatch Logs. <code>VENDED_LOGS</code> indicates that
+     * MediaTailor is sending logs to CloudWatch, which then vends the logs to your
+     * destination of choice. Supported destinations are CloudWatch Logs log group,
+     * Amazon S3 bucket, and Amazon Data Firehose stream. </p>
+     */
+    inline const Aws::Vector<LoggingStrategy>& GetEnabledLoggingStrategies() const{ return m_enabledLoggingStrategies; }
+    inline bool EnabledLoggingStrategiesHasBeenSet() const { return m_enabledLoggingStrategiesHasBeenSet; }
+    inline void SetEnabledLoggingStrategies(const Aws::Vector<LoggingStrategy>& value) { m_enabledLoggingStrategiesHasBeenSet = true; m_enabledLoggingStrategies = value; }
+    inline void SetEnabledLoggingStrategies(Aws::Vector<LoggingStrategy>&& value) { m_enabledLoggingStrategiesHasBeenSet = true; m_enabledLoggingStrategies = std::move(value); }
+    inline LogConfiguration& WithEnabledLoggingStrategies(const Aws::Vector<LoggingStrategy>& value) { SetEnabledLoggingStrategies(value); return *this;}
+    inline LogConfiguration& WithEnabledLoggingStrategies(Aws::Vector<LoggingStrategy>&& value) { SetEnabledLoggingStrategies(std::move(value)); return *this;}
+    inline LogConfiguration& AddEnabledLoggingStrategies(const LoggingStrategy& value) { m_enabledLoggingStrategiesHasBeenSet = true; m_enabledLoggingStrategies.push_back(value); return *this; }
+    inline LogConfiguration& AddEnabledLoggingStrategies(LoggingStrategy&& value) { m_enabledLoggingStrategiesHasBeenSet = true; m_enabledLoggingStrategies.push_back(std::move(value)); return *this; }
+    ///@}
   private:
 
     int m_percentEnabled;
     bool m_percentEnabledHasBeenSet = false;
+
+    Aws::Vector<LoggingStrategy> m_enabledLoggingStrategies;
+    bool m_enabledLoggingStrategiesHasBeenSet = false;
   };
 
 } // namespace Model
---
+++ generated/src/aws-cpp-sdk-mediatailor/include/aws/mediatailor/model/LoggingStrategy.h
@@ -0,0 +1,31 @@
+﻿/**
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+#pragma once
+#include <aws/mediatailor/MediaTailor_EXPORTS.h>
+#include <aws/core/utils/memory/stl/AWSString.h>
+
+namespace Aws
+{
+namespace MediaTailor
+{
+namespace Model
+{
+  enum class LoggingStrategy
+  {
+    NOT_SET,
+    VENDED_LOGS,
+    LEGACY_CLOUDWATCH
+  };
+
+namespace LoggingStrategyMapper
+{
+AWS_MEDIATAILOR_API LoggingStrategy GetLoggingStrategyForName(const Aws::String& name);
+
+AWS_MEDIATAILOR_API Aws::String GetNameForLoggingStrategy(LoggingStrategy value);
+} // namespace LoggingStrategyMapper
+} // namespace Model
+} // namespace MediaTailor
+} // namespace Aws
