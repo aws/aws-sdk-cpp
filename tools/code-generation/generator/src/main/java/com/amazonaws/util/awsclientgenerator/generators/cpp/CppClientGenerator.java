f0836b1a44adc43010869aa67069696462233cb4
SergeyRyabinin <serzhick@gmail.com>
2025-02-04 21:02:12 +0000

    Protocol test build fixes: timestamp unix format, enum handling, nested structures deserialization, payload streaming selection

--- tools/code-generation/generator/src/main/java/com/amazonaws/util/awsclientgenerator/generators/cpp/CppClientGenerator.java
+++ tools/code-generation/generator/src/main/java/com/amazonaws/util/awsclientgenerator/generators/cpp/CppClientGenerator.java
@@ -59,6 +59,7 @@ public abstract class CppClientGenerator implements ClientGenerator {
         velocityEngine.setProperty("resource.loader.classpath.class", ClasspathResourceLoader.class.getName());
         velocityEngine.addProperty(RuntimeConstants.RUNTIME_LOG_INSTANCE, new NOPLoggerFactory().getLogger(""));
         velocityEngine.setProperty("context.scope_control.template", true);
+        velocityEngine.setProperty("context.scope_control.macro", true);
         // Migration from 1.7 to 2.3:: https://velocity.apache.org/engine/2.3/upgrading.html
         // # Use backward compatible space gobbling
         velocityEngine.setProperty(RuntimeConstants.SPACE_GOBBLING, RuntimeConstants.SpaceGobbling.BC.toString());
