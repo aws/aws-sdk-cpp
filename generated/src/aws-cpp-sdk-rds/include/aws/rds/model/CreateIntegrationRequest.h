56fa92e28e979f8ea7472c1578f53efb39a5bcaf
aws-sdk-cpp-automation <github-aws-sdk-cpp-automation@amazon.com>
2025-02-05 19:13:56 +0000

    Documentation updates to clarify the description for the parameter AllocatedStorage for the DB cluster data type, the description for the parameter DeleteAutomatedBackups for the DeleteDBCluster API operation, and removing an outdated note for the CreateDBParameterGroup API operation.

--- generated/src/aws-cpp-sdk-rds/include/aws/rds/model/CreateIntegrationRequest.h
+++ generated/src/aws-cpp-sdk-rds/include/aws/rds/model/CreateIntegrationRequest.h
@@ -139,8 +139,9 @@ namespace Model
     /**
      * <p>Data filtering options for the integration. For more information, see <a
      * href="https://docs.aws.amazon.com/AmazonRDS/latest/AuroraUserGuide/zero-etl.filtering.html">Data
-     * filtering for Aurora zero-ETL integrations with Amazon Redshift</a>. </p>
-     * <p>Valid for: Integrations with Aurora MySQL source DB clusters only</p>
+     * filtering for Aurora zero-ETL integrations with Amazon Redshift</a> or <a
+     * href="https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/zero-etl.filtering.html">Data
+     * filtering for Amazon RDS zero-ETL integrations with Amazon Redshift</a>. </p>
      */
     inline const Aws::String& GetDataFilter() const{ return m_dataFilter; }
     inline bool DataFilterHasBeenSet() const { return m_dataFilterHasBeenSet; }
