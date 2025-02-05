6980bb06de6a7cff600d8e4de880d2bf43c239f2
aws-sdk-cpp-automation <github-aws-sdk-cpp-automation@amazon.com>
2025-02-05 19:13:56 +0000

    merges

--- generated/src/aws-cpp-sdk-rds/include/aws/rds/RDSClient.h
+++ generated/src/aws-cpp-sdk-rds/include/aws/rds/RDSClient.h
@@ -847,18 +847,8 @@ namespace Aws
          * instance using <code>ModifyDBInstance</code>. When you associate a new DB
          * parameter group with a running DB instance, you need to reboot the DB instance
          * without failover for the new DB parameter group and associated settings to take
-         * effect.</p> <p>This command doesn't apply to RDS Custom.</p> 
-         * <p>After you create a DB parameter group, you should wait at least 5 minutes
-         * before creating your first DB instance that uses that DB parameter group as the
-         * default parameter group. This allows Amazon RDS to fully complete the create
-         * action before the parameter group is used as the default for a new DB instance.
-         * This is especially important for parameters that are critical when creating the
-         * default database for a DB instance, such as the character set for the default
-         * database defined by the <code>character_set_database</code> parameter. You can
-         * use the <i>Parameter Groups</i> option of the <a
-         * href="https://console.aws.amazon.com/rds/">Amazon RDS console</a> or the
-         * <i>DescribeDBParameters</i> command to verify that your DB parameter group has
-         * been created or modified.</p> <p><h3>See Also:</h3>   <a
+         * effect.</p> <p>This command doesn't apply to RDS Custom.</p><p><h3>See
+         * Also:</h3>   <a
          * href="http://docs.aws.amazon.com/goto/WebAPI/rds-2014-10-31/CreateDBParameterGroup">AWS
          * API Reference</a></p>
          */
@@ -4075,10 +4065,8 @@ namespace Aws
         }
 
         /**
-         * <p>Modifies a zero-ETL integration with Amazon Redshift.</p> 
-         * <p>Currently, you can only modify integrations that have Aurora MySQL source DB
-         * clusters. Integrations with Aurora PostgreSQL and RDS sources currently don't
-         * support modifying the integration.</p> <p><h3>See Also:</h3>   <a
+         * <p>Modifies a zero-ETL integration with Amazon Redshift.</p><p><h3>See
+         * Also:</h3>   <a
          * href="http://docs.aws.amazon.com/goto/WebAPI/rds-2014-10-31/ModifyIntegration">AWS
          * API Reference</a></p>
          */
