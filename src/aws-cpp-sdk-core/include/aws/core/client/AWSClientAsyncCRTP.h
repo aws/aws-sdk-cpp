aws/aws-sdk-cpp

commit c9e5d601631e6403d652a87798b790b827332dcc

Peek stream at the end of the event streaming to sync concurrent backbuffer (#3302)

src/aws-cpp-sdk-core/source/http/curl/CurlHttpClient.cpp
@@ -411,37 +411,32 @@ int CurlHttpClient::CurlProgressCallback(void *userdata, double, double, double,
     CurlReadCallbackContext* context = reinterpret_cast<CurlReadCallbackContext*>(userdata);
 
     const std::shared_ptr<Aws::IOStream>& ioStream = context->m_request->GetContentBody();
-    if (ioStream->eof())
-    {
-        curl_easy_pause(context->m_curlHandle, CURLPAUSE_CONT);
-        return 0;
+    if (!ioStream || ioStream->bad()) {
+      AWS_LOGSTREAM_ERROR(CURL_HTTP_CLIENT_TAG, "Input stream is bad!");
+      return 1;  // libcurl abort the transfer and return CURLE_ABORTED_BY_CALLBACK.
     }
 
-    if (context->m_client->m_perfMode == TransferLibPerformanceMode::LOW_LATENCY)
-    {
-        // forcing "underflow" on the IOStream with ConcurrentStreamBuf to move data from back buffer to put area
-        int peekVal = ioStream->peek();
-        AWS_UNREFERENCED_PARAM(peekVal);
-
-        // forcing curl to try to ReadBody again (~to poll body IOStream for HTTP2)
-        // This is a spin pause-unpause in case of no data provided by a customer callback
-        // But otherwise curl will slow down the transfer and start calling as at frequency of 1s
-        //   see https://curl.se/mail/lib-2020-07/0046.html
-        // we should use multi handle or another HTTP client in the future to avoid this
-        curl_easy_pause(context->m_curlHandle, CURLPAUSE_CONT);
+    const int peekVal = ioStream->peek();
+    if (ioStream->eof() && peekVal == std::char_traits<char>::eof()) {
+      // curl won't call ReadBody after the last ReadBody call returns 0
+      // however, this Progress method is still called few times for incoming data.
+      return 0;
     }
-    else
-    {
-        char output[1];
-        if (ioStream->readsome(output, 1) > 0)
-        {
-            ioStream->unget();
-            if (!ioStream->good())
-            {
-                AWS_LOGSTREAM_WARN(CURL_HTTP_CLIENT_TAG, "Input stream failed to perform unget().");
-            }
-            curl_easy_pause(context->m_curlHandle, CURLPAUSE_CONT);
-        }
+
+    if (context->m_client->m_perfMode == TransferLibPerformanceMode::LOW_LATENCY) {
+      AWS_UNREFERENCED_PARAM(peekVal);
+      // forcing curl to try to ReadBody again (~to poll body IOStream for HTTP2)
+      // This is a spin pause-unpause in case of no data provided by a customer callback
+      // But otherwise curl will slow down the transfer and start calling as at frequency of 1s
+      //   see https://curl.se/mail/lib-2020-07/0046.html
+      // we should use multi handle or another HTTP client in the future to avoid this
+      curl_easy_pause(context->m_curlHandle, CURLPAUSE_CONT);
+    } else {
+      if (peekVal != std::char_traits<char>::eof()) {
+        curl_easy_pause(context->m_curlHandle, CURLPAUSE_CONT);
+      } else {
+        curl_easy_pause(context->m_curlHandle, CURLPAUSE_SEND);
+      }
     }
 
     return 0;

tests/aws-cpp-sdk-transcribestreaming-integ-tests/TranscribeErrorCaseTests.cpp
@@ -1,0 +1,157 @@
+﻿/**
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+// WinHTTP SDK client does not work with streaming
+#if defined(HAVE_H2_CLIENT) && !defined(_WIN32)
+#include <aws/core/Aws.h>
+#include <aws/testing/AwsCppSdkGTestSuite.h>
+#include <aws/testing/AwsTestHelpers.h>
+#include <aws/transcribestreaming/TranscribeStreamingServiceClient.h>
+#include <aws/transcribestreaming/model/StartStreamTranscriptionHandler.h>
+#include <aws/transcribestreaming/model/StartStreamTranscriptionRequest.h>
+
+#include <fstream>
+
+using namespace Aws;
+using namespace Aws::Utils;
+using namespace Aws::TranscribeStreamingService;
+using namespace Aws::TranscribeStreamingService::Model;
+
+static const char ALLOC_TAG[] = "TranscribeStreamingErrorTests";
+static const char TEST_FILE_NAME[] = "transcribe-test-file.wav";
+
+#define AWS_ADD_FAILURE(MSG) \
+  ADD_FAILURE() << MSG;      \
+  TestTraceFailure(MSG);
+
+class TranscribeStreamingErrorTests : public Aws::Testing::AwsCppSdkGTestSuite {
+ public:
+  TranscribeStreamingErrorTests() = default;
+
+ protected:
+  Aws::String m_testTraces;
+  void TestTrace(Aws::String msg) { m_testTraces += Aws::Utils::DateTime::Now().ToGmtStringWithMs() + " " + std::move(msg) + "\n"; };
+  void TestTraceFailure(Aws::String msg) {
+    m_testTraces += "[FAILURE] " + Aws::Utils::DateTime::Now().ToGmtStringWithMs() + " " + std::move(msg) + "\n";
+  };
+  void SetUp() {}
+  void TearDown() {
+    if (::testing::Test::HasFailure()) {
+      std::cout << "Test traces: " << m_testTraces << "\n";
+    }
+    m_testTraces.erase();
+  }
+};
+
+TEST_F(TranscribeStreamingErrorTests, TranscribeAudioFile) {
+  Aws::Client::ClientConfigurationInitValues cfgInit;
+  cfgInit.shouldDisableIMDS = true;
+  Aws::Client::ClientConfiguration config(cfgInit);
+  config.httpLibPerfMode = Http::TransferLibPerformanceMode::REGULAR;
+  config.enableHttpClientTrace = true;
+  Aws::UniquePtr<TranscribeStreamingServiceClient> client = Aws::MakeUnique<TranscribeStreamingServiceClient>(ALLOC_TAG, config);
+
+  Aws::String transcribedResult;
+  StartStreamTranscriptionHandler handler;
+  handler.SetTranscriptEventCallback([&transcribedResult](const TranscriptEvent& ev) {
+    const auto& results = ev.GetTranscript().GetResults();
+    if (results.empty()) {
+      return;
+    }
+    const auto& last = results.back();
+    const auto& alternatives = last.GetAlternatives();
+    if (alternatives.empty()) {
+      return;
+    }
+    transcribedResult = alternatives.back().GetTranscript();
+  });
+
+  Aws::String operationRequestId;
+  handler.SetInitialResponseCallback([&](const StartStreamTranscriptionInitialResponse& initialResponse) {
+    operationRequestId = initialResponse.GetRequestId();
+    if (operationRequestId.empty()) {
+      AWS_ADD_FAILURE("InitialResponseCallback is called but received empty RequestId");
+      TestTrace(Aws::String("initialResponse was: ") + initialResponse.Jsonize().View().AsString());
+    }
+    TestTrace(Aws::String("InitialResponse aws RequestId: ") + operationRequestId);
+    TestTrace(Aws::String("InitialResponse transcribe SessionId: ") + initialResponse.GetSessionId());
+  });
+  handler.SetOnErrorCallback([&transcribedResult, this](const Aws::Client::AWSError<TranscribeStreamingServiceErrors>& errors) {
+    // we will receive an error because the request was abruptly shutdown (via stream.Close()).
+    // However, we cannot delay the call to closing the stream, because HTTP clients such as libcurl buffer the
+    // requests before sending them over the wire, so it will keep BUFFER_SIZE bytes in its memory if we don't
+    // signal the completion of the stream (by closing it).
+    // To discern between that case and a true error, we check if we have received any text back from the service.
+    if (transcribedResult.empty()) {
+      AWS_ADD_FAILURE(Aws::String("Received error: ") + errors.GetExceptionName() + ". with msg: " + errors.GetMessage())
+    }
+  });
+
+  StartStreamTranscriptionRequest request;
+  request.SetMediaSampleRateHertz(8000);
+  request.SetLanguageCode(LanguageCode::en_US);
+  request.SetMediaEncoding(MediaEncoding::pcm);
+  request.SetEventStreamHandler(handler);
+
+  auto OnStreamReady = [this](AudioStream& stream) {
+    TestTrace(Aws::String("OnStreamReady"));
+
+    for (size_t i = 0; i < 2; ++i) {
+      Aws::FStream file(TEST_FILE_NAME, std::ios_base::in | std::ios_base::binary);
+      ASSERT_TRUE(file);
+      char buf[2048];
+      while (file) {
+        file.read(buf, sizeof(buf));
+        Aws::Vector<unsigned char> bits{buf, buf + file.gcount()};
+        AudioEvent event(std::move(bits));
+        if (!stream) {
+          break;
+        }
+        TestTrace(Aws::String("Writing good event"));
+        if (!stream.WriteAudioEvent(event)) {
+          AWS_ADD_FAILURE("Failed to write an audio event");
+          break;
+        }
+      }
+      if (i == 0) {
+        TestTrace(Aws::String("Writing large bad event"));
+        Aws::Utils::Event::Message msg;
+        msg.InsertEventHeader(":message-type", Aws::String("event"));
+        msg.InsertEventHeader(":event-type", Aws::String("AudioEvent"));
+        msg.InsertEventHeader(":content-type", Aws::String("application/octet-stream"));
+        msg.WriteEventPayload([]() {
+          Aws::String payload;
+          for (size_t j = 0; j < 3000; ++j) {
+            payload += "very bad event ";
+          }
+          return payload;
+        }());
+        stream.WriteEvent(msg);
+        if (!stream) {
+          AWS_ADD_FAILURE("Failed to write a bad event");
+          break;
+        }
+      }
+    }
+
+    TestTrace(Aws::String("Sending final empty frame"));
+    stream.WriteAudioEvent({});
+    TestTrace(Aws::String("Flushing and closing the stream"));
+    stream.flush();
+    stream.Close();
+  };
+
+  Aws::Utils::Threading::Semaphore semaphore(0, 1);
+  auto OnResponseCallback = [&semaphore](const TranscribeStreamingServiceClient*, const StartStreamTranscriptionRequest&,
+                                         const StartStreamTranscriptionOutcome&,
+                                         const std::shared_ptr<const Aws::Client::AsyncCallerContext>&) { semaphore.ReleaseAll(); };
+
+  client->StartStreamTranscriptionAsync(request, OnStreamReady, OnResponseCallback, nullptr /*context*/);
+  semaphore.WaitOne();
+
+  EXPECT_FALSE(operationRequestId.empty()) << "Did not receive a request id for the StartStreamTranscription";
+}
+
+#endif

tests/aws-cpp-sdk-transcribestreaming-integ-tests/CMakeLists.txt
@@ -4,7 +4,7 @@ add_project(aws-cpp-sdk-transcribestreaming-integ-tests
     testing-resources
     aws-cpp-sdk-core)
 
-set(AWS_TRANSCRIBESTREAMING_INTEGRATION_TESTS_SRC "RunTests.cpp" "TranscribeTests.cpp")
+set(AWS_TRANSCRIBESTREAMING_INTEGRATION_TESTS_SRC "RunTests.cpp" "TranscribeTests.cpp" "TranscribeErrorCaseTests.cpp")
 
 add_definitions(-DRESOURCES_DIR="${CMAKE_CURRENT_SOURCE_DIR}/resources")
 

tests/aws-cpp-sdk-transcribestreaming-integ-tests/RunTests.cpp
@@ -3,18 +3,58 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 
-#include <gtest/gtest.h>
+#include <aws/core/Aws.h>
+#include <aws/core/internal/AWSHttpResourceClient.h>
+#include <aws/core/platform/FileSystem.h>
 #include <aws/testing/TestingEnvironment.h>
+#include <gtest/gtest.h>
 
+#include <fstream>
 
-int main(int argc, char** argv)
-{
-    Aws::Testing::SetDefaultSigPipeHandler();
+void DownloadTestStreams() {
+  Aws::SDKOptions options = {};
+  options.httpOptions.installSigPipeHandler = true;
+  Aws::InitAPI(options);
+  {
+    Aws::Client::ClientConfigurationInitValues cfgInit;
+    cfgInit.shouldDisableIMDS = true;
+    Aws::Internal::AWSHttpResourceClient httpclient(cfgInit);
+    const Aws::Vector<Aws::String> TEST_FILE_NAMES = {"transcribe-test-file.wav", "this_is_a_cpp_test_sample_8kHz_2162ms.wav",
+                                                      "Kant_16kHz_17176ms.wav"};
+    for (const auto& toDownload : TEST_FILE_NAMES) {
+      Aws::String path = "aws-sdk-cpp-builds-sdks-team/test-resources/" + toDownload;
+      auto payload = httpclient.GetResource("https://s3.amazonaws.com/" /*endpoint*/, path.c_str(), "" /*authToken*/);
+      Aws::OFStream testFile(toDownload.c_str(), std::ios_base::out | std::ios_base::binary | std::ios_base::trunc);
+      testFile.write(payload.c_str(), payload.size());
+      testFile.close();
+    }
+  }
+  Aws::ShutdownAPI(options);
+}
 
-    Aws::Testing::ParseArgs(argc, argv);
+void Cleanup() {
+  Aws::SDKOptions options = {};
+  options.httpOptions.installSigPipeHandler = true;
+  Aws::InitAPI(options);
+  {
+    const Aws::Vector<Aws::String> TEST_FILE_NAMES = {"transcribe-test-file.wav", "this_is_a_cpp_test_sample_8kHz_2162ms.wav",
+                                                      "Kant_16kHz_17176ms.wav"};
+    for (const auto& testFilename : TEST_FILE_NAMES) {
+      Aws::FileSystem::RemoveFileIfExists(testFilename.c_str());
+    }
+    Aws::ShutdownAPI(options);
+  }
+}
 
-    ::testing::InitGoogleTest(&argc, argv);
-    int exitCode = RUN_ALL_TESTS();
+int main(int argc, char** argv) {
+  Aws::Testing::SetDefaultSigPipeHandler();
 
-    return exitCode;
+  Aws::Testing::ParseArgs(argc, argv);
+
+  ::testing::InitGoogleTest(&argc, argv);
+  DownloadTestStreams();
+  int exitCode = RUN_ALL_TESTS();
+  Cleanup();
+
+  return exitCode;
 }

tests/aws-cpp-sdk-transcribestreaming-integ-tests/TranscribeTests.cpp
@@ -42,19 +42,6 @@ public:
         Aws::Client::ClientConfigurationInitValues cfgInit;
         cfgInit.shouldDisableIMDS = true;
         Aws::Client::ClientConfiguration config(cfgInit);
-
-        Aws::Internal::AWSHttpResourceClient httpclient(cfgInit);
-        const Aws::Vector<Aws::String> TEST_FILE_NAMES = {"transcribe-test-file.wav", "this_is_a_cpp_test_sample_8kHz_2162ms.wav", "Kant_16kHz_17176ms.wav"};
-        for(const auto& toDownload : TEST_FILE_NAMES)
-        {
-            Aws::String path = "aws-sdk-cpp-builds-sdks-team/test-resources/" + toDownload;
-            auto payload = httpclient.GetResource("https://s3.amazonaws.com/" /*endpoint*/,
-                                                  path.c_str(), "" /*authToken*/);
-            Aws::OFStream testFile(toDownload.c_str(), std::ios_base::out | std::ios_base::binary | std::ios_base::trunc);
-            testFile.write(payload.c_str(), payload.size());
-            testFile.close();
-        }
-
         config.enableHttpClientTrace = true;
 #ifdef _WIN32
         // TODO: remove this once we get H2 working with WinHttp client
@@ -65,10 +52,7 @@ public:
         m_clientWithWrongEndpoint = Aws::MakeUnique<TranscribeStreamingServiceClient>(ALLOC_TAG, config);
     }
 
-    ~TranscribeStreamingTests()
-    {
-        Aws::FileSystem::RemoveFileIfExists(TEST_FILE_NAME);
-    }
+    ~TranscribeStreamingTests() override = default;
 
     Aws::UniquePtr<TranscribeStreamingServiceClient> m_clientWithWrongCreds;
     Aws::UniquePtr<TranscribeStreamingServiceClient> m_clientWithWrongEndpoint;
@@ -83,10 +67,10 @@ protected:
     {
         m_testTraces += "[FAILURE] " + Aws::Utils::DateTime::Now().ToGmtStringWithMs() + " " + std::move(msg) + "\n";
     };
-    void SetUp()
+    void SetUp() override
     {
     }
-    void TearDown()
+    void TearDown() override
     {
         if (::testing::Test::HasFailure())
         {
@@ -611,8 +595,7 @@ TEST_F(TranscribeStreamingTests, TranscribeStreamingCppSdkSample)
   Aws::UniquePtr<TranscribeStreamingServiceClient> client = Aws::MakeUnique<TranscribeStreamingServiceClient>(ALLOC_TAG, config);
 
   const Aws::Vector<Aws::String> EXPECTED_ALTERNATIVES = {"This is a C plus plus test sample", "This is a C++ test sample"};
-  for(size_t chunkDuration = 50; chunkDuration <= 200; chunkDuration += 50)
-  {
+  for (size_t chunkDuration = 50; chunkDuration <= 200; chunkDuration += 75) {
     m_testTraces.clear();
     TestTrace(Aws::String("### Starting TranscribeStreamingCppSdkSample with chunks of ") + Aws::Utils::StringUtils::to_string(chunkDuration) + " ms ##");
     int64_t startedAt = Aws::Utils::DateTime::Now().Millis();
@@ -641,8 +624,7 @@ TEST_F(TranscribeStreamingTests, TranscribeStreamingKantSample)
   static const char expected[] = "Categorical imperative: Act only according to that maxim whereby you can at the same time will that it should become a universal law. "
                                  "Two things fill the mind with ever-increasing wonder and awe, the more often and the more intensely the mind of thought is drawn to them: "
                                  "the starry heavens above me and the moral law within me.";
-  for(size_t chunkDuration = 50; chunkDuration <= 200; chunkDuration += 50)
-  {
+  for (size_t chunkDuration = 50; chunkDuration <= 200; chunkDuration += 75) {
     m_testTraces.clear();
     TestTrace(Aws::String("### Starting TranscribeStreamingKantSample with chunks of ") + Aws::Utils::StringUtils::to_string(chunkDuration) + " ms ##");
     int64_t startedAt = Aws::Utils::DateTime::Now().Millis();

commit e26f29b89fbb3ae469606b8c69a7cce2e55a7ab7

Fix high CPU utilization regression on event streaming introduced by #3302

src/aws-cpp-sdk-core/include/aws/core/client/AWSClientAsyncCRTP.h
@@ -14,6 +14,8 @@ namespace Aws
 namespace Client
 {
     class AsyncCallerContext;
+    template <typename OutcomeT, typename ClientT, typename AWSEndpointT, typename RequestT, typename HandlerT>
+    class BidirectionalEventStreamingTask;
 
     /**
      * A helper to determine if AWS Operation is EventStream-enabled or not (based on const-ness of the request)
@@ -204,6 +206,9 @@ namespace Client
             return Aws::Client::MakeCallableOperation(AwsServiceClientT::GetAllocationTag(), operationFunc, clientThis, clientThis->m_clientConfiguration.executor.get());
         }
     protected:
+        template <typename OutcomeT, typename ClientT, typename AWSEndpointT, typename RequestT, typename HandlerT>
+        friend class BidirectionalEventStreamingTask; // allow BidirectionalEventStreamingTask to access m_isInitialized
+
         std::atomic<bool> m_isInitialized;
         mutable std::atomic<size_t> m_operationsProcessed;
         mutable std::condition_variable m_shutdownSignal;

src/aws-cpp-sdk-core/include/aws/core/client/AWSClientEventStreamingAsyncTask.h
@@ -91,6 +91,16 @@ class AWS_CORE_LOCAL BidirectionalEventStreamingTask final {
    * @return OutcomeT, operation response, NoResult on success. (Check InitialResponse for the actual service reply).
    */
   OutcomeT operator()() {
+    if(!m_clientThis->m_isInitialized)
+    {
+      AWS_LOGSTREAM_ERROR("BidirectionalEventStreamingTask", "Unable to call " <<
+        m_pRequest->GetServiceRequestName() << ": client is not initialized (or already terminated)");
+      m_handler(m_clientThis, *m_pRequest, Aws::Client::AWSError<CoreErrors>(CoreErrors::NOT_INITIALIZED,
+        "NOT_INITIALIZED", "Client is not initialized or already terminated", false), m_handlerContext);
+      return OutcomeT(NoResult());
+    }
+    Aws::Utils::RAIICounter raiiGuard(m_clientThis->m_operationsProcessed, &m_clientThis->m_shutdownSignal);
+
     const auto outcome = m_clientThis->MakeRequest(*m_pRequest, m_endpoint, m_method, m_signerName);
     if (outcome.IsSuccess()) {
       m_handler(m_clientThis, *m_pRequest, OutcomeT(NoResult()), m_handlerContext);

src/aws-cpp-sdk-core/include/aws/core/utils/logging/ErrorMacros.h
@@ -64,7 +64,7 @@ if(!m_isInitialized) \
   AWS_LOGSTREAM_ERROR(#OPERATION, "Unable to call " #OPERATION ": client is not initialized (or already terminated)"); \
   return Aws::Client::AWSError<CoreErrors>(CoreErrors::NOT_INITIALIZED, "NOT_INITIALIZED", "Client is not initialized or already terminated", false); \
 } \
-Aws::Utils::RAIICounter(this->m_operationsProcessed, &this->m_shutdownSignal)
+Aws::Utils::RAIICounter raiiGuard(this->m_operationsProcessed, &this->m_shutdownSignal)
 
 #define AWS_ASYNC_OPERATION_GUARD(OPERATION) \
 if(!m_isInitialized) \
@@ -72,4 +72,4 @@ if(!m_isInitialized) \
   AWS_LOGSTREAM_ERROR(#OPERATION, "Unable to call " #OPERATION ": client is not initialized (or already terminated)"); \
   return handler(this, request, Aws::Client::AWSError<CoreErrors>(CoreErrors::NOT_INITIALIZED, "NOT_INITIALIZED", "Client is not initialized or already terminated", false), handlerContext); \
 } \
-Aws::Utils::RAIICounter(this->m_operationsProcessed, &this->m_shutdownSignal)
+Aws::Utils::RAIICounter raiiGuard(this->m_operationsProcessed, &this->m_shutdownSignal)

src/aws-cpp-sdk-core/include/aws/core/utils/stream/ConcurrentStreamBuf.h
@@ -41,6 +41,11 @@ namespace Aws
                  */
                 bool WaitForDrain(int64_t timeoutMs);
 
+                /**
+                 * A flag returned by underflow() if there is no data available at the moment but stream must not be closed yet.
+                 */
+                static const int noData;
+
             protected:
                 std::streampos seekoff(std::streamoff off, std::ios_base::seekdir dir, std::ios_base::openmode which = std::ios_base::in | std::ios_base::out) override;
                 std::streampos seekpos(std::streampos pos, std::ios_base::openmode which = std::ios_base::in | std::ios_base::out) override;

src/aws-cpp-sdk-core/source/client/AWSClient.cpp
@@ -357,6 +357,14 @@ HttpResponseOutcome AWSClient::AttemptExhaustively(const Aws::Http::URI& uri,
         {
             break;
         }
+        if (request.IsEventStreamRequest() &&
+             (request.GetBody()->eof() ||
+             (outcome.GetError().GetResponseCode() != Http::HttpResponseCode::REQUEST_NOT_MADE &&
+              outcome.GetError().GetResponseCode() != Http::HttpResponseCode::NETWORK_CONNECT_TIMEOUT &&
+              outcome.GetError().GetResponseCode() != Http::HttpResponseCode::SERVICE_UNAVAILABLE))) {
+          AWS_LOGSTREAM_ERROR(AWS_CLIENT_LOG_TAG, "SDK is not able to retry EventStream request after the connection was established");
+          break;
+        }
 
         AWS_LOGSTREAM_WARN(AWS_CLIENT_LOG_TAG, "Request failed, now waiting " << sleepMillis << " ms before attempting again.");
         if(request.GetBody())

src/aws-cpp-sdk-core/source/http/curl/CurlHttpClient.cpp
@@ -15,6 +15,7 @@
 #include <aws/core/utils/logging/LogMacros.h>
 #include <aws/core/utils/ratelimiter/RateLimiterInterface.h>
 #include <aws/core/utils/stream/AwsChunkedStream.h>
+#include <aws/core/utils/stream/ConcurrentStreamBuf.h>
 
 #include <algorithm>
 #include <cassert>
@@ -315,12 +316,18 @@ static size_t ReadBody(char* ptr, size_t size, size_t nmemb, void* userdata, boo
     {
         size_t amountRead = 0;
         if (isStreaming) {
-          if (!ioStream->eof() && ioStream->peek() != EOF) {
-            amountRead = (size_t)ioStream->readsome(ptr, amountToRead);
+          if (ioStream->bad()) {
+            AWS_LOGSTREAM_ERROR(CURL_HTTP_CLIENT_TAG, "Input stream is bad!");
+            return CURL_READFUNC_ABORT;
           }
-          if (amountRead == 0 && !ioStream->eof()) {
+          const int peekVal = ioStream->peek();
+          if (peekVal == ConcurrentStreamBuf::noData) {
             return CURL_READFUNC_PAUSE;
           }
+          if (ioStream->eof() || peekVal == EOF) {
+            return 0;
+          }
+          amountRead = (size_t)ioStream->readsome(ptr, amountToRead);
         } else if (isAwsChunked && context->m_chunkedStream != nullptr) {
           amountRead = context->m_chunkedStream->BufferedRead(ptr, amountToRead);
         } else {
@@ -417,9 +424,9 @@ int CurlHttpClient::CurlProgressCallback(void *userdata, double, double, double,
     }
 
     const int peekVal = ioStream->peek();
-    if (ioStream->eof() && peekVal == std::char_traits<char>::eof()) {
+    if (ioStream->eof() || peekVal == std::char_traits<char>::eof()) {
       // curl won't call ReadBody after the last ReadBody call returns 0
-      // however, this Progress method is still called few times for incoming data.
+      curl_easy_pause(context->m_curlHandle, CURLPAUSE_CONT);
       return 0;
     }
 
@@ -432,10 +439,10 @@ int CurlHttpClient::CurlProgressCallback(void *userdata, double, double, double,
       // we should use multi handle or another HTTP client in the future to avoid this
       curl_easy_pause(context->m_curlHandle, CURLPAUSE_CONT);
     } else {
-      if (peekVal != std::char_traits<char>::eof()) {
-        curl_easy_pause(context->m_curlHandle, CURLPAUSE_CONT);
-      } else {
+      if (peekVal == ConcurrentStreamBuf::noData) {
         curl_easy_pause(context->m_curlHandle, CURLPAUSE_SEND);
+      } else {
+        curl_easy_pause(context->m_curlHandle, CURLPAUSE_CONT);
       }
     }
 

src/aws-cpp-sdk-core/source/utils/stream/ConcurrentStreamBuf.cpp
@@ -15,10 +15,11 @@ namespace Aws
         namespace Stream
         {
             const char TAG[] = "ConcurrentStreamBuf";
+
+            const int ConcurrentStreamBuf::noData = ((((('n' << 8) | 'z') << 8) | 'm') << 8) | 'a';
+
             ConcurrentStreamBuf::ConcurrentStreamBuf(size_t bufferLength) :
-                m_putArea(bufferLength), // we access [0] of the put area below so we must initialize it.
-                m_eofInput(false),
-                m_eofOutput(false)
+                m_putArea(bufferLength) // we access [0] of the put area below so we must initialize it.
             {
                 m_getArea.reserve(bufferLength);
                 m_backbuf.reserve(bufferLength);
@@ -163,7 +164,7 @@ namespace Aws
                     if (!lock.try_lock())
                     {
                         // don't block consumer, it will retry asking later
-                        return 'z'; // just returning some valid value other than EOF
+                        return noData;
                     }
 
                     if (m_eofInput && m_backbuf.empty())
@@ -187,10 +188,10 @@ namespace Aws
                 char* gbegin = reinterpret_cast<char*>(m_getArea.data());
                 setg(gbegin, gbegin, gbegin + m_getArea.size());
 
-                if (!m_getArea.empty())
+                if (!m_getArea.empty()) {
                   return std::char_traits<char>::to_int_type(*gptr());
-                else
-                  return 'a'; // just returning some valid value other than EOF
+                }
+                return noData;
             }
 
             int ConcurrentStreamBuf::uflow()

src/aws-cpp-sdk-core/source/utils/threading/DefaultExecutor.cpp
@@ -126,7 +126,6 @@ void DefaultExecutor::impl::Detach(std::thread::id id) {
   assert(it != m_tasks.end());
   it->second.first.detach();
   m_tasks.erase(it);
-  m_state = State::Free;
   m_cv.notify_one();
 }
 

tests/aws-cpp-sdk-transcribestreaming-integ-tests/TranscribeErrorCaseTests.cpp
@@ -217,7 +217,7 @@ TEST_F(TranscribeStreamingErrorTests, TranscribeTerminateByLowSpeedLimit) {
         }
         TestTrace(Aws::String("Writing good event"));
         if (!stream.WriteAudioEvent(event)) {
-          AWS_ADD_FAILURE("Failed to write an audio event");
+          // the stream may be force closed by timeout, no test assertion here.
           break;
         }
       }