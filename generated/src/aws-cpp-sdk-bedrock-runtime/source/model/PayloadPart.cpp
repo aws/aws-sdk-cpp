1f73c19f2b7db21c4bd1d80beda1df689c6995b9
SergeyRyabinin <serzhick@gmail.com>
2025-03-27 22:16:43 +0000

    Codegen for #3358

---
+++ generated/src/aws-cpp-sdk-bedrock-runtime/source/model/PayloadPart.cpp
@@ -0,0 +1,51 @@
+﻿/**
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+#include <aws/bedrock-runtime/model/PayloadPart.h>
+#include <aws/core/utils/json/JsonSerializer.h>
+#include <aws/core/utils/HashingUtils.h>
+
+#include <utility>
+
+using namespace Aws::Utils::Json;
+using namespace Aws::Utils;
+
+namespace Aws
+{
+namespace BedrockRuntime
+{
+namespace Model
+{
+
+PayloadPart::PayloadPart(JsonView jsonValue)
+{
+  *this = jsonValue;
+}
+
+PayloadPart& PayloadPart::operator =(JsonView jsonValue)
+{
+  if(jsonValue.ValueExists("bytes"))
+  {
+    m_bytes = HashingUtils::Base64Decode(jsonValue.GetString("bytes"));
+    m_bytesHasBeenSet = true;
+  }
+  return *this;
+}
+
+JsonValue PayloadPart::Jsonize() const
+{
+  JsonValue payload;
+
+  if(m_bytesHasBeenSet)
+  {
+   payload.WithString("bytes", HashingUtils::Base64Encode(m_bytes));
+  }
+
+  return payload;
+}
+
+} // namespace Model
+} // namespace BedrockRuntime
+} // namespace Aws
