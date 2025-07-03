a062677b51f986633dd4c729eefa07c3bdf5661e
YUXUAN CHEN <yuxnchen@amazon.com>
2025-07-03 15:29:02 -0400

    address issues from PR comments

--- tests/performance-tests/include/performance-tests/services/s3/S3PerformanceTest.h
+++ tests/performance-tests/include/performance-tests/services/s3/S3PerformanceTest.h
@@ -5,8 +5,10 @@
 
 #pragma once
 
+#include <aws/core/utils/memory/AWSMemory.h>
 #include <aws/core/utils/memory/stl/AWSString.h>
 #include <aws/s3/S3Client.h>
+#include <performance-tests/PerformanceTestBase.h>
 
 #include <cstddef>
 
@@ -17,46 +19,32 @@ namespace S3 {
  * Configuration for S3 performance test cases.
  */
 struct TestCase {
-  Aws::String sizeLabel;
+  const char* sizeLabel;
   size_t sizeBytes;
-  Aws::String bucketTypeLabel;
+  const char* bucketTypeLabel;
 };
 
 /**
- * Runs S3 performance test by creating a bucket, performing PutObject and GetObject operations,
- * then cleaning up resources.
- * @param s3 S3 client instance to use for operations
- * @param config Test configuration containing object size and bucket type parameters
- * @param availabilityZoneId Availability zone ID for S3 Express buckets
- * @param iterations Number of put/get operations to perform
+ * S3 performance test implementation.
+ * Tests PutObject and GetObject operations with different payload sizes and bucket types.
  */
-void RunTest(Aws::S3::S3Client& s3, const TestCase& config, const Aws::String& availabilityZoneId, int iterations = 3);
+class S3PerformanceTest : public PerformanceTestBase {
+ public:
+  S3PerformanceTest(const Aws::String& region, const TestCase& config, const Aws::String& availabilityZoneId, int iterations = 3);
 
-/**
- * Create S3 bucket for testing.
- * @param s3 S3 client instance
- * @param config Test configuration
- * @param availabilityZoneId Availability zone ID for S3 Express buckets
- * @return Bucket name if successful, empty string if failed
- */
-Aws::String SetupBucket(Aws::S3::S3Client& s3, const TestCase& config, const Aws::String& availabilityZoneId);
+  void Setup() override;
+  void TearDown() override;
+  void Run() override;
 
-/**
- * Run PutObject and GetObject operations.
- * @param s3 S3 client instance
- * @param bucketName Name of the bucket to use
- * @param config Test configuration
- * @param iterations Number of operations to perform
- */
-void RunOperations(Aws::S3::S3Client& s3, const Aws::String& bucketName, const TestCase& config, int iterations);
+ private:
+  const TestCase& m_config;
+  const Aws::String m_region;
+  const Aws::String m_availabilityZoneId;
+  const int m_iterations;
+  Aws::UniquePtr<Aws::S3::S3Client> m_s3;
+  Aws::String m_bucketName;
+};
 
-/**
- * Clean up S3 resources (objects and bucket).
- * @param s3 S3 client instance
- * @param bucketName Name of the bucket to clean up
- * @param iterations Number of objects to delete
- */
-void CleanupResources(Aws::S3::S3Client& s3, const Aws::String& bucketName, int iterations);
 }  // namespace S3
 }  // namespace Services
 }  // namespace PerformanceTest
