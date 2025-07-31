5559d128dadcb3114642db14841a65ef7207d315
YUXUAN CHEN <yuxnchen@amazon.com>
2025-07-31 13:08:28 -0400

    add build-mode as a new test dimension into performance tests

--- tools/scripts/build-tests/run-al2-s3-performance-tests.sh
+++ tools/scripts/build-tests/run-al2-s3-performance-tests.sh
@@ -12,24 +12,27 @@ set -e
 DEFAULT_REGION="us-east-1"
 DEFAULT_AZ_ID="use1-az4"
 DEFAULT_ITERATIONS=10
+DEFAULT_BUILD_MODE="unknown"
 
 if [ "$#" -lt 1 ]; then
-  echo "Error: Missing required argument. Usage: ${0} PREFIX_DIR [-r|--region REGION] [-a|--az-id AZ_ID] [-i|--iterations NUM]"
+  echo "Error: Missing required argument. Usage: ${0} PREFIX_DIR [-r|--region REGION] [-a|--az-id AZ_ID] [-i|--iterations NUM] [-b|--build-mode BUILD_MODE]"
   exit 1
 fi
 
 PREFIX_DIR="$1"
-shift  
+shift
 
 REGION="$DEFAULT_REGION"
 AZ_ID="$DEFAULT_AZ_ID"
 ITERATIONS="$DEFAULT_ITERATIONS"
+BUILD_MODE="$DEFAULT_BUILD_MODE"
 
 while [[ "$#" -gt 0 ]]; do
   case "$1" in
     -r|--region) REGION="$2"; shift 2 ;;
     -a|--az-id) AZ_ID="$2"; shift 2 ;;
     -i|--iterations) ITERATIONS="$2"; shift 2 ;;
+    -b|--build-mode) BUILD_MODE="$2"; shift 2 ;;
     *) echo "Unknown parameter: $1"; exit 1 ;;
   esac
 done
@@ -45,6 +48,8 @@ fi
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${PREFIX_DIR}/al2-install/lib64/"
 
 cd "${PREFIX_DIR}/al2-build"
-if [ -f "${PREFIX_DIR}/aws-sdk-cpp/tools/scripts/suppressions.txt" ]; then export LSAN_OPTIONS=suppressions="${PREFIX_DIR}/aws-sdk-cpp/tools/scripts/suppressions.txt"; fi
-./tests/performance-tests/s3-performance-test --region "$REGION" --az-id "$AZ_ID" --iterations "$ITERATIONS" --commit-id "$COMMIT_ID"
+if [ "$BUILD_MODE" != "release" ] && [ -f "${PREFIX_DIR}/aws-sdk-cpp/tools/scripts/suppressions.txt" ]; then
+  export LSAN_OPTIONS=suppressions="${PREFIX_DIR}/aws-sdk-cpp/tools/scripts/suppressions.txt"
+fi
+./tests/performance-tests/s3-performance-test --region "$REGION" --az-id "$AZ_ID" --iterations "$ITERATIONS" --commit-id "$COMMIT_ID" --build-mode "$BUILD_MODE"
 cat s3-performance-test-results.json
