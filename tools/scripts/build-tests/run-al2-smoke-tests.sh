0997f6e7cdeeaef65c511308a126953c77922f3b
pulimsr <pulimsr@amazon.com>
2025-08-18 14:26:28 -0400

    updating the path for the smoke tests script

--- tools/scripts/build-tests/run-al2-smoke-tests.sh
+++ tools/scripts/build-tests/run-al2-smoke-tests.sh
@@ -20,15 +20,8 @@ if [ -z "$CATAPULT_TEST_ACCOUNT" ]; then
   exit 1
 fi
 
-echo "Setting up AWS authentication"
-export TEST_ASSUME_ROLE_ARN=arn:aws:iam::${CATAPULT_TEST_ACCOUNT}:role/SmokeTest
-export sts=$(aws sts assume-role --role-arn "$TEST_ASSUME_ROLE_ARN" --role-session-name "catapult-smoke-test" --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]')
-export profile=sdk-smoke-test
-aws configure set aws_access_key_id $(echo "$sts" | jq -r '.[0]') --profile "$profile"
-aws configure set aws_secret_access_key $(echo "$sts" | jq -r '.[1]') --profile "$profile"
-aws configure set aws_session_token $(echo "$sts" | jq -r '.[2]') --profile "$profile"
-aws configure list --profile "$profile"
-export AWS_PROFILE=$profile
+echo "Using current AWS credentials for smoke tests"
+aws sts get-caller-identity
 
 echo "Setting the run environment"
 export LD_LIBRARY_PATH="${CODEBUILD_SRC_DIR}/al2-install/lib:${CODEBUILD_SRC_DIR}/al2-build/tests/testing-resources/"
