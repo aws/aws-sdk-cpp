4fea1d38ccde895c4d18d281cce4860d3ee938bb
pulimsr <pulimsr@amazon.com>
2025-08-18 10:49:44 -0400

    chaging the parameters for run-al2-smoke-test.sh

--- tools/scripts/build-tests/run-al2-smoke-tests.sh
+++ tools/scripts/build-tests/run-al2-smoke-tests.sh
@@ -11,23 +11,13 @@
 set -e
 
 if [ "$#" -ne 3 ]; then
-  echo "Usage: ${0} PREFIX_DIR AWS_ACCOUNT ROLE_SESSION_NAME"
+  echo "Usage: ${0} CODEBUILD_SRC_DIR"
   exit 1
 fi
-PREFIX_DIR="$1"
-AWS_ACCOUNT="$2"
-AWS_ROLE_SESSION_NAME="$3"
+CODEBUILD_SRC_DIR="$1"
 
 echo "Setting the run environment"
-export TEST_ASSUME_ROLE_ARN=arn:aws:iam::${AWS_ACCOUNT}:role/IntegrationTest
-export sts=$(aws sts assume-role --role-arn "$TEST_ASSUME_ROLE_ARN" --role-session-name "${AWS_ROLE_SESSION_NAME}" --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]')
-export profile=sdk-smoke-test
-aws configure set aws_access_key_id $(echo "$sts" | jq -r '.[0]') --profile "$profile"
-aws configure set aws_secret_access_key $(echo "$sts" | jq -r '.[1]') --profile "$profile"
-aws configure set aws_session_token $(echo "$sts" | jq -r '.[2]') --profile "$profile"
-aws configure list --profile "$profile"
-export AWS_PROFILE=$profile
-export LD_LIBRARY_PATH="${PREFIX_DIR}/al2-install/lib:${PREFIX_DIR}/al2-build/tests/testing-resources/"
-cd "${PREFIX_DIR}/al2-build"
-if [ -f "${PREFIX_DIR}/aws-sdk-cpp/tools/scripts/suppressions.txt" ]; then export LSAN_OPTIONS=suppressions="${PREFIX_DIR}/aws-sdk-cpp/tools/scripts/suppressions.txt"; fi
+export LD_LIBRARY_PATH="${CODEBUILD_SRC_DIR}/al2-install/lib:${CODEBUILD_SRC_DIR}/al2-build/tests/testing-resources/"
+cd "${CODEBUILD_SRC_DIR}/al2-build"
+if [ -f "${CODEBUILD_SRC_DIR}/aws-sdk-cpp/tools/scripts/suppressions.txt" ]; then export LSAN_OPTIONS=suppressions="${CODEBUILD_SRC_DIR}/aws-sdk-cpp/tools/scripts/suppressions.txt"; fi
 python3 ../aws-sdk-cpp/tools/scripts/run_smoke_tests_simple.py --testDir ./tests
