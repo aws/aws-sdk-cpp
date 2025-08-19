e55800d1b6640648b6d7e8c9f2d243e2f5f33d15
pulimsr <pulimsr@amazon.com>
2025-08-19 10:11:44 -0400

    right smoke.sh

--- tools/scripts/build-tests/run-al2-smoke-tests.sh
+++ tools/scripts/build-tests/run-al2-smoke-tests.sh
@@ -15,35 +15,8 @@ if [ -z "$CODEBUILD_SRC_DIR" ]; then
   exit 1
 fi
 
-if [ -z "$CATAPULT_TEST_ACCOUNT" ]; then
-  echo "Error: CATAPULT_TEST_ACCOUNT environment variable is not set"
-  exit 1
-fi
-
-echo "Setting up AWS authentication"
-export TEST_ASSUME_ROLE_ARN=arn:aws:iam::${CATAPULT_TEST_ACCOUNT}:role/SmokeTest
-echo "Attempting to assume role: $TEST_ASSUME_ROLE_ARN"
-if export sts=$(aws sts assume-role --role-arn "$TEST_ASSUME_ROLE_ARN" --role-session-name "catapult-smoke-test" --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' 2>/dev/null) && [ -n "$sts" ] && [ "$sts" != "null" ]; then
-    echo "Successfully assumed SmokeTest role"
-    export profile=sdk-smoke-test
-    aws configure set aws_access_key_id $(echo "$sts" | jq -r '.[0]') --profile "$profile"
-    aws configure set aws_secret_access_key $(echo "$sts" | jq -r '.[1]') --profile "$profile"
-    aws configure set aws_session_token $(echo "$sts" | jq -r '.[2]') --profile "$profile"
-    aws configure list --profile "$profile"
-    export AWS_PROFILE=$profile
-else
-    echo "Failed to assume SmokeTest role, using current credentials"
-    aws sts get-caller-identity
-fi
-
 echo "Setting the run environment"
 echo "Contents of CODEBUILD_SRC_DIR: ${CODEBUILD_SRC_DIR}"
-ls -la "${CODEBUILD_SRC_DIR}/al2-build"
-ls -la "${CODEBUILD_SRC_DIR}/al2-build/lib"
-ls -la "${CODEBUILD_SRC_DIR}/al2-build/lib/libs2n.so"
-ls -la "${CODEBUILD_SRC_DIR}/al2-build/lib/libs2n.so.1"
-ls -la "${CODEBUILD_SRC_DIR}/al2-build/lib/libs2n.so.1.0.0"
-export LD_LIBRARY_PATH="${CODEBUILD_SRC_DIR}/al2-install/lib:${CODEBUILD_SRC_DIR}/al2-build/lib/generated/smoke-tests"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${CODEBUILD_SRC_DIR}/al2-install/lib64/"
 cd "${CODEBUILD_SRC_DIR}/al2-build"
-if [ -f "${CODEBUILD_SRC_DIR}/aws-sdk-cpp/tools/scripts/suppressions.txt" ]; then export LSAN_OPTIONS=suppressions="${CODEBUILD_SRC_DIR}/aws-sdk-cpp/tools/scripts/suppressions.txt"; fi
 python3 ../aws-sdk-cpp/tools/scripts/run_smoke_tests_simple.py --testDir "${CODEBUILD_SRC_DIR}/al2-build"
