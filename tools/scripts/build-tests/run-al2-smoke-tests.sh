00772e8c82517de3cac36ce43bc4ad4683e22641
pulimsr <pulimsr@amazon.com>
2025-08-19 22:31:14 -0400

    updating path to run_smoke_tests_simple.py

--- tools/scripts/build-tests/run-al2-smoke-tests.sh
+++ tools/scripts/build-tests/run-al2-smoke-tests.sh
@@ -17,4 +17,5 @@ fi
 
 echo "Contents of CODEBUILD_SRC_DIR: ${CODEBUILD_SRC_DIR}"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${CODEBUILD_SRC_DIR}/al2-install/lib64/"
-python3 "${CODEBUILD_SRC_DIR}/tools/scripts/run_smoke_tests_simple.py" --testDir "${CODEBUILD_SRC_DIR}/al2-build"
+cd "${CODEBUILD_SRC_DIR}/mac-build"
+python3 "../aws-sdk-cpp/tools/scripts/run_smoke_tests_simple.py" --testDir "${CODEBUILD_SRC_DIR}/al2-build"
