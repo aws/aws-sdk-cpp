e2fc090dde671b3d707be7472ab3c451abcae2ce
kai lin <cailinn@amazon.com>
2025-09-16 14:26:14 -0400

    added tracking for profile providers

--- src/aws-cpp-sdk-core/source/client/UserAgent.cpp
+++ src/aws-cpp-sdk-core/source/client/UserAgent.cpp
@@ -48,6 +48,7 @@ const std::pair<UserAgentFeature, const char*> BUSINESS_METRIC_MAPPING[] = {
     {UserAgentFeature::CREDENTIALS_IMDS, "0"},
     {UserAgentFeature::CREDENTIALS_STS_ASSUME_ROLE, "i"},
     {UserAgentFeature::CREDENTIALS_HTTP, "z"},
+    {UserAgentFeature::CREDENTIALS_PROFILE_SOURCE_PROFILE, "p"},
 };
 
 const std::pair<const char*, UserAgentFeature> RETRY_FEATURE_MAPPING[] = {
