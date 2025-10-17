a9ed91efbc0907cfe842c47da538ab81e30dfaab
kai lin <cailinn@amazon.com>
2025-10-17 15:29:35 -0400

    Add Profile computed getters using Aws::Crt::Optional for proper null handling:   - GetServicesName() returns services definition name or empty Optional   - GetEndpointUrl() returns global endpoint URL or empty Optional - Add AWSConfigFileProfileConfigLoader::GetServices() const accessor Uses Aws::Crt::Optional instead of empty strings to properly represent "no value" state.

--- src/aws-cpp-sdk-core/include/aws/core/config/AWSConfigFileProfileConfigLoader.h
+++ src/aws-cpp-sdk-core/include/aws/core/config/AWSConfigFileProfileConfigLoader.h
@@ -39,12 +39,6 @@ namespace Aws
              * This can avoid creating new loader object if the file changed.
              */
             void SetFileName(const Aws::String& fileName) { m_fileName = fileName; }
-
-            /**
-             * Get services map for endpoint resolution.
-             */
-            const Aws::Map<Aws::String, Aws::Map<Aws::String, Aws::String>>& GetServices() const;
-
         protected:
             virtual bool LoadInternal() override;
             virtual bool PersistInternal(const Aws::Map<Aws::String, Aws::Config::Profile>&) override;
