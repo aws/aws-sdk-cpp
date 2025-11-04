203992a77ea94a2920c0dd1005947f145a91c63e
sbaluja <saarthibaluja@hotmail.com>
2025-11-04 15:55:28 -0500

    Add support for client level auth resolution with preferences

--- src/aws-cpp-sdk-core/include/smithy/identity/auth/built-in/SigV4MultiAuthResolver.h
+++ src/aws-cpp-sdk-core/include/smithy/identity/auth/built-in/SigV4MultiAuthResolver.h
@@ -12,12 +12,18 @@
 #include <smithy/identity/auth/built-in/SigV4aAuthSchemeOption.h>
 #include <smithy/identity/auth/built-in/SigV4AuthScheme.h>
 
+/**
+ * This is an auth scheme resolver prioritizing endpoints2.0 auth resolution
+ * used for s3 which supersedes the model based auth resolution we do in GenericAuthResolver.
+ */
+
 namespace smithy {
     template<typename EndPointProviderType, typename ClientConfigType,typename ServiceAuthSchemeParametersT = DefaultAuthSchemeResolverParameters>
     class SigV4MultiAuthSchemeResolver : public AuthSchemeResolverBase<ServiceAuthSchemeParametersT, ClientConfigType>
     {
-    private:
     public:
+        SigV4MultiAuthSchemeResolver(const Aws::Vector<AuthSchemeOption>& allowedAuth) : m_allowedAuth(allowedAuth) {}
+        SigV4MultiAuthSchemeResolver() = default;
 
         void Init(const ClientConfigType& config) override{
             m_endpointProviderForAuth = Aws::MakeShared<EndPointProviderType>("SigV4MultiAuthSchemeResolver");
@@ -27,8 +33,12 @@ namespace smithy {
         using ServiceAuthSchemeParameters = ServiceAuthSchemeParametersT;
         virtual ~SigV4MultiAuthSchemeResolver() = default;
 
-        Aws::Vector<AuthSchemeOption> resolveAuthScheme(const ServiceAuthSchemeParameters& identityProperties) override
+        Aws::Vector<AuthSchemeOption> resolveAuthSchemeImpl(const ServiceAuthSchemeParameters& identityProperties) override
         {
+            if (!m_allowedAuth.empty()) {
+              // This code path will not currently be taken, but we should enable a way for the user to disable preference for endpoints2.0 resolution and use the preference list
+              return m_allowedAuth;
+            }
             //pack endpoint params from identityProperties
             Aws::Endpoint::EndpointParameters epParams;
 
@@ -66,6 +76,10 @@ namespace smithy {
 
             return {SigV4AuthSchemeOption::sigV4AuthSchemeOption};
         }
+
+    private:
+      Aws::Vector<AuthSchemeOption> m_allowedAuth;
+
     protected:
         std::shared_ptr<EndPointProviderType> m_endpointProviderForAuth;
     };
