7f5f529810bcf86ad2054bcf54a34c5a275a4ac2
sbiscigl <sbiscigl@amazon.com>
2025-11-10 18:09:09 -0500

    refactor common crt creds impl, add aws login provider

--- src/aws-cpp-sdk-core/include/aws/core/auth/LoginCredentialsProvider.h
+++ src/aws-cpp-sdk-core/include/aws/core/auth/LoginCredentialsProvider.h
@@ -2,11 +2,9 @@
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0.
  */
-
 #pragma once
 
 #include <aws/core/Core_EXPORTS.h>
-#include <aws/core/auth/AWSCredentialsProvider.h>
 #include <aws/core/auth/CrtCredentialsProvider.h>
 
 namespace Aws {
@@ -16,11 +14,10 @@ namespace Auth {
  * Note that STS accepts request with protocol of queryxml. Calling GetAWSCredentials() will trigger (if expired)
  * a query request using AWSHttpResourceClient under the hood.
  */
-class AWS_CORE_API STSAssumeRoleWebIdentityCredentialsProvider : public CrtCredentialsProvider {
+class AWS_CORE_API LoginCredentialsProvider : public CrtCredentialsProvider {
  public:
-  STSAssumeRoleWebIdentityCredentialsProvider();
-  STSAssumeRoleWebIdentityCredentialsProvider(const Aws::Client::ClientConfiguration::CredentialProviderConfiguration& config);
-  ~STSAssumeRoleWebIdentityCredentialsProvider() override;
+  LoginCredentialsProvider(const Aws::Client::ClientConfiguration::CredentialProviderConfiguration& config);
+  ~LoginCredentialsProvider() override;
 };
 }  // namespace Auth
 }  // namespace Aws
