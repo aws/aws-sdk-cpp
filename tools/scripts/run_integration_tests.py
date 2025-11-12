b915d3bba88ed4cfa24595b21a70f5201039d6fa
Snigdha <pulimsr@amazon.com>
2025-11-12 11:06:56 -0500

    fix/when s_preservePathSeparators enabled (#3619)

--- tools/scripts/run_integration_tests.py
+++ tools/scripts/run_integration_tests.py
@@ -44,6 +44,7 @@ def main():
         "aws-cpp-sdk-dynamodb-integration-tests",
         "aws-cpp-sdk-sqs-integration-tests",
         "aws-cpp-sdk-sqs-unit-tests",
+        "aws-cpp-sdk-sns-integration-tests",
         "aws-cpp-sdk-s3-integration-tests",
         "aws-cpp-sdk-s3-unit-tests",
         "aws-cpp-sdk-s3-crt-integration-tests",
