1e629c75cbd4a883fe3e5fe8e33f6592df343af0
pulimsr <pulimsr@amazon.com>
2025-11-13 02:39:21 -0500

    unit test for transfer

--- tests/aws-cpp-sdk-transfer-unit-tests/TransferUnitTests.cpp
+++ tests/aws-cpp-sdk-transfer-unit-tests/TransferUnitTests.cpp
@@ -19,39 +19,17 @@ const char* ALLOCATION_TAG = "TransferUnitTest";
 
 class MockS3Client : public S3Client {
 public:
-    MockS3Client() : S3Client(), shouldReturnWrongRange(false) {}
-    
-    void SetReturnWrongRange(bool wrongRange) {
-        shouldReturnWrongRange = wrongRange;
-    }
+    MockS3Client() : S3Client(){};
 
-    GetObjectOutcome GetObject(const GetObjectRequest& request) const override {
+    GetObjectOutcome GetObject(const GetObjectRequest&) const override {
         GetObjectResult result;
-        
-        if (request.RangeHasBeenSet()) {
-            if (shouldReturnWrongRange) {
-                // Return a different range than requested
-                result.SetContentRange("bytes 1024-2047/2048");
-            } else {
-                // Parse the requested range and return matching ContentRange
-                Aws::String requestRange = request.GetRange();
-                if (requestRange.find("bytes=") == 0) {
-                    Aws::String range = requestRange.substr(6); // Remove "bytes="
-                    result.SetContentRange("bytes " + range + "/2048");
-                }
-            }
-        }
-        
-        // Create a raw pointer stream
+        // Return wrong range to trigger validation failure
+        result.SetContentRange("bytes 1024-2047/2048");
         auto stream = Aws::New<std::stringstream>(ALLOCATION_TAG);
         *stream << "mock data";
         result.ReplaceBody(stream);
-        
         return GetObjectOutcome(std::move(result));
     }
-
-private:
-    bool shouldReturnWrongRange;
 };
 
 class TransferUnitTest : public testing::Test {
@@ -62,39 +40,21 @@ protected:
     }
 
     static void SetUpTestSuite() {
-#ifdef USE_AWS_MEMORY_MANAGEMENT
-        _testMemorySystem.reset(new ExactTestMemorySystem(1024, 128));
-        _options.memoryManagementOptions.memoryManager = _testMemorySystem.get();
-#endif
         InitAPI(_options);
     }
 
     static void TearDownTestSuite() {
         ShutdownAPI(_options);
-#ifdef USE_AWS_MEMORY_MANAGEMENT
-        EXPECT_EQ(_testMemorySystem->GetCurrentOutstandingAllocations(), 0ULL);
-        EXPECT_EQ(_testMemorySystem->GetCurrentBytesAllocated(), 0ULL);
-        EXPECT_TRUE(_testMemorySystem->IsClean());
-        _testMemorySystem.reset();
-#endif
     }
 
     std::shared_ptr<PooledThreadExecutor> executor;
     std::shared_ptr<MockS3Client> mockS3Client;
     static SDKOptions _options;
-#ifdef USE_AWS_MEMORY_MANAGEMENT
-    static std::unique_ptr<ExactTestMemorySystem> _testMemorySystem;
-#endif
 };
 
 SDKOptions TransferUnitTest::_options;
-#ifdef USE_AWS_MEMORY_MANAGEMENT
-std::unique_ptr<ExactTestMemorySystem> TransferUnitTest::_testMemorySystem = nullptr;
-#endif
 
-TEST_F(TransferUnitTest, ContentRangeVerification_Failure) {
-    mockS3Client->SetReturnWrongRange(true);
-    
+TEST_F(TransferUnitTest, ContentValidationShouldFail) {
     TransferManagerConfiguration config(executor.get());
     config.s3Client = mockS3Client;
     auto transferManager = TransferManager::Create(config);
@@ -103,6 +63,7 @@ TEST_F(TransferUnitTest, ContentRangeVerification_Failure) {
         return Aws::New<std::stringstream>(ALLOCATION_TAG);
     };
     
+    // Request bytes 0-1023 but mock returns 1024-2047, should fail validation
     auto handle = transferManager->DownloadFile("test-bucket", "test-key", 0, 1024, createStreamFn);
     handle->WaitUntilFinished();
     
