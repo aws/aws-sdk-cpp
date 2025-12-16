4aca5630ba6e49869a0f173f9439ae73722d4558
aws-sdk-cpp-automation <github-aws-sdk-cpp-automation@amazon.com>
2025-12-16 21:59:50 +0000

    This release adds support for rebooting InfluxDB DbInstances and DbClusters Add support for dynamic payloads in IoT Device Management Commands

---
+++ generated/src/aws-cpp-sdk-iot/include/aws/iot/model/OutputFormat.h
@@ -0,0 +1,22 @@
+﻿/**
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+#pragma once
+#include <aws/core/utils/memory/stl/AWSString.h>
+#include <aws/iot/IoT_EXPORTS.h>
+
+namespace Aws {
+namespace IoT {
+namespace Model {
+enum class OutputFormat { NOT_SET, JSON, CBOR };
+
+namespace OutputFormatMapper {
+AWS_IOT_API OutputFormat GetOutputFormatForName(const Aws::String& name);
+
+AWS_IOT_API Aws::String GetNameForOutputFormat(OutputFormat value);
+}  // namespace OutputFormatMapper
+}  // namespace Model
+}  // namespace IoT
+}  // namespace Aws
---
+++ generated/src/aws-cpp-sdk-iot/source/model/AwsJsonSubstitutionCommandPreprocessorConfig.cpp
@@ -0,0 +1,40 @@
+﻿/**
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+#include <aws/core/utils/json/JsonSerializer.h>
+#include <aws/iot/model/AwsJsonSubstitutionCommandPreprocessorConfig.h>
+
+#include <utility>
+
+using namespace Aws::Utils::Json;
+using namespace Aws::Utils;
+
+namespace Aws {
+namespace IoT {
+namespace Model {
+
+AwsJsonSubstitutionCommandPreprocessorConfig::AwsJsonSubstitutionCommandPreprocessorConfig(JsonView jsonValue) { *this = jsonValue; }
+
+AwsJsonSubstitutionCommandPreprocessorConfig& AwsJsonSubstitutionCommandPreprocessorConfig::operator=(JsonView jsonValue) {
+  if (jsonValue.ValueExists("outputFormat")) {
+    m_outputFormat = OutputFormatMapper::GetOutputFormatForName(jsonValue.GetString("outputFormat"));
+    m_outputFormatHasBeenSet = true;
+  }
+  return *this;
+}
+
+JsonValue AwsJsonSubstitutionCommandPreprocessorConfig::Jsonize() const {
+  JsonValue payload;
+
+  if (m_outputFormatHasBeenSet) {
+    payload.WithString("outputFormat", OutputFormatMapper::GetNameForOutputFormat(m_outputFormat));
+  }
+
+  return payload;
+}
+
+}  // namespace Model
+}  // namespace IoT
+}  // namespace Aws
