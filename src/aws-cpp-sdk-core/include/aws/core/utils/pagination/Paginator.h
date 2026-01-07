d8766e4bafa518cdd1c34d43e5fd16b4c5311e57
kai lin <cailinn@amazon.com>
2026-01-07 13:23:07 -0500

    paginator template and unit test

---
+++ src/aws-cpp-sdk-core/include/aws/core/utils/pagination/Paginator.h
@@ -0,0 +1,97 @@
+/**
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+#pragma once
+
+namespace Aws
+{
+namespace Utils
+{
+namespace Pagination
+{
+
+template <class ServiceClient, class OperationRequest, class OperationTraits>
+class PagePaginator
+{
+public:
+    using OutcomeType = typename OperationTraits::OutcomeType;
+    using ResultType = typename OperationTraits::ResultType;
+
+    class PageIterator
+    {
+    public:
+        PageIterator() = default; // end iterator
+
+        PageIterator(ServiceClient* client, const OperationRequest& firstReq)
+            : m_client(client),
+              m_request(firstReq),
+              m_atEnd(false)
+        {
+            FetchPage(); // load first page eagerly so * is valid after begin()
+        }
+
+        const OutcomeType& operator*() const { return m_currentOutcome; }
+
+        PageIterator& operator++()
+        {
+            if (m_atEnd) return *this;
+
+            // If last fetch failed, end iteration
+            if (!m_currentOutcome.IsSuccess())
+            {
+                m_atEnd = true;
+                return *this;
+            }
+
+            // If no more results, end iteration
+            if (!OperationTraits::HasMoreResults(m_currentOutcome.GetResult()))
+            {
+                m_atEnd = true;
+                return *this;
+            }
+
+            // Mutate iterator-owned request for next page
+            OperationTraits::SetNextRequest(m_currentOutcome.GetResult(), m_request);
+            FetchPage();
+            return *this;
+        }
+
+        bool operator!=(const PageIterator& other) const
+        {
+            return !(m_atEnd && other.m_atEnd);
+        }
+        
+        bool operator==(const PageIterator& other) const
+        {
+            return !(*this != other);
+        }
+
+    private:
+        void FetchPage()
+        {
+            m_currentOutcome = OperationTraits::Invoke(*m_client, m_request);
+        }
+
+        ServiceClient* m_client{};
+        OperationRequest m_request{};
+        OutcomeType m_currentOutcome{};
+        bool m_atEnd{true};
+    };
+
+    PagePaginator(ServiceClient* client, const OperationRequest& firstReq)
+        : m_client(client),
+          m_firstRequest(firstReq) {}
+
+    PageIterator begin() const { return PageIterator(m_client, m_firstRequest); }
+    PageIterator end() const { return PageIterator(); }
+
+private:
+    ServiceClient* m_client{};
+    OperationRequest m_firstRequest{};
+};
+
+} // namespace Pagination
+} // namespace Utils
+} // namespace Aws
---
+++ tests/aws-cpp-sdk-core-tests/utils/pagination/PaginatorTest.cpp
@@ -0,0 +1,198 @@
+/**
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+#include <aws/testing/AwsCppSdkGTestSuite.h>
+#include <aws/core/utils/pagination/Paginator.h>
+#include <aws/core/utils/StringUtils.h>
+#include <vector>
+#include <string>
+
+using namespace Aws::Utils::Pagination;
+using namespace Aws::Utils;
+
+// Test types for pagination
+struct TestResult {
+    Aws::Vector<Aws::String> items;
+    Aws::String outputToken;
+    bool moreResults;
+    
+    const Aws::Vector<Aws::String>& GetItems() const { return items; }
+    const Aws::String& GetOutputToken() const { return outputToken; }
+    bool GetMoreResults() const { return moreResults; }
+};
+
+struct TestOutcome {
+    bool success;
+    Aws::String error;
+    TestResult result;
+    
+    bool IsSuccess() const { return success; }
+    const Aws::String& GetError() const { return error; }
+    const TestResult& GetResult() const { return result; }
+};
+
+struct TestRequest {
+    Aws::String inputToken;
+    int limitKey = 3;
+    
+    void SetInputToken(const Aws::String& t) { inputToken = t; }
+    const Aws::String& GetInputToken() const { return inputToken; }
+};
+
+class TestClient {
+private:
+    Aws::Vector<Aws::String> allData;
+    bool shouldFail = false;
+    
+public:
+    TestClient() : allData({"apple", "banana", "cherry", "dragon-fruit", "elderberry", "fig", "grape"}) {}
+    
+    void SetData(const Aws::Vector<Aws::String>& data) { allData = data; }
+    void SetShouldFail(bool fail) { shouldFail = fail; }
+    void Reset() {
+        allData = {"apple", "banana", "cherry", "dragon-fruit", "elderberry", "fig", "grape"};
+        shouldFail = false;
+    }
+    
+    TestOutcome ListItems(const TestRequest& request) {
+        if (shouldFail) {
+            return {false, "Request failed", {}};
+        }
+        
+        int startIdx = 0;
+        if (!request.GetInputToken().empty()) {
+            startIdx = StringUtils::ConvertToInt32(request.GetInputToken().c_str());
+        }
+        
+        TestResult result;
+        for (size_t i = 0; i < static_cast<size_t>(request.limitKey) && startIdx + i < allData.size(); ++i) {
+            result.items.push_back(allData[startIdx + i]);
+        }
+        
+        int nextIdx = startIdx + request.limitKey;
+        if (static_cast<size_t>(nextIdx) < allData.size()) {
+            result.outputToken = StringUtils::to_string(nextIdx);
+            result.moreResults = true;
+        } else {
+            result.moreResults = false;
+        }
+        
+        return {true, "", result};
+    }
+};
+
+struct ListItemsTraits {
+    using OutcomeType = TestOutcome;
+    using ResultType = TestResult;
+    
+    static OutcomeType Invoke(TestClient& client, const TestRequest& request) {
+        return client.ListItems(request);
+    }
+    
+    static bool HasMoreResults(const TestResult& result) {
+        return result.GetMoreResults();
+    }
+    
+    static void SetNextRequest(const TestResult& result, TestRequest& request) {
+        request.SetInputToken(result.GetOutputToken());
+    }
+};
+
+class PaginatorTest : public Aws::Testing::AwsCppSdkGTestSuite
+{
+protected:
+    void SetUp() override
+    {
+        client.Reset();
+    }
+    
+    void TearDown() override
+    {
+        client.Reset();
+    }
+    
+    TestClient client;
+    TestRequest request;
+};
+
+TEST_F(PaginatorTest, TestIteratesThroughAllPages)
+{
+    PagePaginator<TestClient, TestRequest, ListItemsTraits> paginator(&client, request);
+    
+    Aws::Vector<Aws::String> allItems;
+    int pageCount = 0;
+    
+    for (const auto& outcome : paginator)
+    {
+        ASSERT_TRUE(outcome.IsSuccess());
+        pageCount++;
+        
+        const auto& page = outcome.GetResult();
+        for (const auto& item : page.GetItems())
+        {
+            allItems.push_back(item);
+        }
+    }
+    
+    EXPECT_EQ(pageCount, 3); // 7 items / 3 per page = 3 pages
+    EXPECT_EQ(allItems.size(), 7u);
+    EXPECT_STREQ(allItems[0].c_str(), "apple");
+    EXPECT_STREQ(allItems[6].c_str(), "grape");
+}
+
+TEST_F(PaginatorTest, TestHandlesErrorGracefully)
+{
+    client.SetShouldFail(true);
+    PagePaginator<TestClient, TestRequest, ListItemsTraits> paginator(&client, request);
+    
+    auto it = paginator.begin();
+    ASSERT_TRUE(it != paginator.end());
+    
+    const auto& outcome = *it;
+    EXPECT_FALSE(outcome.IsSuccess());
+    EXPECT_STREQ(outcome.GetError().c_str(), "Request failed");
+    
+    // Iterator should terminate after error
+    ++it;
+    EXPECT_TRUE(it == paginator.end());
+}
+
+TEST_F(PaginatorTest, TestEmptyResultSet)
+{
+    client.SetData({});
+    PagePaginator<TestClient, TestRequest, ListItemsTraits> paginator(&client, request);
+    
+    auto it = paginator.begin();
+    ASSERT_TRUE(it != paginator.end());
+    
+    const auto& outcome = *it;
+    EXPECT_TRUE(outcome.IsSuccess());
+    EXPECT_TRUE(outcome.GetResult().GetItems().empty());
+    EXPECT_FALSE(outcome.GetResult().GetMoreResults());
+    
+    // Should terminate after first empty page
+    ++it;
+    EXPECT_TRUE(it == paginator.end());
+}
+
+TEST_F(PaginatorTest, TestBeginEndIteratorComparison)
+{
+    PagePaginator<TestClient, TestRequest, ListItemsTraits> paginator(&client, request);
+    
+    auto begin = paginator.begin();
+    auto end = paginator.end();
+    
+    EXPECT_TRUE(begin != end);
+    EXPECT_FALSE(begin == end);
+    
+    // Iterate to end
+    while (begin != end)
+    {
+        ++begin;
+    }
+    
+    EXPECT_TRUE(begin == end);
+    EXPECT_FALSE(begin != end);
+}
