439f456f144ccdc1af01d0273a948b009bdfff59
kai lin <cailinn@amazon.com>
2026-01-13 14:44:20 -0500

    Smithy code gen for pagination

--- generated/src/aws-cpp-sdk-s3/include/aws/s3/S3ClientPagination.h
+++ generated/src/aws-cpp-sdk-s3/include/aws/s3/S3ClientPagination.h
@@ -4,22 +4,23 @@
  */
 
 #pragma once
-#include <aws/s3/S3Client.h>
 #include <aws/core/utils/pagination/Paginator.h>
+#include <aws/s3/S3Client.h>
 #include <aws/s3/model/pagination/ListBucketsPaginationTraits.h>
 #include <aws/s3/model/pagination/ListDirectoryBucketsPaginationTraits.h>
 #include <aws/s3/model/pagination/ListObjectsV2PaginationTraits.h>
 #include <aws/s3/model/pagination/ListPartsPaginationTraits.h>
 
-namespace Aws
-{
-namespace S3
-{
+namespace Aws {
+namespace S3 {
 
-using ListBucketsPaginator = Aws::Utils::Pagination::PagePaginator<S3Client, Model::ListBucketsRequest, Pagination::ListBucketsPaginationTraits>;
-using ListDirectoryBucketsPaginator = Aws::Utils::Pagination::PagePaginator<S3Client, Model::ListDirectoryBucketsRequest, Pagination::ListDirectoryBucketsPaginationTraits>;
-using ListObjectsV2Paginator = Aws::Utils::Pagination::PagePaginator<S3Client, Model::ListObjectsV2Request, Pagination::ListObjectsV2PaginationTraits>;
+using ListBucketsPaginator =
+    Aws::Utils::Pagination::PagePaginator<S3Client, Model::ListBucketsRequest, Pagination::ListBucketsPaginationTraits>;
+using ListDirectoryBucketsPaginator =
+    Aws::Utils::Pagination::PagePaginator<S3Client, Model::ListDirectoryBucketsRequest, Pagination::ListDirectoryBucketsPaginationTraits>;
+using ListObjectsV2Paginator =
+    Aws::Utils::Pagination::PagePaginator<S3Client, Model::ListObjectsV2Request, Pagination::ListObjectsV2PaginationTraits>;
 using ListPartsPaginator = Aws::Utils::Pagination::PagePaginator<S3Client, Model::ListPartsRequest, Pagination::ListPartsPaginationTraits>;
 
-} // namespace S3
-} // namespace Aws
+}  // namespace S3
+}  // namespace Aws
--- generated/src/aws-cpp-sdk-s3/include/aws/s3/model/pagination/ListBucketsPaginationTraits.h
+++ generated/src/aws-cpp-sdk-s3/include/aws/s3/model/pagination/ListBucketsPaginationTraits.h
@@ -4,41 +4,30 @@
  */
 
 #pragma once
+#include <aws/s3/S3Client.h>
 #include <aws/s3/S3_EXPORTS.h>
 #include <aws/s3/model/ListBucketsRequest.h>
 #include <aws/s3/model/ListBucketsResult.h>
-#include <aws/s3/S3Client.h>
 
-namespace Aws
-{
-namespace S3
-{
-namespace Pagination
-{
+namespace Aws {
+namespace S3 {
+namespace Pagination {
 
-struct ListBucketsPaginationTraits
-{
-    using RequestType = Model::ListBucketsRequest;
-    using ResultType = Model::ListBucketsResult;
-    using OutcomeType = Model::ListBucketsOutcome;
-    using ClientType = S3Client;
+struct ListBucketsPaginationTraits {
+  using RequestType = Model::ListBucketsRequest;
+  using ResultType = Model::ListBucketsResult;
+  using OutcomeType = Model::ListBucketsOutcome;
+  using ClientType = S3Client;
 
-    static OutcomeType Invoke(ClientType& client, const RequestType& request)
-    {
-        return client.ListBuckets(request);
-    }
+  static OutcomeType Invoke(ClientType& client, const RequestType& request) { return client.ListBuckets(request); }
 
-    static bool HasMoreResults(const ResultType& result)
-    {
-        return result.GetIsTruncated();
-    }
+  static bool HasMoreResults(const ResultType& result) { return result.GetIsTruncated(); }
 
-    static void SetNextRequest(const ResultType& result, RequestType& request)
-    {
-        request.SetContinuationToken(result.GetContinuationToken());
-    }
+  static void SetNextRequest(const ResultType& result, RequestType& request) {
+    request.SetContinuationToken(result.GetContinuationToken());
+  }
 };
 
-} // namespace Pagination
-} // namespace S3
-} // namespace Aws
+}  // namespace Pagination
+}  // namespace S3
+}  // namespace Aws
