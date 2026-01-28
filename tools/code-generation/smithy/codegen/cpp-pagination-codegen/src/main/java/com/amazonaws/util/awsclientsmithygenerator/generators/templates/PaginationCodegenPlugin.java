a92eb251e7025659081803357ba66468b9d349aa
kai lin <cailinn@amazon.com>
2026-01-28 01:05:17 -0500

    some services only exist in c2j and not smithy, added temporary solution to generate them

--- tools/code-generation/smithy/codegen/cpp-pagination-codegen/src/main/java/com/amazonaws/util/awsclientsmithygenerator/generators/templates/PaginationCodegenPlugin.java
+++ tools/code-generation/smithy/codegen/cpp-pagination-codegen/src/main/java/com/amazonaws/util/awsclientsmithygenerator/generators/templates/PaginationCodegenPlugin.java
@@ -17,7 +17,12 @@ import com.amazonaws.util.awsclientsmithygenerator.generators.templates.Paginati
 import com.amazonaws.util.awsclientsmithygenerator.generators.templates.PaginationCompilationTestGenerator;
 import com.amazonaws.util.awsclientsmithygenerator.generators.templates.PaginationBaseGenerator;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.stream.Collectors;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class PaginationCodegenPlugin implements SmithyBuildPlugin {
 
@@ -29,6 +34,15 @@ public class PaginationCodegenPlugin implements SmithyBuildPlugin {
     @Override
     public void execute(PluginContext context) {
         var model = context.getModel();
+        
+        // TODO: Remove this workaround - mock projections should use proper Smithy model generation
+        // instead of manually writing files in the plugin
+        // Check if this is a legacy service mock projection
+        String projectionName = context.getProjectionName();
+        if (projectionName.endsWith(".mock")) {
+            generateLegacyServiceFile(context);
+            return;
+        }
         
         for (ServiceShape service : model.getServiceShapes()) {
             // Find paginated operations using TopDownIndex, excluding deprecated operations
@@ -73,6 +87,52 @@ public class PaginationCodegenPlugin implements SmithyBuildPlugin {
                 PaginationCompilationTestGenerator testGenerator = new PaginationCompilationTestGenerator(context, service, paginatedOps, parser.getServiceMap());
                 testGenerator.run();
             }
+        }
+    }
+    
+    private void generateLegacyServiceFile(PluginContext context) {
+        Map<String, String> legacyServices = new HashMap<>();
+        legacyServices.put("importexport", "ImportExport");
+        legacyServices.put("sdb", "SimpleDB");
+        
+        String projectionName = context.getProjectionName();
+        String c2jName = projectionName.replace(".mock", "");
+        String pascalName = legacyServices.get(c2jName);
+        
+        if (pascalName == null) {
+            return;
+        }
+        
+        try {
+            Path baseDir = context.getFileManifest().getBaseDir();
+            Path includeDir = baseDir.resolve("include").resolve("aws").resolve(c2jName);
+            Files.createDirectories(includeDir);
+                
+                // Generate empty pagination base header
+                Path headerFile = includeDir.resolve(pascalName + "PaginationBase.h");
+                String content = String.format(
+                    "/**\n" +
+                    " * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.\n" +
+                    " * SPDX-License-Identifier: Apache-2.0.\n" +
+                    " */\n\n" +
+                    "#pragma once\n\n" +
+                    "namespace Aws\n" +
+                    "{\n" +
+                    "    namespace %s\n" +
+                    "    {\n" +
+                    "        template<typename ClientType>\n" +
+                    "        class %sPaginationBase\n" +
+                    "        {\n" +
+                    "        public:\n" +
+                    "            virtual ~%sPaginationBase() = default;\n" +
+                    "        };\n" +
+                    "    } // namespace %s\n" +
+                    "} // namespace Aws\n",
+                    pascalName, pascalName, pascalName, pascalName
+                );
+            Files.writeString(headerFile, content);
+        } catch (Exception e) {
+            System.err.println("Failed to generate legacy service " + c2jName + ": " + e.getMessage());
         }
     }
 }
