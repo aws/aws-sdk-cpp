5d243325499c12ef5910436cafc4e31126ad0eac
Parth Patel <parth.patel7@ibm.com>
2026-02-11 06:50:02 -0600

    Addressed the review comments (#3714)

--- src/aws-cpp-sdk-core/source/platform/aix/TimeHelper.cpp
+++ src/aws-cpp-sdk-core/source/platform/aix/TimeHelper.cpp
@@ -1,15 +1,36 @@
-/* TimeHelper.cpp
-   timegm() implementation for AIX platform.
+/*
+  This is free and unencumbered software released into the public domain.
+
+  Anyone is free to copy, modify, publish, use, compile, sell, or
+  distribute this software, either in source code form or as a compiled
+  binary, for any purpose, commercial or non-commercial, and by any
+  means.
+
+  In jurisdictions that recognize copyright laws, the author or authors
+  of this software dedicate any and all copyright interest in the
+  software to the public domain. We make this dedication for the benefit
+  of the public at large and to the detriment of our heirs and
+  successors. We intend this dedication to be an overt act of
+  relinquishment in perpetuity of all present and future rights to this
+  software under copyright law.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+  IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+  OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+  ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+  OTHER DEALINGS IN THE SOFTWARE.
+
+  For more information, please refer to <https://unlicense.org>
+  <https://github.com/lzsiga/timegm/blob/main/aix-timegm.c>
 */
 
-#include <aws/core/platform/Time.h>
+/* timegm() implementation for AIX platform. */
 
 #include <time.h>
 
-namespace Aws
-{
-namespace Time
-{
+time_t timegm (struct tm *);
 time_t timegm (struct tm *tm)
 {
     static const int msum [2][12] = {
@@ -29,7 +50,10 @@ time_t timegm (struct tm *tm)
     int isleapyear;
     long long t;
 
-    if (!tm) return -1;
+    if (!tm){
+        errno = EINVAL;
+        return (time_t)-1;
+    }
 
     year = tm->tm_year + tmstr_year;
     isleapyear= (year%4==0) - (year%100==0) + (year%400==0);
@@ -39,8 +63,11 @@ time_t timegm (struct tm *tm)
         tm->tm_mday<1 || tm->tm_mday>mlen[isleapyear][tm->tm_mon] ||
         tm->tm_hour<0 || tm->tm_hour>23 ||
         tm->tm_min<0  || tm->tm_min>59 ||
-        tm->tm_min<0  || tm->tm_sec>59) return -1;
+        tm->tm_sec<0  || tm->tm_sec>59) {
 
+        errno = EINVAL;
+        return (time_t)-1;
+    }
 /* days between 'current year' and 'epoch_year' has to be calculated
    in three steps: */
 
@@ -69,12 +96,10 @@ time_t timegm (struct tm *tm)
 
 #if LONG_MAX == 2147483647L
     if (t<LONG_MIN || t>LONG_MAX) {
-        t= -1;
+        errno = EOVERFLOW;
+        return (time_t)-1;
     }
 #endif
 
-    return t;
+    return (time_t)t;
 }
-
-} // namespace Time
-} // namespace Aws
