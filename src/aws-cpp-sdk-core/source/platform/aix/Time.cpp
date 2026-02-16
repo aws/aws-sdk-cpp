146d08cd942925b241c31748f39315f60a5d5739
Parth Patel <parth.patel7@ibm.com>
2026-02-16 10:37:20 -0600

    Addressed the review comments. (#3714)

--- src/aws-cpp-sdk-core/source/platform/aix/Time.cpp
+++ src/aws-cpp-sdk-core/source/platform/aix/Time.cpp
@@ -1,3 +1,7 @@
+/**
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
 /*
   This is free and unencumbered software released into the public domain.
 
@@ -24,46 +28,56 @@
 
   For more information, please refer to <https://unlicense.org>
   <https://github.com/lzsiga/timegm/blob/main/aix-timegm.c>
+
+  Modified the above code to align with C++ constructs.
 */
 
-/* timegm() implementation for AIX platform. */
+#include <aws/core/platform/Time.h>
 
 #include <time.h>
 
-time_t timegm (struct tm *);
-time_t timegm (struct tm *tm)
+namespace Aws
 {
-    static const int msum [2][12] = {
-        { 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334},        /* normal years */
-        { 0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335}        /* leap years */
-    };
-    static const int mlen [2][12] = {
-        { 31, 28, 31, 30,  31,  30,  31,  31,  30,  31,  30, 31},
-        { 31, 29, 31, 30,  31,  30,  31,  31,  30,  31,  30, 31}
-    };
-    static const int tmstr_year= 1900; /* base of 'tm_year' in 'struct tm' */
-    static const int epoch_year= 1970; /* unix timestamp epoch */
-    static const int base_year=  -9999;  /* start of a 400-year period: used to be 1601,
-                                            but this allows larger range (in 64 bit)
-                                            mind you, this is proleptic Gregorian */
+namespace Time
+{
+namespace
+{
+    const Aws::Array<Aws::Array<int, 12>, 2> msum{{
+        {{ 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334}},       /* normal years */
+        {{ 0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335}}        /* leap years */
+    }};
+    const Aws::Array<Aws::Array<int, 12>, 2> msummlen{{
+        {{ 31, 28, 31, 30,  31,  30,  31,  31,  30,  31,  30, 31}},
+        {{ 31, 29, 31, 30,  31,  30,  31,  31,  30,  31,  30, 31}}
+    }};
+
+    const int tmstr_year = 1900;   /* base of 'tm_year' in 'struct tm' */
+    const int epoch_year = 1970;   /* unix timestamp epoch */
+    const int base_year = -9999;   /* start of a 400-year period: used to be 1601,
+                                      but this allows larger range (in 64 bit)
+                                      mind you, this is proleptic Gregorian */
+}
+
+time_t TimeGM(struct tm* const t)
+{
     int year, ytmp, dtmp, ytmpe, dtmpe;
     int isleapyear;
-    long long t;
+    long long ret_val;
 
-    if (!tm){
+    if (!t){
         errno = EINVAL;
         return (time_t)-1;
     }
 
-    year = tm->tm_year + tmstr_year;
+    year = t->tm_year + tmstr_year;
     isleapyear= (year%4==0) - (year%100==0) + (year%400==0);
 
     if (year<-9999    || year>9999  ||
-        tm->tm_mon<0  || tm->tm_mon>11 ||
-        tm->tm_mday<1 || tm->tm_mday>mlen[isleapyear][tm->tm_mon] ||
-        tm->tm_hour<0 || tm->tm_hour>23 ||
-        tm->tm_min<0  || tm->tm_min>59 ||
-        tm->tm_sec<0  || tm->tm_sec>59) {
+        t->tm_mon<0  || t->tm_mon>11 ||
+        t->tm_mday<1 || t->tm_mday>msummlen[isleapyear][t->tm_mon] ||
+        t->tm_hour<0 || t->tm_hour>23 ||
+        t->tm_min<0  || t->tm_min>59 ||
+        t->tm_sec<0  || t->tm_sec>59) {
 
         errno = EINVAL;
         return (time_t)-1;
@@ -80,26 +94,39 @@ time_t timegm (struct tm *tm)
     dtmpe = ytmpe*365 + ytmpe/4 - ytmpe/100 + ytmpe/400;
 
 /* 3. days between 'current year' and 'epoch_year' */
-    t  = dtmp - dtmpe;
+    ret_val  = dtmp - dtmpe;
 
-    t += msum[isleapyear][tm->tm_mon];
-    t += tm->tm_mday-1;
+    ret_val += msum[isleapyear][t->tm_mon];
+    ret_val += t->tm_mday-1;
 
-    tm->tm_wday = (t+4)%7;/* 0..6=Sun..Sat; adding 4 to adjust 1970.01.01.=Thursday; */
-    if (tm->tm_wday<0) tm->tm_wday += 7;
-    tm->tm_yday = msum[isleapyear][tm->tm_mon] + tm->tm_mday-1;
-    tm->tm_isdst= 0;
+    t->tm_wday = (ret_val+4)%7;/* 0..6=Sun..Sat; adding 4 to adjust 1970.01.01.=Thursday; */
+    if (t->tm_wday<0) t->tm_wday += 7;
+    t->tm_yday = msum[isleapyear][t->tm_mon] + t->tm_mday-1;
+    t->tm_isdst= 0;
 
-    t  = t*24 + tm->tm_hour;
-    t  = t*60 + tm->tm_min;
-    t  = t*60 + tm->tm_sec;
+    ret_val  = ret_val*24 + t->tm_hour;
+    ret_val  = ret_val*60 + t->tm_min;
+    ret_val  = ret_val*60 + t->tm_sec;
 
 #if LONG_MAX == 2147483647L
-    if (t<LONG_MIN || t>LONG_MAX) {
+    if (ret_val<LONG_MIN || ret_val>LONG_MAX) {
         errno = EOVERFLOW;
         return (time_t)-1;
     }
 #endif
 
-    return (time_t)t;
+    return (time_t)ret_val;
 }
+
+void LocalTime(tm* t, std::time_t time)
+{
+    localtime_r(&time, t);
+}
+
+void GMTime(tm* t, std::time_t time)
+{
+    gmtime_r(&time, t);
+}
+
+} // namespace Time
+} // namespace Aws
