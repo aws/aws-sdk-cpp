40911e0e07dcd7082da7ae734e8f4d3b53546f1e
sbaluja <saarthibaluja@hotmail.com>
2026-03-17 17:31:34 -0400

    Seperate different acceptors into seperate structs/classes

--- src/aws-cpp-sdk-core/include/aws/core/utils/Waiter.h
+++ src/aws-cpp-sdk-core/include/aws/core/utils/Waiter.h
@@ -10,6 +10,7 @@
 
 #include <algorithm>
 #include <functional>
+#include <aws/core/utils/memory/AWSMemory.h>
 #include <utility>
 #include <thread>
 #include <type_traits>
@@ -18,7 +19,6 @@ namespace Aws {
 namespace Utils {
 
 enum class WaiterState { SUCCESS, RETRY, FAILURE };
-enum class MatcherType { STATUS, ERROR, PATH, PATH_ALL, PATH_ANY };
 enum class WaiterErrors { MAX_ATTEMPTS, INVALID_ACTION };
 
 using ExpectedValue = Crt::Variant<int, bool, Aws::String>;
@@ -30,46 +30,85 @@ using WaiterOutcome = Outcome<OutcomeT, WaiterError>;
 template <typename OutcomeT>
 struct Acceptor {
   WaiterState state;
-  MatcherType matcher;
-  ExpectedValue expected;
-  std::function<bool(const OutcomeT&, const ExpectedValue&)> pathMatcher;
-
-  Acceptor(WaiterState s, MatcherType m, ExpectedValue e, std::function<bool(const OutcomeT&, const ExpectedValue&)> pm = [](const OutcomeT&, const ExpectedValue&) { return false; })
-      : state(s), matcher(m), expected(std::move(e)), pathMatcher(std::move(pm)) {}
+  explicit Acceptor(WaiterState s) : state(s) {}
+  virtual ~Acceptor() = default;
+  virtual bool Matches(const OutcomeT& outcome) const = 0;
 };
 
 template <typename OutcomeT>
-using PathMatcher = std::function<bool(const OutcomeT&, const ExpectedValue&)>;
+struct StatusAcceptor : Acceptor<OutcomeT> {
+  int expectedStatus;
+  StatusAcceptor(WaiterState s, int status) : Acceptor<OutcomeT>(s), expectedStatus(status) {}
+  bool Matches(const OutcomeT& outcome) const override {
+    return GetStatusCode(outcome) == expectedStatus;
+  }
 
-template <typename RequestT, typename OutcomeT>
-class Waiter {
-  bool Matches(const Acceptor<OutcomeT>& acceptor, const OutcomeT& outcome) {
-    switch (acceptor.matcher) {
-      case MatcherType::STATUS: {
-        int status = GetStatusCode(outcome);
-        return status == acceptor.expected.template get<int>();
-      }
-      case MatcherType::ERROR: {
-        if (acceptor.expected.template holds_alternative<bool>()) {
-          bool expectError = acceptor.expected.template get<bool>();
-          return outcome.IsSuccess() != expectError;
-        }
-        if (!outcome.IsSuccess()) {
-          auto errorCode = outcome.GetError().GetExceptionName();
-          return errorCode == acceptor.expected.template get<Aws::String>();
-        }
-        return false;
-      }
-      case MatcherType::PATH:
-      case MatcherType::PATH_ALL:
-      case MatcherType::PATH_ANY:
-        return acceptor.pathMatcher ? acceptor.pathMatcher(outcome, acceptor.expected) : false;
+ private:
+  template<typename T>
+  struct HasGetHttpResponseCode {
+    template<typename U> static auto test(int) -> decltype(std::declval<U>().GetHttpResponseCode(), std::true_type());
+    template<typename> static std::false_type test(...);
+    static constexpr bool value = decltype(test<T>(0))::value;
+  };
+  template<typename T>
+  struct HasGetResponseCode {
+    template<typename U> static auto test(int) -> decltype(std::declval<U>().GetResponseCode(), std::true_type());
+    template<typename> static std::false_type test(...);
+    static constexpr bool value = decltype(test<T>(0))::value;
+  };
+  template<typename T>
+  static int GetStatusCodeFromResult(const T& result) {
+    static_assert(HasGetHttpResponseCode<T>::value, "Result type must have GetHttpResponseCode() method");
+    return static_cast<int>(result.GetHttpResponseCode());
+  }
+  template<typename T>
+  static int GetStatusCodeFromError(const T& error) {
+    static_assert(HasGetResponseCode<T>::value, "Error type must have GetResponseCode() method");
+    return static_cast<int>(error.GetResponseCode());
+  }
+  static int GetStatusCode(const OutcomeT& outcome) {
+    return outcome.IsSuccess()
+      ? GetStatusCodeFromResult(outcome.GetResult())
+      : GetStatusCodeFromError(outcome.GetError());
+  }
+};
+
+template <typename OutcomeT>
+struct ErrorAcceptor : Acceptor<OutcomeT> {
+  ExpectedValue expected;
+  ErrorAcceptor(WaiterState s, ExpectedValue e) : Acceptor<OutcomeT>(s), expected(std::move(e)) {}
+  bool Matches(const OutcomeT& outcome) const override {
+    if (this->expected.holds_alternative<bool>()) {
+      return outcome.IsSuccess() != this->expected.get<bool>();
+    }
+    if (!outcome.IsSuccess()) {
+      return outcome.GetError().GetExceptionName() == this->expected.get<Aws::String>();
     }
     return false;
   }
+};
 
+template <typename OutcomeT>
+using PathMatcherFn = std::function<bool(const OutcomeT&, const ExpectedValue&)>;
+
+template <typename OutcomeT>
+struct PathAcceptor : Acceptor<OutcomeT> {
+  ExpectedValue expected;
+  PathMatcherFn<OutcomeT> pathMatcher;
+  PathAcceptor(WaiterState s, ExpectedValue e, PathMatcherFn<OutcomeT> pm)
+      : Acceptor<OutcomeT>(s), expected(std::move(e)), pathMatcher(std::move(pm)) {}
+  bool Matches(const OutcomeT& outcome) const override {
+    return pathMatcher ? pathMatcher(outcome, expected) : false;
+  }
+};
+
+
+template <typename RequestT, typename OutcomeT>
+class Waiter {
  public:
-  Waiter(int delay, int maxAttempts, std::vector<Acceptor<OutcomeT>> acceptors, std::function<OutcomeT(const RequestT&)> op,
+  using AcceptorPtr = Aws::UniquePtr<Acceptor<OutcomeT>>;
+
+  Waiter(int delay, int maxAttempts, std::vector<AcceptorPtr> acceptors, std::function<OutcomeT(const RequestT&)> op,
          const Aws::String& waiterName = "Waiter")
       : m_delay(delay),
         m_maxAttempts(maxAttempts),
@@ -82,9 +121,9 @@ class Waiter {
       auto outcome = m_operation(request);
 
       auto matched = std::find_if(m_acceptors.begin(), m_acceptors.end(),
-                             [this, &outcome](const Acceptor<OutcomeT>& acceptor) -> bool { return Matches(acceptor, outcome); });
+                             [&outcome](const AcceptorPtr& acceptor) -> bool { return acceptor->Matches(outcome); });
       if (matched != m_acceptors.end()) {
-        switch (matched->state) {
+        switch (matched->get()->state) {
           case WaiterState::SUCCESS:
             return WaiterOutcome<OutcomeT>(outcome);
           case WaiterState::FAILURE:
@@ -106,56 +145,9 @@ class Waiter {
 private:
   int m_delay;
   int m_maxAttempts;
-  std::vector<Acceptor<OutcomeT>> m_acceptors;
+  std::vector<AcceptorPtr> m_acceptors;
   std::function<OutcomeT(const RequestT&)> m_operation;
   Aws::String m_name;
-  /**
-   * Returns the HTTP status code from the result or error.
-   * Result objects use GetHttpResponseCode(), errors use GetResponseCode().
-   */
-  inline int GetStatusCode(OutcomeT outcome) const { 
-    return outcome.IsSuccess() 
-      ? GetStatusCodeFromResult(outcome.GetResult()) 
-      : GetStatusCodeFromError(outcome.GetError()); 
-  }
-
-  template<typename T>
-  struct has_get_http_response_code
-  {
-      template<typename U>
-      static auto test(int) -> decltype(std::declval<U>().GetHttpResponseCode(), std::true_type());
-
-      template<typename>
-      static std::false_type test(...);
-
-      static constexpr bool value = decltype(test<T>(0))::value;
-  };
-
-  template<typename T>
-  struct has_get_response_code
-  {
-      template<typename U>
-      static auto test(int) -> decltype(std::declval<U>().GetResponseCode(), std::true_type());
-
-      template<typename>
-      static std::false_type test(...);
-
-      static constexpr bool value = decltype(test<T>(0))::value;
-  };
-
-  template<typename T>
-  static int GetStatusCodeFromResult(const T& result)
-  {
-      static_assert(has_get_http_response_code<T>::value, "Result type must have GetHttpResponseCode() method");
-      return static_cast<int>(result.GetHttpResponseCode());
-  }
-
-  template<typename T>
-  static int GetStatusCodeFromError(const T& error)
-  {
-      static_assert(has_get_response_code<T>::value, "Error type must have GetResponseCode() method");
-      return static_cast<int>(error.GetResponseCode());
-  }
 
 };
 }  // namespace Utils
