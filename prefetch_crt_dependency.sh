48d7149f3e4f72f189cd68295dac29ce6326d3ce
sbiscigl <sbiscigl@amazon.com>
2026-04-02 16:29:22 -0400

    Update CRT to v0.38.4

--- prefetch_crt_dependency.sh
+++ prefetch_crt_dependency.sh
@@ -3,17 +3,17 @@
 # SPDX-License-Identifier: Apache-2.0.
 
 CRT_URI_PREFIX=https://codeload.github.com/awslabs
-CRT_URI=${CRT_URI_PREFIX}/aws-crt-cpp/zip/25a2e61972f5a0d517c6dc62533fdc7d7bd122a1  # v0.37.4
+CRT_URI=${CRT_URI_PREFIX}/aws-crt-cpp/zip/2705dbd13811d584dc03beb17567bd7fc1fa7069  # v0.38.4
 
 AWS_C_AUTH_URI=${CRT_URI_PREFIX}/aws-c-auth/zip/fc4b87655e5cd3921f18d1859193c74af4102071  # v0.10.1
 AWS_C_CAL_URI=${CRT_URI_PREFIX}/aws-c-cal/zip/1cb9412158890201a6ffceed779f90fe1f48180c  # v0.9.13
 AWS_C_COMMON_URI=${CRT_URI_PREFIX}/aws-c-common/zip/95515a8b1ff40d5bb14f965ca4cbbe99ad1843df  # v0.12.6
 AWS_C_COMPRESSION_URI=${CRT_URI_PREFIX}/aws-c-compression/zip/d8264e64f698341eb03039b96b4f44702a9b3f83  # v0.3.2
-AWS_C_EVENT_STREAM_URI=${CRT_URI_PREFIX}/aws-c-event-stream/zip/c741f95e9050a1a4bed4b3aa7543bd3e024f6e56  # v0.6.0
-AWS_C_HTTP_URI=${CRT_URI_PREFIX}/aws-c-http/zip/0d8e1a933f46b8af984dfc8168ebcdf32748c184  # v0.10.11
+AWS_C_EVENT_STREAM_URI=${CRT_URI_PREFIX}/aws-c-event-stream/zip/66cafb1d8bb1bfeb62a7601ce03d1a6fcd4798ed  # v0.6.1
+AWS_C_HTTP_URI=${CRT_URI_PREFIX}/aws-c-http/zip/da535b1bf9c9334730eb78a26a1bbb3c069b38c9  # v0.10.14
 AWS_C_IO_URI=${CRT_URI_PREFIX}/aws-c-io/zip/bfb0819d3906502483611ce832a5ec6b897c8421  # v0.26.1
 AWS_C_MQTT_URI=${CRT_URI_PREFIX}/aws-c-mqtt/zip/41b6a7d6d566a56eff69743df66c077d56a80c9d  # v0.14.0
-AWS_C_S3_URI=${CRT_URI_PREFIX}/aws-c-s3/zip/e9d1bde139f88b08aaa3bf0507f443f31ccede93  # v0.11.5
+AWS_C_S3_URI=${CRT_URI_PREFIX}/aws-c-s3/zip/ab764f57ec3cab7866c0f8b7d9e9772b7cc1b9ee  # v0.12.2
 AWS_C_SDKUTILS_URI=${CRT_URI_PREFIX}/aws-c-sdkutils/zip/f678bda9e21f7217e4bbf35e0d1ea59540687933  # v0.2.4
 AWS_CHECKSUMS_URI=${CRT_URI_PREFIX}/aws-checksums/zip/1d5f2f1f3e5d013aae8810878ceb5b3f6f258c4e  # v0.2.10
 AWS_LC_URI=${CRT_URI_PREFIX}/aws-lc/zip/37d86461a95782fd5d8b77873f9e1cb134ea2f95  # v1.69.0
