677481c79634c3c065fcb2bc3a6d8357d282c817
Vinay Narahari <narahavi@amazon.com>
2026-06-01 10:42:15 -0400

    Add aws-cpp-sdk-s3-transfer package scaffolding for Transfer Manager 2.0

---
+++ src/aws-cpp-sdk-s3-transfer/include/aws/s3-transfer/DownloadRequest.h
@@ -0,0 +1,18 @@
+/**
+* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+#pragma once
+#include <aws/s3-transfer/S3Transfer_EXPORTS.h>
+
+
+namespace Aws {
+  namespace S3 {
+    namespace Transfer {
+      class AWS_S3_TRANSFER_API DownloadRequest {
+        //need to fill in class defintion later
+      };
+    }
+  }
+}
+
---
+++ src/aws-cpp-sdk-s3-transfer/source/s3-transfer/S3TransferManager.cpp
@@ -0,0 +1,15 @@
+/**
+* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+* SPDX-License-Identifier: Apache-2.0.
+*/
+
+#include <aws/s3-transfer/S3TransferManager.h>
+
+
+namespace Aws {
+  namespace S3{
+    namespace Transfer {
+
+    }
+  }
+}
