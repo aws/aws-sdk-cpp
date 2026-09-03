ad41e4dce4024a63e45c4c4d658312b80db6df5f
Snigdha <pulimsr@amazon.com>
2026-09-03 11:15:37 -0400

    integration test for bidirectional-stream

--- tests/aws-cpp-sdk-bedrock-runtime-integration-tests/RunTests.cpp
+++ tests/aws-cpp-sdk-bedrock-runtime-integration-tests/RunTests.cpp
@@ -8,6 +8,11 @@
 #include <aws/testing/platform/PlatformTesting.h>
 #include <aws/testing/TestingEnvironment.h>
 #include <aws/testing/MemoryTesting.h>
+#include <chrono>
+#include <cstdlib>
+#include <future>
+#include <iostream>
+#include <thread>
 
 int main(int argc, char** argv)
 {
@@ -23,7 +28,18 @@ int main(int argc, char** argv)
     ::testing::InitGoogleTest(&argc, argv);
     int exitCode = RUN_ALL_TESTS();
 
-    Aws::ShutdownAPI(options);
+    {
+        static constexpr auto SHUTDOWN_TIMEOUT = std::chrono::seconds(60);
+        auto shutdownFuture = std::async(std::launch::async, [&options]() { Aws::ShutdownAPI(options); });
+        if (shutdownFuture.wait_for(SHUTDOWN_TIMEOUT) != std::future_status::ready)
+        {
+            std::cerr << "FATAL: Aws::ShutdownAPI() did not complete within "
+                      << std::chrono::duration_cast<std::chrono::seconds>(SHUTDOWN_TIMEOUT).count()
+                      << "s after bidirectional streaming; shutdown is hung (issue #3911)." << std::endl;
+            std::abort();
+        }
+        shutdownFuture.get();
+    }
     AWS_END_MEMORY_TEST_EX;
     Aws::Testing::ShutdownPlatformTest(options);
     return exitCode;
