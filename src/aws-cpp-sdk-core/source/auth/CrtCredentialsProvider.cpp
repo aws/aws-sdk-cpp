fbf4f5376e2902bf86755ba807984bf8c4728201
okruitho <oscar@ducklabs.com>
2026-09-15 12:17:19 +0200

    Datetime uin64_t constructor now checks for overflow. perserves ns.

--- src/aws-cpp-sdk-core/source/auth/CrtCredentialsProvider.cpp
+++ src/aws-cpp-sdk-core/source/auth/CrtCredentialsProvider.cpp
@@ -9,7 +9,6 @@
 #include <aws/crt/auth/Credentials.h>
 
 #include <condition_variable>
-#include <limits>
 #include <mutex>
 
 using namespace Aws::Auth;
@@ -101,10 +100,7 @@ AWSCredentials CrtCredentialsProvider::ExtractCredentialsFromCrt(const Aws::Crt:
   const auto secretKeyCursor = crtCredentials.GetSecretAccessKey();
   credentials.SetAWSSecretKey({reinterpret_cast<char*>(secretKeyCursor.ptr), secretKeyCursor.len});
   const uint64_t expiration = crtCredentials.GetExpirationTimepointInSeconds();
-  // CRT uses UINT64_MAX for no expiration; preserve AWSCredentials' non-expiring default.
-  if (expiration != (std::numeric_limits<uint64_t>::max)()) {
-    credentials.SetExpiration(DateTime{expiration});
-  }
+  credentials.SetExpiration(DateTime{expiration});
   const auto sessionTokenCursor = crtCredentials.GetSessionToken();
   credentials.SetSessionToken({reinterpret_cast<char*>(sessionTokenCursor.ptr), sessionTokenCursor.len});
   return credentials;
